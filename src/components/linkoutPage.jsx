c4756a8718385e76a71a844eac6347ea6fc89778
Samir Akre <sakre@ucdavis.edu>
2018-11-13 14:38:57 -0800

    Styled linkout page and added actual links

--- src/components/linkoutPage.jsx
+++ src/components/linkoutPage.jsx
@@ -1,13 +1,82 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+const linkList = [
+  {
+    name: 'Source Code',
+    links: [
+      {
+        protocol: 'https',
+        url: 'Github.com/AshleyLab/motrpac-frontend',
+        text: 'Github Repository for code creating this sites front end',
+      },
+      {
+        protocol: 'https',
+        url: 'Github.com/AshleyLab/motrpac_metaanalysis',
+        text: 'Github Repository for code used in meta-analysis',
+      },
+    ],
+  },
+  {
+    name: 'Partners',
+    links: [
+      {
+        protocol: 'https',
+        url: 'Commonfund.nih.gov',
+        text: 'The primary funding source for the MoTrPAC Initiative',
+      },
+      {
+        protocol: 'https',
+        url: 'ENCODEdcc.org',
+        text: 'ENCODE project website',
+      },
+      {
+        protocol: 'http',
+        url: 'METABOLOMICSworkbench.org',
+        text: 'UCSD Metabolomics Workbench',
+      },
+      {
+        protocol: 'https',
+        url: 'GTExPortal.org',
+        text: 'The Genotype-Tissue Expression (GTEx) project',
+      },
+    ],
+  },
+];
+
 export function LinkoutPage() {
+  const links = linkList.map(category => (
+    <div key={category.name} className="LinkCategory">
+      <h4>{category.name}</h4>
+      {
+        category.links.map(link => <UsefulLink key={link.url} link={link} />)
+      }
+    </div>
+  ));
+
   return (
     <div className="container linkoutPage">
       <div className="row title">
         <div className="col">
           <h3>Useful Links</h3>
         </div>
+      </div>
+      <UsefulLink link={{ url: 'MoTrPAC.org', text: 'Main entrance point for overview of the entire MoTrPAC project', protocol: 'https' }} />
+      {links}
+    </div>
+  );
+}
+
+function UsefulLink({ link }) {
+  return (
+    <div className="row linkRow">
+      <div className="col-5">
+        <a href={`${link.protocol}://www.${link.url}`} target="_new">{link.url}</a>
+      </div>
+      <div className="col">
+        <p>
+          {link.text}
+        </p>
       </div>
     </div>
   );
