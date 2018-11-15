3aa7820d2a9fd1facfeba4b59e2753e5f60c783b
Samir Akre <sakre@ucdavis.edu>
2018-11-15 11:45:57 -0800

    added modal for confirmation before actual cancelling of upload

--- src/components/uploadListRow.jsx
+++ src/components/uploadListRow.jsx
@@ -6,6 +6,32 @@ import PropTypes from 'prop-types';
 function UploadListRow({ uploadItem, cancelUpload }) {
   let icon;
   let message;
+  const strID = uploadItem.id.replace('.', '_');
+  function ConfirmCancel() {
+    return (
+      <div className="modal fade" id={`cancelUpload${strID}`} tabIndex="-1" role="dialog" aria-labelledby="cancelUploadLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="cancelUploadLabel">
+                Cancel Uploading
+                &nbsp;
+                {uploadItem.file.name}
+                ?
+              </h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" className="btn cancelUploadConfirm" data-dismiss="modal" onClick={() => cancelUpload(uploadItem.id)}>Cancel Upload</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
   // Changes  error message and icon based on upload status
   switch (uploadItem.status) {
     case 'UPLOAD_SUCCESS': {
@@ -42,7 +68,8 @@ function UploadListRow({ uploadItem, cancelUpload }) {
       <td>{uploadItem.file.name}</td>
       <td>{message}</td>
       <td className="centered">{icon}</td>
-      <td className="centered"><button type="button" className="btn cancelBtn" onClick={() => cancelUpload(uploadItem.id)}><span className="oi oi-circle-x" /></button></td>
+      <td className="centered"><button type="button" className="btn cancelBtn" data-toggle="modal" data-target={`#cancelUpload${strID}`}><span className="oi oi-circle-x" /></button></td>
+      <ConfirmCancel />
     </tr>
   );
 }
