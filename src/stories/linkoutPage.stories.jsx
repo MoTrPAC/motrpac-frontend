079034847b7d6a529cb317b241ed7d1d104e9409
Samir Akre <sakre@ucdavis.edu>
2018-11-16 13:14:18 -0800

    MOTR-47 fixed action dependency issue in storybook import

--- src/stories/linkoutPage.stories.jsx
+++ src/stories/linkoutPage.stories.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { storiesOf, action } from '@storybook/react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { LinkoutPage } from '../components/linkoutPage';
 import { Navbar } from '../components/navbar';
 import { Footer } from '../components/footer';
