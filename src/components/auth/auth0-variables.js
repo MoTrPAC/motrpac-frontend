3a1d187cfcd5a17bf42dd721304e28f14d3b0713
Jimmy Zhen <jzhen@stanford.edu>
2018-11-26 08:53:55 -0800

    :tada: Initial commit for Auth0 configs

---
+++ src/components/auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'hypeway.auth0.com',
+  audience: 'https://hypeway.auth0.com/userinfo',
+  clientId: 'a8bPMoRQMu_3oe1Lzj0JKmUSakiOXlOp',
+  callbackUrl: 'http://localhost:3000/callback'
+};
