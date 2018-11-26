8b43365bb68e08c50c0258ced8c25e3377312726
Jimmy Zhen <jzhen@stanford.edu>
2018-11-26 08:53:09 -0800

    :sparkles: Added Auth0 authentication service

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import Auth from './components/auth';
 import * as serviceWorker from './serviceWorker';
+import './main.css';
 
-require('./main.css');
+const auth = new Auth();
 
-render(<App />, document.getElementById('root'));
+render(
+  <BrowserRouter>
+    <App auth={auth} />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
