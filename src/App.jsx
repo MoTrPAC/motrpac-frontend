312cbc07991b25af02cdd7873b5f12810aabcc8e
Jimmy Zhen <jzhen@stanford.edu>
2018-11-26 08:59:33 -0800

    Updated component with Auth0 authentication service
    Passing the Auth0 component and its class methods as prop into the following child components (for the time being):
    1. Dashboard
    2. UploadScreenConnected
    3. Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Route, Router } from 'react-router-dom';
+import { Route, Router, withRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer, { defaultRootState } from './reducers/index';
 import 'bootstrap';
 import History from './history';
 import NavbarConnected from './components/navbar';
-import FooterConnected from './components/footer';
+import Footer from './components/footer';
 import LandingPageConnected from './components/landingPage';
-import DashboardConnected from './components/dashboard';
+import Dashboard from './components/dashboard';
 import UploadScreenConnected from './components/uploadScreen';
-import AuthLoadingConnected from './components/authLoading';
 import LinkoutPageConnected from './components/linkoutPage';
+import Callback from './components/callback';
 
-const hist = History;
+function App(props) {
+  const authenticated = props.auth.isAuthenticated();
 
-function App({ history = hist }) {
   // TODO: Before production remove redux devtools extension javascript
   return (
     <Provider store={createStore(rootReducer,
@@ -24,46 +23,44 @@ function App({ history = hist }) {
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
       }
     >
-      <Router history={history}>
+      <Router history={History}>
         <div className="App">
+          <Route exact path='/callback' render={() => (
+            <Callback auth={props.auth} />
+          )}
+          />
           <header>
             <NavbarConnected />
           </header>
           <div className="componentHolder">
             <Route path="/" exact component={LandingPageConnected} />
-            <Route path="/dashboard" component={DashboardConnected} />
-            <Route path="/upload" component={UploadScreenConnected} />
+            <Route exact path="/dashboard" render={() => (
+              <Dashboard
+                authenticated={authenticated}
+                auth={props.auth}
+                history={props.history}
+              />)
+            }
+            />
+            <Route exact path="/upload" render={() => (
+              <UploadScreenConnected
+                authenticated={authenticated}
+                auth={props.auth}
+                history={props.history}
+              />)
+            }
+            />
             <Route path="/external-links" component={LinkoutPageConnected} />
           </div>
-          <FooterConnected />
-          <AuthLoadingConnected />
+          <Footer
+            authenticated={authenticated}
+            auth={props.auth}
+            history={props.history}
+          />
         </div>
       </Router>
     </Provider>
   );
 }
 
-App.propTypes = {
-  history: PropTypes.shape({
-    length: PropTypes.number,
-    action: PropTypes.string,
-    location: PropTypes.shape({
-      pathname: PropTypes.string,
-      hash: PropTypes.string,
-      key: PropTypes.string,
-    }),
-    createHref: PropTypes.func,
-    push: PropTypes.func,
-    replace: PropTypes.func,
-    go: PropTypes.func,
-    goBack: PropTypes.func,
-    goForward: PropTypes.func,
-    block: PropTypes.func,
-    listen: PropTypes.func,
-  }),
-};
-App.defaultProps = {
-  history: hist,
-};
-
-export default App;
+export default withRouter(App);
