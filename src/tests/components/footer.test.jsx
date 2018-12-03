de58477a0b4d41c2a58856da6092af0c57920bfb
Jimmy Zhen <jzhen@stanford.edu>
2018-12-03 10:11:40 -0800

    Changed the 'loggedIn' state to be named 'isAuthenticated' and some formatting changes

--- src/tests/components/footer.test.jsx
+++ src/tests/components/footer.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme, { mount } from 'enzyme';
-import { Footer } from '../../components/footer';
+import Footer from '../../components/footer';
 
 Enzyme.configure({ adapter: new Adapter() });
 const testUser = require('../../testData/testUser');
@@ -11,17 +11,23 @@ const footerActions = {
   onLogOut: jest.fn(),
 };
 const defaultMountFooter = mount(<Footer {...footerActions} />);
-const loggedInMountFooter = mount(<Footer user={testUser} loggedIn {...footerActions} />);
+const loggedInMountFooter = mount(
+  <Footer user={testUser} isAuthenticated {...footerActions} />
+);
 
 describe('Footer', () => {
   test('Has submitter login button by default', () => {
-    expect(defaultMountFooter.props().loggedIn).toBeFalsy();
-    expect(defaultMountFooter.find('.logInOutBtn').text()).toBe('Submitter Login');
+    expect(defaultMountFooter.props().isAuthenticated).toBeFalsy();
+    expect(defaultMountFooter.find('.logInOutBtn').text()).toBe(
+      'Submitter Login'
+    );
   });
 
   test('Has [username] logout button if logged in', () => {
-    expect(loggedInMountFooter.props().loggedIn).toBeTruthy();
-    expect(loggedInMountFooter.find('.logInOutBtn').text()).toMatch(new RegExp(testUser.name));
+    expect(loggedInMountFooter.props().isAuthenticated).toBeTruthy();
+    expect(loggedInMountFooter.find('.logInOutBtn').text()).toMatch(
+      new RegExp(testUser.name)
+    );
     expect(loggedInMountFooter.find('.logInOutBtn').text()).toMatch('Logout');
   });
 });
