afb9f2c47d7e315fa50d0a07d93adbbdf6768dbc
Jimmy Zhen <jzhen@stanford.edu>
2018-12-03 10:10:41 -0800

    Changed the 'loggedIn' state to be named 'isAuthenticated'

--- src/stories/footer.stories.jsx
+++ src/stories/footer.stories.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { Footer } from '../components/footer';
+import Footer from '../components/footer';
 
 const loggedOutState = {
-  loggedIn: false,
+  isAuthenticated: false,
   onLogIn: action('Logging In'),
   onLogOut: action('Logging Out'),
 };
+
 const loggedInState = {
-  loggedIn: true,
+  isAuthenticated: true,
   user: {
     name: 'Test User',
   },
   onLogIn: action('Logging In'),
   onLogOut: action('Logging Out'),
 };
+
 storiesOf('Footer', module)
   .add('default', () => <Footer {...loggedOutState} />)
   .add('Logged in', () => <Footer {...loggedInState} />);
