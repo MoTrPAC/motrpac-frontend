7d0d037b9e47836675c001ea69aa5a99f89e0f2e
Jimmy Zhen <jzhen@stanford.edu>
2018-12-19 14:40:07 -0800

    Revert to initial configurations

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer, { defaultRootState } from './reducers';
-import History from './history';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './main.css';
 
-// TODO: Before production remove redux devtools extension javascript
-const store = createStore(
-  rootReducer,
-  defaultRootState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-render(
-  <Provider store={store}>
-    <Router history={History}>
-      <Route component={App} />
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
