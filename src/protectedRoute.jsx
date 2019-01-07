a7b4e72da69985dfda04a5161324cf53519ce0f9
Jimmy Zhen <jzhen@stanford.edu>
2019-01-07 11:42:34 -0800

    Updated component logic

--- src/protectedRoute.jsx
+++ src/protectedRoute.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const ProtectedRoute = ({component: Component, isAuthenticated, ...rest }) => (
-  <Route {...rest} render={(props) => (
-    isAuthenticated ?
-      <Component {...props} />
-      : <Redirect to='/login' />
-    )}
-  />
-);
+export function ProtectedRoute({ component: Component, isAuthenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        isAuthenticated ?
+          <Component {...props} />
+          :
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+      )}
+    />
+  );
+}
 
 ProtectedRoute.propTypes = {
   isAuthenticated: PropTypes.bool,
