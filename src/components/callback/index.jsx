ccfc50568159288445fae3996ca6d3d75c3eaa65
Jimmy Zhen <jzhen@stanford.edu>
2019-01-07 11:45:31 -0800

    Updated component with correct props and changed it to functional component

--- src/components/callback/index.jsx
+++ src/components/callback/index.jsx
@@ -1,32 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import actions from '../../actions';
 
-class Callback extends Component {
-  componentDidMount() {
-    const { location, handleAuthCallback } = this.props;
-    // Handle authentication if expected values are in the URL.
-    if (/access_token|id_token|error/.test(location.hash)) {
-      handleAuthCallback();
-    } else {
-      throw new Error('Invalid callback URL.');
-    }
+export function Callback({
+  history,
+  location,
+  message,
+  handleAuthCallback,
+}) {
+  // Handle authentication if expected values are in the URL.
+  if (/access_token|id_token|error/.test(location.hash)) {
+    handleAuthCallback();
+    // history.push('/dashboard');
+    return <Redirect to="/dashboard" />
   }
 
-  render() {
-    const { authError } = this.props.auth;
-    const message = authError || 'Authenticating...';
+  const callbackMsg = message || 'Authenticating...';
 
-    return (
-      <div className="authLoading">
-        <span className="oi oi-shield" />
-        <h3>{message}</h3>
-      </div>
-    );
-  }
+  return (
+    <div className="authLoading">
+      <span className="oi oi-shield" />
+      <h3>{callbackMsg}</h3>
+    </div>
+  );
+
 }
 
-export default connect(
-  state => state,
-  actions
-)(Callback);
+Callback.propTypes = {
+  history: PropTypes.object,
+  location: PropTypes.object,
+  message: PropTypes.string,
+  handleAuthCallback: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  message: state.auth.message
+});
+
+const mapDispatchToProps = dispatch => ({
+  handleAuthCallback: () => dispatch(actions.handleAuthCallback())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Callback);
