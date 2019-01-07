a18b75600784c056923651e17bb3129305261ba9
Jimmy Zhen <jzhen@stanford.edu>
2019-01-07 11:26:07 -0800

    Updated Auth0 class component
    1. Removed 'audience' config (not needed).
    2. Removed 'getAccessToken' method (not needed).
    3. Saving 'idTokenPayload' (returned by Auth0) to localStorage.
    4. Updated 'getProfile' method to retrieve 'id_token_payload' from localStorage.

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -5,11 +5,10 @@ export default class Auth {
   constructor() {
     this.auth0 = new auth0.WebAuth({
       domain: AUTH_CONFIG.domain,
-      audience: AUTH_CONFIG.audience,
       clientID: AUTH_CONFIG.clientId,
       redirectUri: AUTH_CONFIG.callbackUrl,
       responseType: 'token id_token',
-      scope: 'openid profile user_metadata',
+      scope: 'openid profile',
     });
 
     this.login = this.login.bind(this);
@@ -18,7 +17,6 @@ export default class Auth {
     this.isAuthenticated = this.isAuthenticated.bind(this);
     this.setSession = this.setSession.bind(this);
     this.getProfile = this.getProfile.bind(this);
-    this.getAccessToken = this.getAccessToken.bind(this);
   }
 
   login() {
@@ -29,6 +27,7 @@ export default class Auth {
     // Clear access token and ID token from local storage
     localStorage.removeItem('access_token');
     localStorage.removeItem('id_token');
+    localStorage.removeItem('id_token_payload');
     localStorage.removeItem('expires_at');
   }
 
@@ -39,6 +38,7 @@ export default class Auth {
     );
     localStorage.setItem('access_token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('id_token_payload', JSON.stringify(authResult.idTokenPayload));
     localStorage.setItem('expires_at', expiresAt);
   }
 
@@ -49,7 +49,7 @@ export default class Auth {
         console.log('Could not authenticate');
         cb(err);
       } else if (authResult && authResult.accessToken && authResult.idToken) {
-        console.log('Authenticated');
+        console.log(authResult);
         this.setSession(authResult);
         cb(null, authResult);
       } else {
@@ -63,6 +63,7 @@ export default class Auth {
     if (
       !localStorage.getItem('access_token') ||
       !localStorage.getItem('id_token') ||
+      !localStorage.getItem('id_token_payload') ||
       !localStorage.getItem('expires_at')
     ) {
       return false;
@@ -72,25 +73,19 @@ export default class Auth {
     return new Date().getTime() < expiresAt;
   }
 
-  getAccessToken() {
-    const accessToken = localStorage.getItem('access_token');
-
-    if (!accessToken) {
-      throw new Error('No access token found');
-    }
-    return accessToken;
-  }
-
   getProfile(cb) {
-    const accessToken = this.getAccessToken();
-
-    this.auth0.client.userInfo(accessToken, (err, profile) => {
-      if (err) {
-        cb(err);
-        return;
-      }
-
-      cb(null, profile);
-    });
+    const idTokenPayload = JSON.parse(localStorage.getItem('id_token_payload'));
+    if (!idTokenPayload) {
+      throw new Error('No id token payload found');
+    }
+    const profile = {
+      name: idTokenPayload.name,
+      nickname: idTokenPayload.nickname,
+      email: idTokenPayload.email,
+      picture: idTokenPayload.picture,
+      user_metadata: idTokenPayload.user_metadata,
+      app_metadata: idTokenPayload.app_metadata
+    }
+    cb(null, profile);
   }
 }
