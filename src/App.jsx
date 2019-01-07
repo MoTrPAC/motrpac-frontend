7e1aebf9758412fb5cdf3f2209a09a930c7301ae
Jimmy Zhen <jzhen@stanford.edu>
2019-01-07 11:50:35 -0800

    Updated and simplified routes in the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import 'bootstrap';
-import configureStore from './configureStore';
-import ProtectedRoute from './protectedRoute';
 import NavbarConnected from './components/navbar';
-import Footer from './components/footer';
+import FooterConnected from './components/footer';
 import LandingPageConnected from './components/landingPage';
 import Dashboard from './components/dashboard';
 import UploadScreenConnected from './components/uploadScreen';
 import LinkoutPageConnected from './components/linkoutPage';
 import AnalysisHomePageConnected from './components/analysisHomePage';
 import DownloadPageConnected from './components/downloadPage';
-import Callback from './components/callback';
+import CallbackConnected from './components/callback';
 
-const store = configureStore();
-const supportsHistory = 'pushState' in window.history;
-
-function App() {
-  const { isAuthenticated } = true;
-
+function App({ history }) {
   return (
-    <Provider store={store}>
-      <BrowserRouter forceRefresh={!supportsHistory}>
-        <div className="App">
-          <header>
-            <NavbarConnected isAuthenticated={isAuthenticated} />
-          </header>
-          <Switch>
-            <Route path="/" exact component={LandingPageConnected} />
-            <Route path="/callback" component={Callback} />
-            <ProtectedRoute path="/dashboard" component={Dashboard} />
-            <ProtectedRoute path="/upload" component={UploadScreenConnected} />
-            <Route path="/external-links" component={LinkoutPageConnected} />
-            <ProtectedRoute path="/analysis/:subjectType" component={AnalysisHomePageConnected} />
-          </Switch>
-          <Footer isAuthenticated={isAuthenticated} />
-            <Route path="/download" component={DownloadPageConnected} />
-            <Route path="/analysis/:subjectType" component={AnalysisHomePageConnected} />
-          </div>
-          <FooterConnected />
-          <AuthLoadingConnected />
-        </div>
-      </BrowserRouter>
-    </Provider>
+    <div className="App">
+      <header>
+        <NavbarConnected />
+      </header>
+      <div className="componentHolder">
+        <Switch>
+          <Route path="/callback" component={CallbackConnected} history={history} />
+          <Route path="/" exact component={LandingPageConnected} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/upload" component={UploadScreenConnected} />
+          <Route path="/external-links" component={LinkoutPageConnected} />
+          <Route path="/download" component={DownloadPageConnected} />
+          <Route path="/analysis/:subjectType" component={AnalysisHomePageConnected} />
+        </Switch>
+      </div>
+      <FooterConnected history={history} />
+    </div>
   );
 }
 
-export default App;
+App.propTypes = {
+  history: PropTypes.object
+};
+
+export default withRouter(App);
