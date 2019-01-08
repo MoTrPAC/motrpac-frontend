231412bc871ac1a7f8b40fafd3c4ffc30977de63
Jimmy Zhen <jzhen@stanford.edu>
2019-01-08 14:32:46 -0800

    Added/updated test assertions

--- src/tests/reducers/authReducer.test.js
+++ src/tests/reducers/authReducer.test.js
@@ -1,30 +1,58 @@
 import { AuthReducer, defaultAuthState } from '../../reducers/authReducer';
 
-const testUser = require('../../testData/testUser');
+const data = require('../../testData/testUser');
+
+const loginRequestState = {
+  ...defaultAuthState,
+  isFetching: true,
+  isAuthenticated: false,
+};
 
 const loggedInState = {
   ...defaultAuthState,
-  loggedIn: true,
-  user: testUser,
+  isAuthenticated: true,
+  payload: data,
+};
+
+const receiveProfileState = {
+  ...defaultAuthState,
+  isAuthenticated: true,
+  profile: data,
+};
+
+const loginRequestAction = {
+  type: 'LOGIN_REQUEST',
 };
 
 const loginSuccessAction = {
   type: 'LOGIN_SUCCESS',
-  user: testUser,
+  payload: data,
 };
+
+const receiveProfileAction = {
+  type: 'PROFILE_RECEIVE',
+  profile: data,
+};
+
 const logoutAction = {
-  type: 'LOGOUT',
+  type: 'LOGOUT_SUCCESS',
 };
 
 describe('Authentication Reducer', () => {
   test('Return initial state if no action', () => {
     expect(AuthReducer(defaultAuthState, {})).toEqual(defaultAuthState);
   });
+  test('Login request returns authentication fetching state', () => {
+    expect(AuthReducer(defaultAuthState, loginRequestAction)).toEqual(loginRequestState);
+  });
   test('Succesful login with user returns correct logged in state', () => {
-    expect(AuthReducer(defaultAuthState, loginSuccessAction)).toEqual(loggedInState);
+    expect(AuthReducer(loginRequestState, loginSuccessAction)).toEqual(loggedInState);
+  });
+  test('Authenticated user returns profile in state', () => {
+    expect(AuthReducer(loggedInState, receiveProfileAction)).toEqual(receiveProfileState);
   });
   test('Logging out returns default authentication state', () => {
-    expect(AuthReducer(loggedInState, logoutAction)).toEqual(defaultAuthState);
+    expect(AuthReducer(receiveProfileState, logoutAction)).toEqual(defaultAuthState);
     expect(AuthReducer(defaultAuthState, logoutAction)).toEqual(defaultAuthState);
   });
 });
