088b4ffbf63355a512079292793ba5f17f9497b9
Jimmy Zhen <jzhen@stanford.edu>
2019-01-09 00:04:31 -0800

    Moved <Provider> and <Router> back to <App /> component
    To address unit testing failures

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './configureStore';
 import 'bootstrap';
+import History from './history';
 import NavbarConnected from './components/navbar';
 import FooterConnected from './components/footer';
 import LandingPageConnected from './components/landingPage';
@@ -11,25 +14,31 @@ import AnalysisHomePageConnected from './components/analysisHomePage';
 import DownloadPageConnected from './components/downloadPage';
 import CallbackConnected from './components/callback';
 
-function App() {
+const store = configureStore();
+
+function App({ history = History }) {
   return (
-    <div className="App">
-      <header>
-        <NavbarConnected />
-      </header>
-      <div className="componentHolder">
-        <Switch>
-          <Route path="/callback" component={CallbackConnected} />
-          <Route path="/" exact component={LandingPageConnected} />
-          <Route path="/dashboard" component={DashboardConnected} />
-          <Route path="/upload" component={UploadScreenConnected} />
-          <Route path="/external-links" component={LinkoutPageConnected} />
-          <Route path="/download" component={DownloadPageConnected} />
-          <Route path="/analysis/:subjectType" component={AnalysisHomePageConnected} />
-        </Switch>
-      </div>
-      <FooterConnected />
-    </div>
+    <Provider store={store}>
+      <Router history={history}>
+        <div className="App">
+          <header>
+            <NavbarConnected />
+          </header>
+          <div className="componentHolder">
+            <Switch>
+              <Route path="/callback" component={CallbackConnected} />
+              <Route path="/" exact component={LandingPageConnected} />
+              <Route path="/dashboard" component={DashboardConnected} />
+              <Route path="/upload" component={UploadScreenConnected} />
+              <Route path="/external-links" component={LinkoutPageConnected} />
+              <Route path="/download" component={DownloadPageConnected} />
+              <Route path="/analysis/:subjectType" component={AnalysisHomePageConnected} />
+            </Switch>
+          </div>
+          <FooterConnected />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import configureStore from './configureStore';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './main.css';
 
-const store = configureStore();
-
-render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
