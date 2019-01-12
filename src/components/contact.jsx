01ce87d96fe88de4ce51b724dc5aef89c303ee1f
Jimmy Zhen <jzhen@stanford.edu>
2019-01-11 16:28:48 -0800

    :tada: :construction: Initial commit for work-in-progress 'Contact' component

---
+++ src/components/contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div className="container Contact">
+      <div className="row align-items-center">
+        <div className="col-12 col-md-6 align-self-center">
+          <h2 className="light">Coming soon!</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
