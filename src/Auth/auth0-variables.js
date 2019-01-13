f4a675dedde3a42caf07c8de69468a64db3f01d1
Jimmy Zhen <jzhen@stanford.edu>
2019-01-13 15:38:02 -0800

    :star2: Using official MoTrPAC Project Auth0 account

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
-  domain: 'hypeway.auth0.com',
-  clientId: 'a8bPMoRQMu_3oe1Lzj0JKmUSakiOXlOp',
+  domain: 'motrpac-project.auth0.com',
+  clientId: '4dUo4JxLlZvCtFVCw21Nh0ZRKyznluAZ',
   callbackUrl: 'http://localhost:3000/callback'
 };
