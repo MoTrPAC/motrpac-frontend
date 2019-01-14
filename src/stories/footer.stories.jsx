41bd1372c20137cce479d42f0cc1061fd3a3230d
Jimmy Zhen <jzhen@stanford.edu>
2019-01-14 14:04:55 -0800

    :book: Update Storybook behaviors due to Auth0 implementation

--- src/stories/footer.stories.jsx
+++ src/stories/footer.stories.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import Footer from '../components/footer';
+import { Footer } from '../components/footer';
 
 const loggedOutState = {
   isAuthenticated: false,
-  onLogIn: action('Logging In'),
-  onLogOut: action('Logging Out'),
+  login: action('Logging In'),
+  logout: action('Logging Out'),
 };
 
 const loggedInState = {
   isAuthenticated: true,
-  user: {
+  profile: {
     name: 'Test User',
+    picture: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y',
+    user_metadata: {
+      name: 'Test User',
+      givenName: 'TestUser',
+    },
   },
-  onLogIn: action('Logging In'),
-  onLogOut: action('Logging Out'),
+  login: action('Logging In'),
+  logout: action('Logging Out'),
 };
 
 storiesOf('Footer', module)
