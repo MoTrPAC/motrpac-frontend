a0bdd6fb82539455607cfa6f86cef3e94bfe069e
Jimmy Zhen <jzhen@stanford.edu>
2019-01-15 02:41:03 -0800

    Replaced instances of 'data' variable name with 'testUser' and fixed test error

--- src/tests/components/footer.test.jsx
+++ src/tests/components/footer.test.jsx
@@ -4,7 +4,8 @@ import Enzyme, { mount } from 'enzyme';
 import { Footer } from '../../components/footer';
 
 Enzyme.configure({ adapter: new Adapter() });
-const data = require('../../testData/testUser');
+
+const testUser = require('../../testData/testUser');
 
 const footerActions = {
   login: jest.fn(),
@@ -12,21 +13,21 @@ const footerActions = {
 };
 const defaultMountFooter = mount(<Footer {...footerActions} />);
 const loggedInMountFooter = mount(
-  <Footer profile={data} isAuthenticated {...footerActions} />
+  <Footer profile={testUser} isAuthenticated {...footerActions} />,
 );
 
 describe('Footer', () => {
   test('Has submitter login button by default', () => {
     expect(defaultMountFooter.props().isAuthenticated).toBeFalsy();
     expect(defaultMountFooter.find('.logInOutBtn').text()).toBe(
-      'Submitter Login'
+      'Submitter Login',
     );
   });
 
   test('Has [username] logout button if logged in', () => {
     expect(loggedInMountFooter.props().isAuthenticated).toBeTruthy();
     expect(loggedInMountFooter.find('.logInOutBtn').text()).toMatch(
-      new RegExp(data.name)
+      new RegExp(testUser.user_metadata.givenName),
     );
     expect(loggedInMountFooter.find('.logInOutBtn').text()).toMatch('Logout');
   });
