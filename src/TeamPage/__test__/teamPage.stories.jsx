0f17a02533199128c26243bd2f1455a42d6b9fdd
Samir Akre <sakre@ucdavis.edu>
2019-01-22 11:08:25 -0800

    Changed teamPage story navbar and footer properties to reflect recent changes to authentication process

--- src/TeamPage/__test__/teamPage.stories.jsx
+++ src/TeamPage/__test__/teamPage.stories.jsx
@@ -5,21 +5,23 @@ import TeamPage from '../teamPage';
 import { Navbar } from '../../Navbar/navbar';
 import { Footer } from '../../Footer/footer';
 
+const testUser = require('../../testData/testUser');
+
 const footerActions = {
-  onLogIn: action('logging in'),
-  onLogOut: action('logging out'),
+  login: action('logging in'),
+  logout: action('logging out'),
 };
 
 storiesOf('Team Page', module)
   .addDecorator(story => (
     <div className="App">
       <header>
-        <Navbar loggedIn />
+        <Navbar isAuthenticated />
       </header>
       <div className="componentHolder">
         {story()}
       </div>
-      <Footer loggedIn {...footerActions} user={{ name: 'Test User', site: 'CAS' }} />
+      <Footer isAuthenticated {...footerActions} profile={testUser} />
     </div>
 
   ))
