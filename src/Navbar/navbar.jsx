4ce5eef32f25c62d71b52183adcf8a294756ec81
Jimmy Zhen <jzhen@stanford.edu>
2019-02-15 14:10:42 -0800

    :nail_care: Redesign navbar with the addition of logo and a resizable header upon scrolling

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import MoTrPAClogo from '../assets/logo-motrpac.png';
 
 export function Navbar({ isAuthenticated = false }) {
+  const scrollFunction = () => {
+    if (document.body.scrollTop >= 30 || document.documentElement.scrollTop >= 30) {
+      document.querySelector('.navbar-brand img').classList.add('resized');
+    } else {
+      document.querySelector('.navbar-brand img').classList.remove('resized');
+    }
+  }
+
+  window.addEventListener('scroll', scrollFunction);
+
   const loggedInNavItems = (
     <React.Fragment>
       <li className="nav-item navItem"><Link to="/dashboard" className="nav-link">Dashboard</Link></li>
@@ -24,13 +35,10 @@ export function Navbar({ isAuthenticated = false }) {
     </React.Fragment>
   );
   const navbar = (
-    <nav className="navbar navbar-expand-lg navbar-dark">
+    <nav className="navbar navbar-expand-lg fixed-top navbar-light">
       <div className="container">
-        <Link to="/" className="navbar-brand siteTitle">
-        MoTrPAC
-          <span className="light">
-          &nbsp; Data Hub
-          </span>
+        <Link to="/" className="navbar-brand header-logo">
+          <img default src={MoTrPAClogo} alt="MoTrPAC Data Hub" />
         </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
