31fa656c0e8294fbe0d3bc1557a3de0814d7a236
Jimmy Zhen <jzhen@stanford.edu>
2019-03-01 17:52:45 -0800

    :iphone: Make footer responsive

--- src/Footer/footer.jsx
+++ src/Footer/footer.jsx
@@ -58,43 +58,49 @@ export function Footer({
   // TODO: Find out how to best do error handling
   return (
     <footer className="footer">
-      <div className="container footer-nav d-md-flex h-100 align-items-center">
-        <Link to="/" className="navbar-brand footer-logo flex-grow-1">
-          <img default src={MoTrPAClogo} alt="MoTrPAC Data Hub" />
-        </Link>
-        <ul className="nav justify-content-end">
-          <li className="nav-item navItem"><Link to="/" className="nav-link">Home</Link></li>
-          <li className="nav-item navItem"><Link to="/team" className="nav-link">About Us</Link></li>
-          <li className="nav-item navItem"><Link to="/contact" className="nav-link">Contact Us</Link></li>
-          <li className="nav-item navItem">
-            <LoginButton />
-          </li>
-        </ul>
-      </div>
+      {!isAuthenticated && (
+        <div className="container footer-nav">
+          <div className="row align-items-end">
+            <div className="col-12 col-lg-4 footer-nav-logo">
+              <Link to="/" className="navbar-brand footer-logo">
+                <img default src={MoTrPAClogo} alt="MoTrPAC Data Hub" />
+              </Link>
+            </div>
+            <div className="col-12 col-lg-8 row justify-content-end footer-nav-items">
+              <ul className="nav">
+                <li className="nav-item navItem"><Link to="/" className="nav-link">Home</Link></li>
+                <li className="nav-item navItem"><Link to="/team" className="nav-link">About Us</Link></li>
+                <li className="nav-item navItem"><Link to="/contact" className="nav-link">Contact Us</Link></li>
+                <li className="nav-item navItem">
+                  <LoginButton />
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="footer-disclaimers">
-        <div className="container h-100 align-items-center">
-          <div className="footer-content">
-            <span>
-              MoTrPAC Data Hub is designed and maintained by the MoTrPAC BioInformatics
-              Center at&nbsp;
-              <a href="https://www.stanford.edu/" target="_blank" rel="noopener noreferrer">
-                Stanford University
-              </a>
-              <span>.&nbsp;</span>
-            </span>
-            <span>
-              Funded by the&nbsp;
-              <a href="https://commonfund.nih.gov/" target="_blank" rel="noopener noreferrer">
-                NIH Common Fund
-              </a>
-              <span>.</span>
-            </span>
-          </div>
-          <div className="footer-content">
-            &#169;
-            {getCopyrightYear()}
-            &nbsp;Stanford University
-          </div>
+        <div className="footer-content">
+          <span>
+            MoTrPAC Data Hub is designed and maintained by the MoTrPAC BioInformatics
+            Center at&nbsp;
+            <a href="https://www.stanford.edu/" target="_blank" rel="noopener noreferrer">
+              Stanford University
+            </a>
+            <span>.&nbsp;</span>
+          </span>
+          <span>
+            Funded by the&nbsp;
+            <a href="https://commonfund.nih.gov/" target="_blank" rel="noopener noreferrer">
+              NIH Common Fund
+            </a>
+            <span>.</span>
+          </span>
+        </div>
+        <div className="footer-content">
+          &#169;
+          {getCopyrightYear()}
+          &nbsp;Stanford University
         </div>
       </div>
     </footer>
