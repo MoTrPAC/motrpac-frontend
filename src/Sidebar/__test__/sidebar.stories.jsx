bfa05618bc215789568c16612647c72bc68c0bd8
Jimmy Zhen <jzhen@stanford.edu>
2019-03-03 23:24:11 -0800

    Updated storybook with no authentication state

--- src/Sidebar/__test__/sidebar.stories.jsx
+++ src/Sidebar/__test__/sidebar.stories.jsx
@@ -3,5 +3,4 @@ import { storiesOf } from '@storybook/react';
 import { Sidebar } from '../sidebar';
 
 storiesOf('Sidebar', module)
-  .add('default', () => <Sidebar />)
-  .add('Logged in', () => <Sidebar isAuthenticated />);
+  .add('default', () => <Sidebar />);
