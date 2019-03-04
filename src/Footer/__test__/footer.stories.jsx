8c323761fdf57e0672c09afe5c47ddc2c1b611aa
Jimmy Zhen <jzhen@stanford.edu>
2019-03-03 23:01:21 -0800

    Updated storybook components due to UI changes

--- src/Footer/__test__/footer.stories.jsx
+++ src/Footer/__test__/footer.stories.jsx
@@ -3,19 +3,13 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { Footer } from '../footer';
 
-const testUser = require('../../testData/testUser');
-
 const loggedOutState = {
   isAuthenticated: false,
   login: action('Logging In'),
-  logout: action('Logging Out'),
 };
 
 const loggedInState = {
   isAuthenticated: true,
-  profile: testUser,
-  login: action('Logging In'),
-  logout: action('Logging Out'),
 };
 
 storiesOf('Footer', module)
--- src/Navbar/__test__/navbar.stories.jsx
+++ src/Navbar/__test__/navbar.stories.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { Navbar } from '../navbar';
 
+const testUser = require('../../testData/testUser');
+
+const loggedOutState = {
+  isAuthenticated: false,
+};
+
+const loggedInState = {
+  isAuthenticated: true,
+  profile: testUser,
+  logout: action('Logging Out'),
+};
+
 storiesOf('Navbar', module)
-  .add('default', () => <Navbar />)
-  .add('Logged in', () => <Navbar isAuthenticated />);
+  .add('default', () => <Navbar {...loggedOutState} />)
+  .add('Logged in', () => <Navbar {...loggedInState} />);
