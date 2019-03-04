4d111a768e97ba59fe6fa1722dd545b13b3c7b9a
Jimmy Zhen <jzhen@stanford.edu>
2019-03-03 23:23:36 -0800

    Updated unit text with no authentication state

--- src/Sidebar/__test__/sidebar.test.jsx
+++ src/Sidebar/__test__/sidebar.test.jsx
@@ -6,20 +6,12 @@ import { Sidebar } from '../sidebar';
 Enzyme.configure({ adapter: new Adapter() });
 
 const shallowDefaultSidebar = shallow(<Sidebar />);
-const shallowLoggedInSidebar = shallow(<Sidebar isAuthenticated />);
 
-const loggedInItems = ['Dashboard', 'Analysis', 'Methods', 'Data'];
 const navItems = ['Dashboard', 'Analysis', 'Methods', 'Data'];
 
 describe('Sidebar', () => {
-  test('Login status undefined, does not show logged in nav items', () => {
+  test('Logged In sidebar has expected elements', () => {
     shallowDefaultSidebar.find('.navLink').forEach((navLink) => {
-      expect(loggedInItems.indexOf(navLink.text())).toEqual(-1);
-    });
-  });
-
-  test('Logged In navbar has expected elements', () => {
-    shallowLoggedInSidebar.find('.navLink').forEach((navLink) => {
       expect(navItems.indexOf(navLink.text())).not.toBe(-1);
     });
   });
