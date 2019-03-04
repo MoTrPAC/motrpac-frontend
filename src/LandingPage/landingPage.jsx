036d1f1277eff339221dc4e1ff29522628693499
Jimmy Zhen <jzhen@stanford.edu>
2019-03-03 18:51:45 -0800

    Inline images on ladning page are changed to be background images controlled via CSS media queries
    Also made some tweaks in particle.js properties.

--- src/LandingPage/landingPage.jsx
+++ src/LandingPage/landingPage.jsx
@@ -1,70 +1,67 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Particles from 'react-particles-js';
-import LogoAnimation from '../assets/LandingPageGraphics/LogoAnimation.gif';
-import LayerRunner from '../assets/LandingPageGraphics/Layer_Runner_dark.png';
-import HealthyHeart from '../assets/LandingPageGraphics/Infographic_Healthy_Heart.png';
 
 export function LandingPage() {
-  // TODO: Update email and email link for help requests
-
   return (
     <div className="marketing">
       <main>
-        <div className="d-md-flex justify-content-center w-100 h-100">
-          <div className="container splash d-md-flex h-100 align-items-center">
-            <div className="col-md-8 motrpac-logo-animation align-self-center">
-              <img src={LogoAnimation} alt="MoTrPAC Infographic" />
-            </div>
-            <div className="col-md-4 motrpac-tag-line align-self-center">
-              <h3>
-                <em>Understanding</em>
-                at the molecular level how activity makes us healthier
-              </h3>
-            </div>
+        <div className="container hero h-100">
+          <div className="row motrpac-tag-line h-100">
+            <h3>
+              <em>Understanding</em>
+              at the molecular level how activity makes us healthier
+            </h3>
           </div>
         </div>
       </main>
       <section>
-        <div className="d-md-flex justify-content-center w-100 h-100 about-motrpac" id="about-motrpac">
+        <div className="about-motrpac" id="about-motrpac">
           <Particles
+            width="100%"
+            height="480px"
             className="position-absolute particles-wrapper"
             params={{
               particles: {
-                number: { value: 100 },
+                number: { value: 90, density: { enable: true, value_area: 500 } },
                 color: { value: '#000000' },
                 size: { value: 4.5 },
                 line_linked: { color: '#000000' },
+                move: { speed: 7 },
               },
               interactivity: {
                 events: {
-                  onhover: { enable: true, mode: 'repulse' },
+                  onhover: { enable: true, mode: 'grab' },
+                  onclick: { enable: true, mode: 'push' },
+                  modes: {
+                    grab: { distance: 140, line_linked: { opacity: 1 } },
+                    push: { particles_nb: 4 },
+                  },
                 },
               },
             }}
           />
-          <div className="container featurette d-md-flex h-100 align-items-center">
-            <div className="col-md-6 align-self-center">
-              <h3>About MoTrPAC</h3>
-              <p>
-                Molecular Transducers of Physical Activity Consortium is a national
-                research consortium designed to discover and perform preliminary
-                characterization of the range of molecular transducers
-                (the "molecular map") that underlie the effects of physical activity
-                in humans.
-              </p>
-              <a href="https://motrpac.org/" className="btn btn-dark" role="button" target="_new">READ MORE</a>
+          <div className="container featurette h-100">
+            <div className="row featurette-wrapper h-100">
+              <div className="content">
+                <h3>About MoTrPAC</h3>
+                <p>
+                  Molecular Transducers of Physical Activity Consortium is a national
+                  research consortium designed to discover and perform preliminary
+                  characterization of the range of molecular transducers
+                  (the "molecular map") that underlie the effects of physical activity
+                  in humans.
+                </p>
+                <a href="https://motrpac.org/" className="btn btn-dark" role="button" target="_new">READ MORE</a>
+              </div>
             </div>
           </div>
         </div>
       </section>
       <section>
-        <div className="d-md-flex justify-content-center w-100 h-100 multi-omics">
-          <div className="container featurette d-md-flex h-100 align-items-center">
-            <div className="col-md-6 d-md-flex justify-content-center h-100 align-items-center">
-              <img src={LayerRunner} alt="Runner on data layer" />
-            </div>
-            <div className="col-md-6 align-self-center">
+        <div className="container featurette multi-omics" id="multi-omics">
+          <div className="row featurette-wrapper h-100">
+            <div className="content">
               <h3>Multi-Omics</h3>
               <p>
                 MoTrPAC encourages investigators to develop ancillary studies (AS) in
@@ -77,32 +74,31 @@ export function LandingPage() {
         </div>
       </section>
       <section>
-        <div className="d-md-flex justify-content-center w-100 h-100 interrelated-components" id="interrelated-components">
-          <div className="container featurette d-md-flex h-100 align-items-center">
-            <div className="col-md-6 align-self-center">
-              <h3>Interrelated Components</h3>
-              <p>
-                Consisting of Clinical Centers, Preclinical Animal Study Sites, Chemical Analysis
-                Sites, Bioinformatics Center, Consortium Coordinating Center.
-              </p>
-              <a href="https://commonfund.nih.gov/MolecularTransducers/overview#ClinicalCenter" className="btn btn-success" role="button" target="_new">READ MORE</a>
-            </div>
-            <div className="col-md-6 d-md-flex justify-content-center h-100 align-items-center">
-              <img src={HealthyHeart} alt="Healthy Heart" />
+        <div className="interrelated-components" id="interrelated-components">
+          <div className="container featurette h-100">
+            <div className="row featurette-wrapper h-100">
+              <div className="content">
+                <h3>Interrelated Components</h3>
+                <p>
+                  Consisting of Clinical Centers, Preclinical Animal Study Sites, Chemical Analysis
+                  Sites, Bioinformatics Center, Consortium Coordinating Center.
+                </p>
+                <a href="https://commonfund.nih.gov/MolecularTransducers/overview#ClinicalCenter" className="btn btn-success" role="button" target="_new">READ MORE</a>
+              </div>
             </div>
           </div>
         </div>
       </section>
       <section>
-        <div className="d-md-flex flex-md-equal w-100 data-policies">
-          <div className="container featurette d-md-flex">
-            <div className="p-2 col-md access-data-info">
+        <div className="container featurette data-policies">
+          <div className="row">
+            <div className="p-2 col-12 col-md-6 access-data-info">
               <h5>Accessing Data: </h5>
               Data generated to date is not yet publicly accessible. For updates when publicly
               accessible data are available contact us at&nbsp;
               <a href="mailto:MoTrPAC-data-requests@stanford.edu" target="_new">MoTrPAC-data-requests@stanford.edu</a>
             </div>
-            <div className="p-2 col-md upload-data-info">
+            <div className="p-2 col-12 col-md-6 upload-data-info">
               <h5>Uploading Data From Study Sites:</h5>
               If you are a member of one of the sites involved with MoTrPAC please log in using
               your provided ID at the link on the bottom right of this website.  If you have issues
