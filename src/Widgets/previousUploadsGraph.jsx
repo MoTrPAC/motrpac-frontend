0d4e97f499e9b46f7fdeee1ddd15c55a13183b60
Samir Akre <sakre@ucdavis.edu>
2019-03-05 14:33:52 -0800

    removed unused variables

--- src/Widgets/previousUploadsGraph.jsx
+++ src/Widgets/previousUploadsGraph.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Bar as CBar } from 'react-chartjs-2';
-import tinycolor from 'tinycolor2';
 import PropTypes from 'prop-types';
 import colors from '../lib/colors';
 
@@ -23,28 +22,19 @@ export function countUploads(data, types) {
 
 function PreviousUploadsGraph({ previousUploads }) {
   const types = [...new Set(previousUploads.map(upload => upload.dataType))];
-  const baseColors = Object.values(colors.base_palette);
 
-  const shadeAmount = 19; // % difference in shades of fill color in bar chart
-  const borderDiff = 5; // % difference in shade between border and fill color
 
   // Pending Q.C. sorted data
   const pendingQC = previousUploads.filter(upload => upload.availability === 'Pending Q.C.');
   const pendingQCCount = countUploads(pendingQC.map(upload => upload.dataType), types);
-  const pendingQCColors = baseColors.map(color => tinycolor(color).lighten(shadeAmount).toHexString());
-  const pendingQCBorders = baseColors.map(color => tinycolor(color).lighten(shadeAmount - borderDiff).toHexString());
 
   // Internally available sorted data
   const internalAvailable = previousUploads.filter(upload => upload.availability === 'Internally Available');
   const internalAvailableCount = countUploads(internalAvailable.map(upload => upload.dataType), types);
-  const internalAvailableColors = baseColors;
-  const internalAvailableBorders = baseColors.map(color => tinycolor(color).lighten(-borderDiff).toHexString());
 
   // Publically available sorted data
   const publicAvailable = previousUploads.filter(upload => upload.availability === 'Publicly Available');
   const publicAvailableCount = countUploads(publicAvailable.map(upload => upload.dataType), types);
-  const publicAvailableColors = baseColors.map(color => tinycolor(color).lighten(-shadeAmount).toHexString());
-  const publicAvailableBorders = baseColors.map(color => tinycolor(color).lighten(-shadeAmount - borderDiff).toHexString());
 
   // Input to chartJS bar chart
   const data = {
@@ -53,25 +43,19 @@ function PreviousUploadsGraph({ previousUploads }) {
       {
         label: 'Pending Q.C.',
         data: Object.values(pendingQCCount),
-        // borderColor: pendingQCBorders,
         borderWidth: 1,
-        // backgroundColor: pendingQCColors,
         backgroundColor: colors.base_palette.accent_yellow,
       },
       {
         label: 'Internally Available',
         data: Object.values(internalAvailableCount),
-        // borderColor: internalAvailableBorders,
         borderWidth: 1,
-        // backgroundColor: internalAvailableColors,
         backgroundColor: colors.base_palette.primary_blue,
       },
       {
         label: 'Publicly Available',
         data: Object.values(publicAvailableCount),
-        // borderColor: publicAvailableBorders,
         borderWidth: 1,
-        // backgroundColor: publicAvailableColors,
         backgroundColor: colors.base_palette.accent_green,
       },
     ],
