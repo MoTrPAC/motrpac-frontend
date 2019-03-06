9c1c42aab72fa5db9845460100c346728fa5b0a6
Jimmy Zhen <jzhen@stanford.edu>
2019-03-05 16:10:51 -0800

    Added @class to sidebar nav-link icons

--- src/Sidebar/sidebar.jsx
+++ src/Sidebar/sidebar.jsx
@@ -19,14 +19,14 @@ export function Sidebar({
         <ul className="nav flex-column">
           <li className="nav-item mt-2">
             <Link to="/dashboard" className="nav-link active">
-              <span className="oi oi-home" />
-              &nbsp;Dashboard
+              <span className="oi oi-home nav-link-icon" />
+                Dashboard
             </Link>
           </li>
           <li className="nav-item">
             <Link to="/methods" className="nav-link">
-              <span className="oi oi-excerpt" />
-              &nbsp;Methods
+              <span className="oi oi-excerpt nav-link-icon" />
+                Methods
             </Link>
           </li>
         </ul>
@@ -37,14 +37,14 @@ export function Sidebar({
         <ul className="nav flex-column mb-2">
           <li className="nav-item pl-2">
             <Link to="/analysis/animal" className="nav-link">
-              <span className="icon-Animal" />
-              &nbsp;Animal
+              <span className="icon-Animal nav-link-icon" />
+                Animal
             </Link>
           </li>
           <li className="nav-item pl-2">
             <Link to="/analysis/human" className="nav-link">
-              <span className="icon-Human" />
-              &nbsp;Human
+              <span className="icon-Human nav-link-icon" />
+                Human
             </Link>
           </li>
         </ul>
@@ -55,14 +55,14 @@ export function Sidebar({
         <ul className="nav flex-column mb-2">
           <li className="nav-item pl-2">
             <Link to="/download" className="nav-link">
-              <span className="oi oi-data-transfer-download" />
-              &nbsp;Download/View Data
+              <span className="oi oi-data-transfer-download nav-link-icon" />
+                Download/View Data
             </Link>
           </li>
           <li className="nav-item pl-2">
             <Link to="/upload" className="nav-link">
-              <span className="oi oi-cloud-upload" />
-              &nbsp;Upload Data
+              <span className="oi oi-cloud-upload nav-link-icon" />
+                Upload Data
             </Link>
           </li>
         </ul>
