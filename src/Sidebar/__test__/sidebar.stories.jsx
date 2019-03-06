941b745b577956d913fb48a6bf1d70e965b23830
Jimmy Zhen <jzhen@stanford.edu>
2019-03-05 16:57:48 -0800

    Updated stories

--- src/Sidebar/__test__/sidebar.stories.jsx
+++ src/Sidebar/__test__/sidebar.stories.jsx
@@ -2,5 +2,10 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Sidebar } from '../sidebar';
 
+const loggedInState = {
+  isAuthenticated: true,
+};
+
 storiesOf('Sidebar', module)
-  .add('default', () => <Sidebar />);
+  .add('default', () => <Sidebar />)
+  .add('Logged in', () => <Sidebar {...loggedInState} />);
