7334427b8f50454af49241011a655a88d2436cf0
Jimmy Zhen <jzhen@stanford.edu>
2019-03-12 13:00:59 -0700

    Update story with proper nav action and test user data

--- src/ContactPage/__test__/contact.stories.jsx
+++ src/ContactPage/__test__/contact.stories.jsx
@@ -5,21 +5,26 @@ import { Contact } from '../contact';
 import { Navbar } from '../../Navbar/navbar';
 import { Footer } from '../../Footer/footer';
 
-const footerActions = {
-  login: action('logging in'),
+const testUser = require('../../testData/testUser');
+
+const navAction = {
   logout: action('logging out'),
+};
+
+const footerAction = {
+  login: action('logging in'),
 };
 
 storiesOf('Contact Us Page', module)
   .addDecorator(story => (
     <div className="App">
       <header>
-        <Navbar />
+        <Navbar isAuthenticated {...navAction} profile={testUser} />
       </header>
       <div className="row justify-content-center">
         {story()}
       </div>
-      <Footer {...footerActions} />
+      <Footer isAuthenticated {...footerAction} />
     </div>
   ))
   .add('Default', () => <Contact />);
