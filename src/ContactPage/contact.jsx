947cbaeeca85d6b7b2f5b45efa73e71ccabf1112
Jimmy Zhen <jzhen@stanford.edu>
2019-03-15 11:16:32 -0700

    Update Contact page content

--- src/ContactPage/contact.jsx
+++ src/ContactPage/contact.jsx
@@ -17,11 +17,28 @@ export function Contact({ isAuthenticated }) {
         <div className="page-title pt-3 pb-2 border-bottom">
           <h3>Contact Us</h3>
         </div>
-        <div className="align-items-center">
-          <p>
-            For any questions or suggestions, please contact us at&nbsp;
-            <a href="mailto:motrpac-helpdesk@lists.stanford.edu">motrpac-helpdesk@lists.stanford.edu</a>
-          </p>
+        <div className="card-deck contact-content-container">
+          <div className="card mb-4 shadow-sm">
+            <h5 className="card-header">Accessing Data</h5>
+            <div className="card-body">
+              <p className="card-text">
+                Data generated to date is not yet publicly accessible. For updates when publicly
+                accessible data are available, please contact us at&nbsp;
+                <a href="mailto:motrpac-data-requests@lists.stanford.edu" target="_new">motrpac-data-requests@lists.stanford.edu</a>
+              </p>
+            </div>
+          </div>
+          <div className="card mb-4 shadow-sm">
+            <h5 className="card-header">Uploading Data From Study Sites</h5>
+            <div className="card-body">
+              <p className="card-text">
+                If you are a member of one of the sites involved with MoTrPAC, please log in using
+                your provided ID at the link on the bottom right of this website. If you have issues
+                logging in, please contact the bioinformatic center helpdesk at&nbsp;
+                <a href="mailto:motrpac-helpdesk@lists.stanford.edu" target="_new">motrpac-helpdesk@lists.stanford.edu</a>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
