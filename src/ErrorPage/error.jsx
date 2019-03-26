77bec59f1e15dea193d8ab400378cdd222ad63d6
Jimmy Zhen <jzhen@stanford.edu>
2019-03-26 16:20:39 -0700

    Updated error page text content

--- src/ErrorPage/error.jsx
+++ src/ErrorPage/error.jsx
@@ -9,13 +9,13 @@ function ErrorPage() {
   return (
     <div className="col-md-9 col-lg-10 px-4 errorPage">
       <div className="container">
-        <div className="page-title pt-3 pb-2">
-          <h3>Access denied</h3>
+        <div className="page-title pt-5 pb-3">
+          <h3>Authorized MoTrPAC Consortia Members Only</h3>
         </div>
         <div className="contact-content-container">
           <p className="alert alert-warning">
-            You have not been authorized to access the MoTrPAC Data Hub
-            portal. Please contact the MoTrPAC Helpdesk at&nbsp;
+            At this time, access to the MoTrPAC Data Hub data resources is limited
+            to Consortia members only. Please contact the MoTrPAC Helpdesk at&nbsp;
             <a href="mailto:motrpac-helpdesk@lists.stanford.edu" target="_new">motrpac-helpdesk@lists.stanford.edu</a>
             &nbsp;and request access to the portal.
           </p>
