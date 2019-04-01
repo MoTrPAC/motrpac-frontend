9db0039df2bc875e087c6bfeb12d226ca2911fe4
Jimmy Zhen <jzhen@stanford.edu>
2019-04-01 16:51:12 -0700

    :nail_care: Increment y axis scale to be 1 or greater than the highest upload number (regardless of statues) on bar chart

--- src/Widgets/previousUploadsGraph.jsx
+++ src/Widgets/previousUploadsGraph.jsx
@@ -61,12 +61,24 @@ function PreviousUploadsGraph({ previousUploads }) {
     ],
   };
 
+  // increment highest upload number displayed on graph
+  // to be 1 or greater than highest number on chart data
+  const uploadRange = [];
+  if (data && data.datasets && data.datasets.length) {
+    data.datasets.forEach((set) => {
+      uploadRange.push(...set.data);
+    });
+  }
+  const maxScaleY = Math.max(...uploadRange);
+
   // ChartJS formatting options
   const options = {
     scales: {
       yAxes: [{
         ticks: {
+          stepSize: 1, // integer only
           beginAtZero: true,
+          suggestedMax: +maxScaleY + 1,
         },
         scaleLabel: {
           display: true,
