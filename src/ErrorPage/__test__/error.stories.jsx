518e6e397c7f18b09014fb8df42a19e68199c383
Jimmy Zhen <jzhen@stanford.edu>
2019-04-17 00:04:22 -0700

    :tada: Initial commit for error page story

---
+++ src/ErrorPage/__test__/error.stories.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { ErrorPage } from '../error';
+import { Navbar } from '../../Navbar/navbar';
+import { Footer } from '../../Footer/footer';
+
+const testUser = require('../../testData/testUser');
+
+const loginAction = {
+  login: action('logging in'),
+};
+
+storiesOf('Error Page', module)
+  .addDecorator(story => (
+    <React.Fragment>
+      <div className="App">
+        <header>
+          <Navbar {...loginAction} profile={testUser} />
+        </header>
+        <div className="row justify-content-center">
+          {story()}
+        </div>
+      </div>
+      <Footer profile={testUser} {...loginAction} />
+    </React.Fragment>
+  ))
+  .add('Default', () => <ErrorPage />);
