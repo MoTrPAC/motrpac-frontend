43e47329756dabb6e91739cbdf622fbb03df0b90
Jimmy Zhen <jzhen@stanford.edu>
2019-04-16 17:55:00 -0700

    Added 'profile' state

--- src/Sidebar/__test__/sidebar.stories.jsx
+++ src/Sidebar/__test__/sidebar.stories.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Sidebar } from '../sidebar';
 
+const testUser = require('../../testData/testUser');
+
 const loggedInState = {
   isAuthenticated: true,
+  profile: testUser,
 };
 
 storiesOf('Sidebar', module)
