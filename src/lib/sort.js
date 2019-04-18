10a340577594daec8cff2fe66742b3d85a8a339f
Jimmy Zhen <jzhen@stanford.edu>
2019-04-17 17:41:12 -0700

    :tada: Initial commit for helper function to sort timestamp in numeric format

---
+++ src/lib/sort.js
@@ -0,0 +1,12 @@
+/**
+ * Sort list by timeStamp number (e.g. '1547241116820') in descending order
+ */
+function sortByNumber(list, field) {
+  let sortedList = [];
+  if (list.length) {
+    sortedList = list.sort((x, y) => Number.parseFloat(x[field]) !== Number.parseFloat(y[field]) ? Number.parseFloat(x[field]) > Number.parseFloat(y[field]) ? -1 : 1 : 0);
+  }
+  return sortedList;
+}
+
+export default sortByNumber;
