a9c69c19c89193523d468afd785799ee3a3653b8
Jimmy Zhen <jzhen@stanford.edu>
2019-04-17 17:46:52 -0700

    Added 'previousUploads' prop to be passed down to 'RecentUploadList' child component
    Also updated the UI for the upload file list and appended the recent upload activity list to it.

--- src/UploadPage/uploadList.jsx
+++ src/UploadPage/uploadList.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import UploadListRow from './uploadListRow';
+import RecentUploadList from './recentUploadList';
 
-function UploadList({ uploadFiles, cancelUpload }) {
+function UploadList({ uploadFiles, cancelUpload, previousUploads }) {
   // Return empty message if no uploads
   if (!uploadFiles.length) {
     return (
-      <div className="noListItems centered">
-        <h2>Upload files and they will appear here!</h2>
-      </div>
+      <React.Fragment>
+        <div className="noListItems centered">
+          <h3>Upload files and they will appear here!</h3>
+        </div>
+        <RecentUploadList previousUploads={previousUploads} />
+      </React.Fragment>
     );
   }
 
@@ -23,21 +27,35 @@ function UploadList({ uploadFiles, cancelUpload }) {
     ));
 
   return (
-    <table className="table table-hover uploadList">
-      <thead>
-        <tr>
-          <th scope="col">File Name</th>
-          <th scope="col">Status</th>
-          <th className="centered" scope="col">Upload Successful</th>
-          <th className="centered" scope="col">Cancel Upload</th>
-        </tr>
-      </thead>
-      <tbody>
-        {listObj}
-      </tbody>
-    </table>
+    <React.Fragment>
+      <div className="upload-file-list">
+        <div className="card">
+          <h5 className="card-header">Uploaded Files</h5>
+          <table className="table table-hover uploadList">
+            <thead>
+              <tr>
+                <th scope="col">File Name</th>
+                <th scope="col">Status</th>
+                <th className="centered" scope="col">Upload Successful</th>
+                <th className="centered" scope="col">Cancel Upload</th>
+              </tr>
+            </thead>
+            <tbody>
+              {listObj}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <RecentUploadList previousUploads={previousUploads} />
+    </React.Fragment>
   );
 }
+
+const historyPropType = {
+  fileName: PropTypes.string,
+  timeStamp: PropTypes.number,
+  uuid: PropTypes.string,
+};
 
 UploadList.propTypes = {
   uploadFiles: PropTypes.arrayOf(PropTypes.shape({
@@ -46,9 +64,18 @@ UploadList.propTypes = {
     errorCode: PropTypes.string,
   })),
   cancelUpload: PropTypes.func.isRequired,
+  previousUploads: PropTypes.arrayOf(PropTypes.shape({
+    history: PropTypes.arrayOf(PropTypes.shape({ ...historyPropType })),
+    biospecimenBarcode: PropTypes.string,
+    subject: PropTypes.string,
+    phase: PropTypes.string,
+    dataType: PropTypes.string,
+  })),
 };
+
 UploadList.defaultProps = {
   uploadFiles: [],
+  previousUploads: [],
 };
 
 export default UploadList;
