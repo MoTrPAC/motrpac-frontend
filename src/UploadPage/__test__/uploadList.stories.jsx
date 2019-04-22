e0785497462d55bcbd86febaa52a21e3ebf23b5a
Jimmy Zhen <jzhen@stanford.edu>
2019-04-22 12:25:20 -0700

    Revert the addition of 'previousUploads' prop

--- src/UploadPage/__test__/uploadList.stories.jsx
+++ src/UploadPage/__test__/uploadList.stories.jsx
@@ -7,10 +7,9 @@ const uploadListActions = {
   cancelUpload: action('Cancel Upload'),
 };
 const testUploads = require('../../testData/testUploads');
-const testPreviousUploads = require('../../testData/testPreviousUploads');
 
 storiesOf('Upload List', module)
   // Padding added to indicate it is a component
   .addDecorator(story => <div className="upload-component" style={{ padding: '3rem' }}>{story()}</div>)
   .add('Empty List', () => <UploadList {...uploadListActions} />)
-  .add('Filled Test List', () => <UploadList uploadFiles={testUploads} previousUploads={testPreviousUploads} {...uploadListActions} />);
+  .add('Filled Test List', () => <UploadList uploadFiles={testUploads} {...uploadListActions} />);
