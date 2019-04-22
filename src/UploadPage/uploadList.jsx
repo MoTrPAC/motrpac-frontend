14cd658f9ca7e25211db15aed23d3de6a436ef49
Jimmy Zhen <jzhen@stanford.edu>
2019-04-22 12:24:38 -0700

    Revert the implementation of showing 'all' previous uploads at all times
    Also changed the UIs of the upload file list table to accommodate the need to show both new uploads and previous uploads.

--- src/UploadPage/uploadList.jsx
+++ src/UploadPage/uploadList.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import UploadListRow from './uploadListRow';
-import RecentUploadList from './recentUploadList';
 
-function UploadList({ uploadFiles, cancelUpload, previousUploads }) {
+function UploadList({ uploadFiles, cancelUpload }) {
   // Return empty message if no uploads
   if (!uploadFiles.length) {
     return (
-      <React.Fragment>
-        <div className="noListItems centered">
-          <h3>Upload files and they will appear here!</h3>
-        </div>
-        <RecentUploadList previousUploads={previousUploads} />
-      </React.Fragment>
+      <div className="noListItems centered">
+        <h3>Upload files and they will appear here!</h3>
+      </div>
     );
   }
 
@@ -27,35 +23,22 @@ function UploadList({ uploadFiles, cancelUpload, previousUploads }) {
     ));
 
   return (
-    <React.Fragment>
-      <div className="upload-file-list">
-        <div className="card">
-          <h5 className="card-header">Uploaded Files</h5>
-          <table className="table table-hover uploadList">
-            <thead>
-              <tr>
-                <th scope="col">File Name</th>
-                <th scope="col">Status</th>
-                <th className="centered" scope="col">Upload Successful</th>
-                <th className="centered" scope="col">Cancel Upload</th>
-              </tr>
-            </thead>
-            <tbody>
-              {listObj}
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <RecentUploadList previousUploads={previousUploads} />
-    </React.Fragment>
+    <div className="upload-file-list">
+      <table className="table table-hover uploadList">
+        <thead>
+          <tr>
+            <th scope="col">File Name</th>
+            <th className="centered" scope="col">Upload Status</th>
+            <th className="centered" scope="col">Cancel Upload</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listObj}
+        </tbody>
+      </table>
+    </div>
   );
 }
-
-const historyPropType = {
-  fileName: PropTypes.string,
-  timeStamp: PropTypes.number,
-  uuid: PropTypes.string,
-};
 
 UploadList.propTypes = {
   uploadFiles: PropTypes.arrayOf(PropTypes.shape({
@@ -64,18 +47,10 @@ UploadList.propTypes = {
     errorCode: PropTypes.string,
   })),
   cancelUpload: PropTypes.func.isRequired,
-  previousUploads: PropTypes.arrayOf(PropTypes.shape({
-    history: PropTypes.arrayOf(PropTypes.shape({ ...historyPropType })),
-    biospecimenBarcode: PropTypes.string,
-    subject: PropTypes.string,
-    phase: PropTypes.string,
-    dataType: PropTypes.string,
-  })),
 };
 
 UploadList.defaultProps = {
   uploadFiles: [],
-  previousUploads: [],
 };
 
 export default UploadList;
