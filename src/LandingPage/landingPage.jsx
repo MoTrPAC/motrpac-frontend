d72bfb2fc7d2c1998548c564b17047b1c9e386a4
Jimmy Zhen <jzhen@stanford.edu>
2019-05-07 10:31:16 -0700

    Add parallax effect to 'multi-omics' image on homepage

--- src/LandingPage/landingPage.jsx
+++ src/LandingPage/landingPage.jsx
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import Particles from 'react-particles-js';
+import { useSpring, animated } from 'react-spring';
 import LogoAnimation from '../assets/LandingPageGraphics/LogoAnimation_03082019-yellow_pipelineball_left.gif';
 import LayerRunner from '../assets/LandingPageGraphics/Data_Layer_Runner.png';
 import HealthyHeart from '../assets/LandingPageGraphics/Infographic_Healthy_Heart.png';
+
+// react-spring mouse parallax config
+const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
+const trans3d = (x, y) => `translate3d(${x / 6}px,${y / 16}px,0)`;
 
 /**
  * Renders the landing page in unauthenticated state.
@@ -19,6 +24,12 @@ export function LandingPage({ isAuthenticated, profile }) {
   // Local state for managing particle animation
   const [visibility, setVisibility] = useState(true);
   const hasAccess = profile.user_metadata && profile.user_metadata.hasAccess;
+  // react-spring set animation values
+  const [values, set] = useSpring(() => ({
+    xy: [0, 0],
+    config: { mass: 10, tension: 550, friction: 140 },
+  }));
+  const { xy } = values;
 
   if (isAuthenticated && hasAccess) {
     return <Redirect to="/dashboard" />;
@@ -145,8 +156,10 @@ export function LandingPage({ isAuthenticated, profile }) {
       <section>
         <div className="container featurette multi-omics" id="multi-omics">
           <div className="row featurette-wrapper h-100">
-            <div className="feature-image col-12 col-md-6 mx-auto">
-              <img src={LayerRunner} className="img-fluid" alt="Data Layer Runner" />
+            <div className="feature-image col-12 col-md-6 mx-auto" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
+              <animated.div className="animated-image-container" style={{ transform: xy.interpolate(trans3d) }}>
+                <img src={LayerRunner} className="img-fluid data-layer-runner" alt="Data Layer Runner" />
+              </animated.div>
             </div>
             <div className="content col-12 col-md-6">
               <h3>Multi-Omics</h3>
