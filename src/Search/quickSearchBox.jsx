c5dc0603e60907cf6df53d2b8aa2c0a36e4c506e
Jimmy Zhen <jzhen@stanford.edu>
2019-06-27 14:16:55 -0700

    :battery: Added handling for quick search form submission

--- src/Search/quickSearchBox.jsx
+++ src/Search/quickSearchBox.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import history from '../App/history';
 
 /**
  * Renders the quick search input field
@@ -7,13 +8,38 @@ import { Link } from 'react-router-dom';
  * @returns {object} JSX representation of search input element.
  */
 function QuickSearchBox() {
+  const [quickSearchTerm, setQuickSearchTerm] = useState('');
+  const quickSearchInput = useRef(null);
+
+  // Handler for quick search box value change
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    const searchTerm = quickSearchInput.current.value;
+    setQuickSearchTerm(searchTerm);
+  };
+
+  // Handler for quick search form submission
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    quickSearchInput.current.blur();
+    history.push(`/search?q=${encodeURI(quickSearchTerm)}`);
+  };
+
   return (
     <div className="d-flex align-items-center quick-search-box-container">
       <div className="quick-search-box-icon">
         <i className="material-icons">search</i>
       </div>
-      <form className="form-inline quick-search-box-form">
-        <input className="form-control quick-search-box" type="text" placeholder="Search" aria-label="Search" />
+      <form className="form-inline quick-search-box-form" onSubmit={handleFormSubmit}>
+        <input
+          ref={quickSearchInput}
+          type="text"
+          value={quickSearchTerm}
+          className="form-control quick-search-box"
+          onChange={handleInputChange}
+          placeholder="Search"
+          aria-label="Search"
+        />
       </form>
       <div className="quick-search-box-link">
         <Link to="/search" className="adv-search-link">Advanced</Link>
