7ac50574997620d01a4e9982c27e8aeea06968d0
Jimmy Zhen <jzhen@stanford.edu>
2019-07-03 08:58:12 -0700

    :battery: Implement action creator to append search query to URL when user submits the search form
    Also implement a simple action creator for the 'Advanced' link to show the advanced search page.

--- src/Search/searchActions.js
+++ src/Search/searchActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import history from '../App/history';
 
 export const SEARCH_FORM_CHANGE = 'SEARCH_FORM_CHANGE';
 export const SEARCH_FORM_ADD_PARAM = 'SEARCH_FORM_ADD_PARAM';
@@ -7,6 +8,7 @@ export const SEARCH_FORM_SUBMIT = 'SEARCH_FORM_SUBMIT';
 export const SEARCH_FORM_SUBMIT_FAILURE = 'SEARCH_FORM_SUBMIT_FAILURE';
 export const SEARCH_FORM_SUBMIT_SUCCESS = 'SEARCH_FORM_SUBMIT_SUCCESS';
 export const SEARCH_FORM_RESET = 'SEARCH_FORM_RESET';
+export const GET_SEARCH_FORM = 'GET_SEARCH_FORM';
 
 function searchFormChange(index, field, e) {
   return {
@@ -30,9 +32,10 @@ function searchFormRemoveParam(index) {
   };
 }
 
-function searchFormSubmit() {
+function searchFormSubmit(params) {
   return {
     type: SEARCH_FORM_SUBMIT,
+    params,
   };
 }
 
@@ -56,10 +59,26 @@ function searchFormReset() {
   };
 }
 
+function getSearchForm() {
+  return {
+    type: GET_SEARCH_FORM,
+  };
+}
+
+// Append search query to URL
+function pushHistoryWithQuery(params) {
+  const query = params.map((item) => {
+    const operator = item.operator && item.operator.length ? item.operator : '';
+    return `${operator} (${item.term}:${encodeURI(item.value)})`;
+  }).join(' ');
+
+  history.push(`/search?q=${query}`);
+}
+
 // FIXME: This function is not yet properly implemented
-function handleSearchFormSubmit() {
+function handleSearchFormSubmit(params) {
   return (dispatch) => {
-    dispatch(searchFormSubmit());
+    dispatch(searchFormSubmit(params));
     return axios.get('https://api.github.com/search/repositories', {
       params: {
         q: 'reactjs',
@@ -68,6 +87,7 @@ function handleSearchFormSubmit() {
       },
     }).then((response) => {
       dispatch(searchFormSubmitSuccess(response));
+      pushHistoryWithQuery(params);
     }).catch((err) => {
       dispatch(searchFormSubmitFailure(`${err.error}: ${err.errorDescription}`));
     });
@@ -80,6 +100,7 @@ const SearchActions = {
   searchFormAddParam,
   searchFormRemoveParam,
   searchFormReset,
+  getSearchForm,
 };
 
 export default SearchActions;
