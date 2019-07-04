9519fedd89d6a121342a60a3a7bf6b40069c3b86
Jimmy Zhen <jzhen@stanford.edu>
2019-07-03 17:47:49 -0700

    :wrench: Quick search Redux implementation in Storybook

--- src/Search/__test__/quickSearchBox.stories.jsx
+++ src/Search/__test__/quickSearchBox.stories.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import QuickSearchBox from '../quickSearchBox';
+import { defaultQuickSearchState } from '../quickSearchBoxReducer';
+
+const defaultState = {
+  ...defaultQuickSearchState,
+};
+
+const changedState = {
+  ...defaultState,
+  quickSearchTerm: 'brown adipose',
+};
+
+const actions = {
+  handleQuickSearchInputChange: action('Quick search input value changed'),
+  handleQuickSearchRequestSubmit: action('Quick search form submitted'),
+  resetQuickSearch: action('Quick search form reset'),
+  getSearchForm: action('Go to advanced search page'),
+};
 
 storiesOf('Quick Search Box', module)
   .addDecorator(story => (
@@ -9,4 +26,9 @@ storiesOf('Quick Search Box', module)
       {story()}
     </div>
   ))
-  .add('Default', () => <QuickSearchBox />);
+  .add('Default', () => (
+    <QuickSearchBox {...defaultState} {...actions} />
+  ))
+  .add('Input value changed', () => (
+    <QuickSearchBox {...changedState} {...actions} />
+  ));
