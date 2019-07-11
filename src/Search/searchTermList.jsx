8677942d523149bf3d8d107a3a9de7f034c15887
Jimmy Zhen <jzhen@stanford.edu>
2019-07-11 02:14:30 -0700

    :sparkles: Implemented the rendering of visual cues for selected search params

--- src/Search/searchTermList.jsx
+++ src/Search/searchTermList.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SearchTermRow from './searchTermRow';
 
+import IconClinic from '../assets/searchIcons/clinic.png';
+import IconDNA from '../assets/searchIcons/dna.png';
+import IconFlask from '../assets/searchIcons/flask.png';
+import IconGender from '../assets/searchIcons/gender.png';
+import IconHeart from '../assets/searchIcons/heart.png';
+import IconRat from '../assets/searchIcons/rat.png';
+import IconVial from '../assets/searchIcons/vial.png';
+
+const searchIconMapping = {
+  assay: IconFlask,
+  biospecimenid: IconVial,
+  gene: IconDNA,
+  sex: IconGender,
+  site: IconClinic,
+  species: IconRat,
+  tissue: IconHeart,
+};
+
 function SearchTermList({
   advSearchParams,
   handleSearchFormChange,
@@ -28,10 +46,37 @@ function SearchTermList({
     );
   });
 
+  // Render visuals for selected search term/value pairs
+  const filteredParamList = advSearchParams.filter(item => (item.value && item.value.length) || item.term === 'all');
+
+  const advSearchParamVisualContext = filteredParamList.map((item, idx) => {
+    const termId = `term-${idx}`;
+    const valueId = `value-${idx}`;
+    return (
+      <div key={`${termId}-${valueId}`} className="param-visual alert alert-info" role="alert">
+        {item.term === 'all' ?
+          <div><span>All</span></div>
+          : (
+            <div className="d-flex align-items-center">
+              <img src={searchIconMapping[item.term]} alt={item.term} />
+              <span>{item.value}</span>
+            </div>
+          )
+        }
+      </div>
+    );
+  });
+
   return (
-    <ul className="param-list">
-      {advSearchParamListObj}
-    </ul>
+    <div>
+      <ul className="param-list">
+        {advSearchParamListObj}
+      </ul>
+      <div className="param-list-visual-context d-flex align-items-center border-top pt-3 pb-3 mt-4 mb-4">
+        <h6>Selected criteria:</h6>
+        {advSearchParamVisualContext}
+      </div>
+    </div>
   );
 }
 
