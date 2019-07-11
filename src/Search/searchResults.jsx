7ba7071d87d6352214d2ab36eb60b41d59fe5db5
Jimmy Zhen <jzhen@stanford.edu>
2019-07-10 19:44:55 -0700

    :sparkles: Added implementation of rendering tissue sample results table

--- src/Search/searchResults.jsx
+++ src/Search/searchResults.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SearchResultFilters from './searchResultFilters';
+import SampleSearchResultTable from './sampleSearchResultTable';
 
 /**
  * Renders the search results
  *
  * @returns {object} JSX representation of search result content.
  */
-function SearchResults({ results }) {
+function SearchResults({ results, urlSearchParamsObj }) {
+  if (urlSearchParamsObj && Object.keys(urlSearchParamsObj).length) {
+    return (
+      <div className="sample-search-results-container">
+        <div className="sample-search-results">
+          <SampleSearchResultTable params={urlSearchParamsObj} />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="d-flex search-results-container">
       <div className="search-result-filters-container">
@@ -24,10 +35,19 @@ SearchResults.propTypes = {
   results: PropTypes.shape({
     data: PropTypes.object,
   }),
+  urlSearchParamsObj: PropTypes.shape({
+    action: PropTypes.string,
+    tissue: PropTypes.string,
+    phase: PropTypes.string,
+    study: PropTypes.string,
+    experiment: PropTypes.string,
+    site: PropTypes.string,
+  }),
 };
 
 SearchResults.defaultProps = {
   results: {},
+  urlSearchParamsObj: {},
 };
 
 export default SearchResults;
