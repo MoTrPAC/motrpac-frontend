a986abb55d5f8a22b1a6ef250f3938054f14898f
Jimmy Zhen <jzhen@stanford.edu>
2019-07-11 02:22:18 -0700

    :tada: Initial commit for mocked data

---
+++ src/lib/passSamples.js
@@ -0,0 +1,22 @@
+const passSamples = {
+  whole_blood: [
+    {
+      experiment: 'RNA-seq',
+      site: 'MSSM',
+      expected_samples: null,
+      received_samples: null,
+      study: 'GET',
+      phase: '1A',
+    },
+    {
+      experiment: 'RNA-seq',
+      site: 'Stanford',
+      expected_samples: null,
+      received_samples: null,
+      study: 'GET',
+      phase: '1A',
+    },
+  ],
+};
+
+export default passSamples;
