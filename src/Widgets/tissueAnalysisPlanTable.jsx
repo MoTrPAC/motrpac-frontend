aa346e37a77829ef31e65d871c328f98a3651547
Jimmy Zhen <jzhen@stanford.edu>
2019-07-12 18:28:38 -0700

    :rocket: Various UI enhancements including progress bars and icons for study types

--- src/Widgets/tissueAnalysisPlanTable.jsx
+++ src/Widgets/tissueAnalysisPlanTable.jsx
@@ -3,6 +3,16 @@ import { Link } from 'react-router-dom';
 import tissueList from '../lib/tissueList';
 import passSamples from '../lib/passSamples';
 
+import IconDNA from '../assets/searchIcons/dna.png';
+import IconMetabolite from '../assets/searchIcons/metabolite.png';
+import IconProtein from '../assets/searchIcons/protein.png';
+
+const searchIconMapping = {
+  gene: IconDNA,
+  metabolism: IconMetabolite,
+  protein: IconProtein,
+};
+
 /**
  * Renders the data analysis plan & tracking status table
  *
@@ -83,11 +93,53 @@ function TissueAnalysisPlanTable() {
     return cells;
   }
 
+  // Render individual tissue's progress bar
+  const renderProgressBar = (tissue) => {
+    const totalExpectedSamples = [];
+    const totalReceivedSamples = [];
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    if (passSamples[tissue] && passSamples[tissue].length) {
+      // Find all assays with expected samples
+      const experimentSampleArray = passSamples[tissue].filter((item) => {
+        return item.expected_samples && parseFloat(item.expected_samples);
+      });
+      experimentSampleArray.forEach((assay) => {
+        totalExpectedSamples.push(assay.expected_samples);
+        totalReceivedSamples.push(assay.received_samples);
+      });
+    }
+    const totalExpectedNum = totalExpectedSamples.reduce(reducer);
+    const totalReceivedNum = totalReceivedSamples.reduce(reducer);
+    const valueNow = parseFloat(totalReceivedNum / totalExpectedNum) * 100;
+    const widthStyle = { width: `${Math.round(valueNow)}%` };
+    return (
+      <div className="progress-bar-wrapper">
+        <div className="progress">
+          <div
+            className="progress-bar bg-success"
+            role="progressbar"
+            style={widthStyle}
+            aria-valuenow={Math.round(valueNow)}
+            aria-valuemin="0"
+            aria-valuemax="100"
+          />
+        </div>
+        <div className="tooltip-content">
+          {`${Math.round(valueNow)}%: ${totalExpectedNum} expected; ${totalReceivedNum} received`}
+          <i />
+        </div>
+      </div>
+    );
+  };
+
   // Renders individual rows of tissue samples
   const sampleTableRows = tissueList
     .map(tissue => (
       <tr key={tissue.label} className={tissue.label}>
         <th scope="row" className="text-left tissue-name text-nowrap">{tissue.name}</th>
+        <td className="total-samples">
+          {renderProgressBar(tissue.label)}
+        </td>
         {renderSampleTableCell(tissue.label)}
       </tr>
     ));
@@ -98,29 +150,58 @@ function TissueAnalysisPlanTable() {
         <thead>
           <tr className="table-head">
             <th scope="col" rowSpan="3" className="text-left">Tissue</th>
-            <th colSpan="4" className="PASS-GET-1A">GET - Phase 1A assays/site</th>
-            <th colSpan="4" className="PASS-GET-1B">GET - Phase 1B assays/site</th>
-            <th colSpan="16" className="PASS-Metabolomics-1A">Metabolomics - Phase 1A and 1B assays/site</th>
-            <th colSpan="6" className="PASS-Proteomics-1A">Proteomics - Phase 1A and 1B assays/site</th>
+            <th scope="col" rowSpan="3">Total Samples</th>
+            <th colSpan="4" className="PASS-GET-1A phase-study-heading">
+              <div className="d-flex align-items-center justify-content-center">
+                <img src={searchIconMapping.gene} alt="Genomic" />
+                <span>GET - Phase 1A assays/site</span>
+              </div>
+            </th>
+            <th colSpan="4" className="PASS-GET-1B phase-study-heading">
+              <div className="d-flex align-items-center justify-content-center">
+                <img src={searchIconMapping.gene} alt="Genomic" />
+                <span>GET - Phase 1B assays/site</span>
+              </div>
+            </th>
+            <th colSpan="19" className="PASS-Metabolomics-1A phase-study-heading">
+              <div className="d-flex align-items-center justify-content-center">
+                <img src={searchIconMapping.metabolism} alt="Metabolomic" />
+                <span>Metabolomics - Combined assays/site</span>
+              </div>
+            </th>
+            <th colSpan="6" className="PASS-Proteomics-1A phase-study-heading">
+              <div className="d-flex align-items-center justify-content-center">
+                <img src={searchIconMapping.protein} alt="Proteomic" />
+                <span>Proteomics - Combined assays/site</span>
+              </div>
+            </th>
           </tr>
           <tr className="table-head">
             {/* FIXME: Need to render these headings using data  */}
             {/* PASS-GET-1A */}
             <th className="assay-type PASS-GET-1A">
               <span className="experiment-name text-nowrap">RNA-seq</span>
-              <span className="badge badge-sinai">MSSM</span>
+              <Link to="/search?action=samples&phase=1A&study=GET&experiment=RNA-seq&site=MSSM">
+                <span className="badge badge-sinai">MSSM</span>
+              </Link>
             </th>
             <th className="assay-type PASS-GET-1A">
               <span className="experiment-name text-nowrap">RNA-seq</span>
-              <span className="badge badge-stanford">Stanford</span>
+              <Link to="/search?action=samples&phase=1A&study=GET&experiment=RNA-seq&site=Stanford">
+                <span className="badge badge-stanford">Stanford</span>
+              </Link>
             </th>
             <th className="assay-type PASS-GET-1A">
               <span className="experiment-name text-nowrap">ATAC-seq</span>
-              <span className="badge badge-stanford">Stanford</span>
+              <Link to="/search?action=samples&phase=1A&study=GET&experiment=ATAC-seq&site=Stanford">
+                <span className="badge badge-stanford">Stanford</span>
+              </Link>
             </th>
             <th className="assay-type PASS-GET-1A">
               <span className="experiment-name text-nowrap">Methylome</span>
-              <span className="badge badge-sinai">MSSM</span>
+              <Link to="/search?action=samples&phase=1A&study=GET&experiment=Methylome&site=MSSM">
+                <span className="badge badge-sinai">MSSM</span>
+              </Link>
             </th>
             {/* PASS-GET-1B */}
             <th className="assay-type PASS-GET-1B text-nowrap">
@@ -149,8 +230,16 @@ function TissueAnalysisPlanTable() {
               <span className="badge badge-broad">Broad</span>
             </th>
             <th className="assay-type PASS-Metabolomics-1A">
-              <span className="experiment-name">Untargeted HILIC -negative</span>
+              <span className="experiment-name">Untargeted HILIC-negative</span>
               <span className="badge badge-michigan">UM</span>
+            </th>
+            <th className="assay-type PASS-Metabolomics-1A">
+              <span className="experiment-name">Untargeted HILIC-positive</span>
+              <span className="badge badge-georgia-tech">GT</span>
+            </th>
+            <th className="assay-type PASS-Metabolomics-1A">
+              <span className="experiment-name">Untargeted HILIC-negative</span>
+              <span className="badge badge-georgia-tech">GT</span>
             </th>
             <th className="assay-type PASS-Metabolomics-1A">
               <span className="experiment-name">Untargeted Lipidomics</span>
@@ -186,6 +275,10 @@ function TissueAnalysisPlanTable() {
             </th>
             <th className="assay-type PASS-Metabolomics-1A">
               <span className="experiment-name">Targeted Amines</span>
+              <span className="badge badge-mayo">Mayo</span>
+            </th>
+            <th className="assay-type PASS-Metabolomics-1A">
+              <span className="experiment-name">Targeted TCA</span>
               <span className="badge badge-mayo">Mayo</span>
             </th>
             <th className="assay-type PASS-Metabolomics-1A">
@@ -236,10 +329,22 @@ function TissueAnalysisPlanTable() {
         </tbody>
         <tfoot>
           <tr>
-            <td colSpan="31 text-left">
+            <td colSpan="35 text-left">
               <div className="footnote">
-                <p>* GT submitted tissue samples beyond the experiment as indicated on this TAP.</p>
-                <p>* Mayo submitted tissue samples for <em>Targeted TCA</em> experiment not as indicated on this TAP.</p>
+                <p>
+                  <span className="legend">
+                    <span className="legend-label expected" />
+                    <span className="legend-definition">Expected</span>
+                  </span>
+                  <span className="legend">
+                    <span className="legend-label received" />
+                    <span className="legend-definition">Received</span>
+                  </span>
+                  <span className="legend">
+                    <span className="legend-label unexpected" />
+                    <span className="legend-definition">Unexpected but received</span>
+                  </span>
+                </p>
               </div>
             </td>
           </tr>
