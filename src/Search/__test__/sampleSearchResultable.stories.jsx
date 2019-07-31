1819982f4d2dfc6fca58ee6e0808b8accf6cb048
Jimmy Zhen <jzhen@stanford.edu>
2019-07-31 14:48:23 -0700

    :tada: Initial commit for Storybook UI component for displaying the sample results table

---
+++ src/Search/__test__/sampleSearchResultable.stories.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import SampleSearchResultTable from '../sampleSearchResultTable';
+
+const urlSearchParamsObj = {
+  action: 'sample',
+  tissue: 'heart',
+  phase: '1A',
+  study: 'GET',
+  experiment: 'RNA-seq',
+  site: 'MSSM',
+};
+
+storiesOf('Sample Results Table', module)
+  // Padding added to indicate it is a component
+  .addDecorator(story => (
+    <div className="searchPage container-fluid">
+      <div className="advanced-search-form-container">
+        {story()}
+      </div>
+    </div>
+  ))
+  .add('Default', () => <SampleSearchResultTable params={urlSearchParamsObj} />);
