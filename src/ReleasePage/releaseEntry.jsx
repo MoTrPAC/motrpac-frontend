94fbc10bed2e671f273665bc7f062a007a9809ba
Jimmy Zhen <jzhen@stanford.edu>
2019-08-26 10:26:00 -0700

    Updated UIs given the team's feedback

--- src/ReleasePage/releaseEntry.jsx
+++ src/ReleasePage/releaseEntry.jsx
@@ -65,7 +65,27 @@ function ReleaseEntry() {
     return (
       <div className="card mb-3">
         <div className="card-body">
-          <p className="card-text">Download description goes here.</p>
+          <p className="card-text">
+            Due to the large sizes of raw data files, we recommend users
+            who wish to download raw data files using the&nbsp;
+            <a href="https://cloud.google.com/storage/docs/quickstart-gsutil" target="_blank" rel="noopener noreferrer">
+              gsutil command
+              <i className="material-icons external-linkout-icon">open_in_new</i>
+            </a>
+            . Below are example commands for downloading raw data files of different omics.
+          </p>
+          <p className="card-text">
+            Download raw data files of genomics:
+            <code>gsutil -m cp -r gs://motrpac-release-1-raw-get/* .</code>
+          </p>
+          <p className="card-text">
+            Download raw data files of metabolomics:
+            <code>gsutil -m cp -r gs://motrpac-release-1-raw-metabolomics/* .</code>
+          </p>
+          <p className="card-text">
+            Download raw data files of proteomics:
+            <code>gsutil -m cp -r gs://motrpac-release-1-raw-proteomics/* .</code>
+          </p>
         </div>
       </div>
     );
@@ -78,7 +98,27 @@ function ReleaseEntry() {
     return (
       <div className="card mb-3">
         <div className="card-body">
-          <p className="card-text">Download description goes here.</p>
+          <p className="card-text">
+            Due to the large sizes of intermediate data files, we recommend users
+            who wish to download intermediate files using the&nbsp;
+            <a href="https://cloud.google.com/storage/docs/quickstart-gsutil" target="_blank" rel="noopener noreferrer">
+              gsutil command
+              <i className="material-icons external-linkout-icon">open_in_new</i>
+            </a>
+            . Below are example command lines for downloading individual omics intermediate files.
+          </p>
+          <p className="card-text">
+            Download GET intermediate files of genomics:
+            <code>gsutil -m cp -r gs://motrpac-release-1-intermediate-get/* .</code>
+          </p>
+          <p className="card-text">
+            Download intermediate files of metabolomics:
+            <code>gsutil -m cp -r gs://motrpac-release-1-intermediate-metabolomics/* .</code>
+          </p>
+          <p className="card-text">
+            Download intermediate files of proteomics:
+            <code>gsutil -m cp -r gs://motrpac-release-1-intermediate-proteomics/* .</code>
+          </p>
         </div>
       </div>
     );
@@ -115,19 +155,55 @@ function ReleaseEntry() {
               {/* release content */}
               <div className="col-12 col-md-9 col-lg-10 pl-md-3 py-md-4 release-main-section float-left">
                 <h4 className="release-header"><span role="img" aria-label="rocket">🚀</span> {release.label}</h4>
-                <div className="release-description mb-3">
-                  <p>{release.description}</p>
-                  <p className="mb-2">
-                    <a
-                      href={`http://storage.googleapis.com/${release.bucket_name}${release.object_path}`}
-                      className="d-block d-flex align-items-center justify-content-start release-data-download-link"
-                      download
-                    >
-                      <i className="material-icons release-data-download-icon">save_alt</i>
-                      <span>Download experiment metadata and QC results</span>
-                    </a>
-                  </p>
-                  <h5 className="additional-release-download-header">Additional Downloads</h5>
+                <div className="release-content mb-3">
+                  <div className="card mb-3">
+                    <div className="card-body">
+                      <p className="release-description">{release.description}</p>
+                      <div className="release-links">
+                        <a
+                          href={`http://storage.googleapis.com/${release.bucket_name}${release.object_path}`}
+                          className="d-block d-flex align-items-center justify-content-start release-data-download-link"
+                          download
+                        >
+                          <i className="material-icons release-data-download-icon">save_alt</i>
+                          <span>RNA-seq data (metadata, QC and summary)</span>
+                        </a>
+                        <a
+                          href={`http://storage.googleapis.com/${release.bucket_name}${release.object_path}`}
+                          className="d-block d-flex align-items-center justify-content-start release-data-download-link"
+                          download
+                        >
+                          <i className="material-icons release-data-download-icon">save_alt</i>
+                          <span>RRBS data (metadata, QC and summary)</span>
+                        </a>
+                        <a
+                          href={`http://storage.googleapis.com/${release.bucket_name}${release.object_path}`}
+                          className="d-block d-flex align-items-center justify-content-start release-data-download-link"
+                          download
+                        >
+                          <i className="material-icons release-data-download-icon">save_alt</i>
+                          <span>Global Phospho-proteomics data (metadata, QC and summary)</span>
+                        </a>
+                        <a
+                          href={`http://storage.googleapis.com/${release.bucket_name}${release.object_path}`}
+                          className="d-block d-flex align-items-center justify-content-start release-data-download-link"
+                          download
+                        >
+                          <i className="material-icons release-data-download-icon">save_alt</i>
+                          <span>Phenotypic data</span>
+                        </a>
+                        <a
+                          href={`http://storage.googleapis.com/${release.bucket_name}${release.object_path}`}
+                          className="d-block d-flex align-items-center justify-content-start release-data-download-link"
+                          download
+                        >
+                          <i className="material-icons release-data-download-icon">save_alt</i>
+                          <span>All experiment metadata, QC results and PASS1A 6-Month acute phenotypic data</span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <h6 className="additional-release-download-header">Additional Downloads</h6>
                   <div className="raw-files-download-section">
                     <p className="d-block mb-2 d-flex align-items-center justify-content-start raw-files-download-option">
                       {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
