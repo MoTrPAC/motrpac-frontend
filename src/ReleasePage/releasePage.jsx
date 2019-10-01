83eb71445fe01c90f12ba08c5a4f8cd659ae5b45
Jimmy Zhen <jzhen@stanford.edu>
2019-10-01 15:13:44 -0700

    Update UI change handling to render internal or external release views depending on user types

--- src/ReleasePage/releasePage.jsx
+++ src/ReleasePage/releasePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
@@ -17,7 +17,15 @@ export function ReleasePage({
   isAuthenticated,
   profile,
 }) {
+  const [currentView, setCurrentView] = useState('internal');
+
   const hasAccess = profile.user_metadata && profile.user_metadata.hasAccess;
+  const userType = profile.user_metadata && profile.user_metadata.userType;
+
+  // Handler to set current view to either internal or external releases
+  function handleViewChange(releaseView) {
+    setCurrentView(releaseView);
+  }
 
   // FIXME: temp workaround to handle callback redirect
   if (isPending) {
@@ -31,18 +39,34 @@ export function ReleasePage({
     );
   }
 
-  // Send users back to homepage if not authenticated
+  // Route users back to homepage if not authenticated
   if (!isAuthenticated) {
     return (<Redirect to="/" />);
   }
 
+  // Route users to error page if registered users are
+  // not allowed to have data access
   if (isAuthenticated) {
     if (!hasAccess) {
       return (<Redirect to="/error" />);
     }
   }
 
-  // Render advanced search form by default
+  // Render external release view if user type is 'external'
+  if (userType === 'external') {
+    return (
+      <div className="col-md-9 ml-sm-auto col-lg-10 px-4 dataReleasePage">
+        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 border-bottom">
+          <div className="page-title" style={{ backgroundImage: `url(${IconSet.InternalDataRelease})` }}>
+            <h3>Data Releases</h3>
+          </div>
+        </div>
+        <ReleaseEntry profile={profile} currentView={userType} />
+      </div>
+    );
+  }
+
+  // Render internal release view by default
   return (
     <div className="col-md-9 ml-sm-auto col-lg-10 px-4 dataReleasePage">
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 border-bottom">
@@ -50,13 +74,28 @@ export function ReleasePage({
           <h3>Data Releases</h3>
         </div>
         <div className="btn-toolbar">
-          <div className="btn-group">
-            {/* <Link className="browseDataBtn btn btn-sm btn-outline-primary" to="/download">Browse Data</Link> */}
+          <div className="btn-group mr-2" role="group" aria-label="Release button group">
+            <button
+              type="button"
+              className={`btn btn-sm btn-outline-primary ${currentView === 'internal' ? 'active' : ''}`}
+              onClick={handleViewChange.bind(this, 'internal')}
+            >
+              Internal Release
+            </button>
+            <button
+              type="button"
+              className={`btn btn-sm btn-outline-primary ${currentView === 'external' ? 'active' : ''}`}
+              onClick={handleViewChange.bind(this, 'external')}
+            >
+              External Release
+            </button>
+          </div>
+          <div className="btn-group" role="group" aria-label="Search button group">
             <Link className="advSearchBtn btn btn-sm btn-outline-primary" to="/search">Search Data</Link>
           </div>
         </div>
       </div>
-      <ReleaseEntry profile={profile} />
+      <ReleaseEntry profile={profile} currentView={currentView} />
     </div>
   );
 }
