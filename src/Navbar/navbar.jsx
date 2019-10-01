590efeff0b4a96adf49937ef63ee6057224d61f2
Jimmy Zhen <jzhen@stanford.edu>
2019-10-01 14:24:31 -0700

    Suppress quick search box if logged in as external user
    Also suppress 'Data Access' nav link if user is logged in.

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -38,6 +38,7 @@ export function Navbar({
   };
 
   const hasAccess = profile.user_metadata && profile.user_metadata.hasAccess;
+  const userType = profile.user_metadata && profile.user_metadata.userType;
 
   if (isAuthenticated && hasAccess) {
     document.querySelector('body').classList.add('authenticated');
@@ -88,12 +89,14 @@ export function Navbar({
                   <Link to="/contact" className="dropdown-item">Contact Us</Link>
                 </div>
               </li>
-              <li className="nav-item navItem"><Link to="/data-access" className="nav-link">Data Access</Link></li>
+              {!isAuthenticated && !hasAccess
+                ? (<li className="nav-item navItem"><Link to="/data-access" className="nav-link">Data Access</Link></li>)
+                : null}
               <li className="nav-item navItem">
                 <LogoutButton />
               </li>
             </ul>
-            {isAuthenticated && hasAccess
+            {isAuthenticated && hasAccess && userType === 'internal'
               ? (
                 <QuickSearchBox
                   quickSearchTerm={quickSearchTerm}
