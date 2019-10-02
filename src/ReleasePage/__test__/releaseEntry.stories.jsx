406ca88173dc9e641080f2e1efe6bb89810c1eff
Jimmy Zhen <jzhen@stanford.edu>
2019-10-02 14:30:04 -0700

    :tada: Initial commits for Storybook components

---
+++ src/ReleasePage/__test__/releaseEntry.stories.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import ReleaseEntry from '../releaseEntry';
+
+const internalUser = require('../../testData/testUser');
+
+const externalUser = {
+  ...internalUser,
+  user_metadata: {
+    name: 'Test User',
+    givenName: 'TestUser',
+    siteName: 'Stanford',
+    hasAccess: true,
+    userType: 'external',
+  },
+};
+
+const internalLoggedInState = {
+  currentView: 'internal',
+  profile: internalUser,
+};
+
+const externalLoggedInState = {
+  currentView: 'external',
+  profile: externalUser,
+};
+
+storiesOf('Release Entry', module)
+  .addDecorator(story => (
+    <div className="dataReleasePage container-fluid">
+      {story()}
+    </div>
+  ))
+  .add('Internal user view', () => (
+    <ReleaseEntry {...internalLoggedInState} />
+  ))
+  .add('External user view', () => (
+    <ReleaseEntry {...externalLoggedInState} />
+  ));
