579d6ec1ff0c76aa49f15c63076109e8f1e62c1a
Jimmy Zhen <jzhen@stanford.edu>
2019-10-15 13:07:17 -0700

    Initial commit of shared component for motrpac study documents

---
+++ src/lib/studyDocumentsTable.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import IconSet from './iconSet';
+import StudyDocuments from '../DataAccess/studyDocuments';
+
+function StudyDocumentsTable() {
+  // Handler to render study documents table rows
+  function renderStudyDocumentsTableRow(item) {
+    return (
+      <tr key={item.title} className="document-list-item">
+        <td>
+          <div className="d-flex align-items-center justify-content-start">
+            <img src={IconSet.PDF} alt="PDF" />
+            <a href={item.location} download target="_blank" rel="noopener noreferrer">{item.title}</a>
+          </div>
+        </td>
+        <td>{item.description}</td>
+      </tr>
+    );
+  }
+
+  return (
+    <div className="card mb-3 border-secondary motrpac-study-documents">
+      <div className="card-header bg-secondary text-light">MoTrPAC study documents</div>
+      <div className="card-body">
+        <table className="table table-striped table-sm document-list">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {StudyDocuments.map(item => renderStudyDocumentsTableRow(item))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default StudyDocumentsTable;
