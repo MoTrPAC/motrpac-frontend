f07f13214aa62ddf2592b19c5e0b91aa93ca6aa9
Jimmy Zhen <jzhen@stanford.edu>
2019-11-22 00:05:05 -0800

    Adding rules to suppress eslint warning about using storybook as devDependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,11 @@ module.exports = {
         "jsx-a11y/label-has-for": "off",
         "jsx-a11y/label-has-associated-control": "off",
         "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn"
+        "react-hooks/exhaustive-deps": "warn",
+        "import/no-extraneous-dependencies": [
+            "error",
+            {"devDependencies": true}
+        ]
     },
     "env": {
         "browser": true,
