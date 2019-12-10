c6380c102ae1398b0189e747ab717a593c677627
Jimmy Zhen <jzhen@stanford.edu>
2019-12-09 16:32:18 -0800

    Turning off this eslint rule for now because we are using props spreading extensively in Storybook components

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
         "import/no-extraneous-dependencies": [
             "error",
             {"devDependencies": true}
-        ]
+        ],
+        "react/jsx-props-no-spreading": "off"
     },
     "env": {
         "browser": true,
