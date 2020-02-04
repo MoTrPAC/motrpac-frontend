dc9fb40288463752ae82fa2891a78fa08c28206b
Jimmy Zhen <jzhen@stanford.edu>
2020-02-04 00:31:37 -0800

    Corrected typo (Mac instead of MAC)

--- src/ReleasePage/releaseEntry.jsx
+++ src/ReleasePage/releaseEntry.jsx
@@ -307,7 +307,7 @@ function ReleaseEntry({ profile, currentView }) {
                     <div className="card-body">
                       <p className="release-description">{release.description}</p>
                       <p className="release-description file-extraction-instruction">
-                        All bundled datasets are compressed (*.tar.gz) and need to be extracted upon downloading. For MAC
+                        All bundled datasets are compressed (*.tar.gz) and need to be extracted upon downloading. For Mac
                         users, double-clicking a downloaded *.tar.gz file will extract it. For Windows users, use
                         {' '}
                         <ExternalLink to="https://www.winzip.com" label="WinZip" />
