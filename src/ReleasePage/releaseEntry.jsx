c785d38a7a7180fc4fd615cbf9db0f5013b5e381
Jimmy Zhen <jzhen@stanford.edu>
2020-04-22 18:52:15 -0700

    Added condition for rendering raw and intermediate file download options because the upcoming release only consists of phenotypic data

--- src/ReleasePage/releaseEntry.jsx
+++ src/ReleasePage/releaseEntry.jsx
@@ -16,6 +16,7 @@ const emojiMap = {
   sparkles: '✨',
   zap: '⚡️',
   tada: '🎉',
+  dna: '🧬',
 };
 
 /**
@@ -308,8 +309,9 @@ function ReleaseEntry({ profile, currentView }) {
                     <div className="card-body">
                       <p className="release-description">{release.description}</p>
                       <p className="release-description file-extraction-instruction">
-                        All bundled datasets are compressed (*.tar.gz) and need to be extracted upon downloading. For Mac
-                        users, double-clicking a downloaded *.tar.gz file will extract it. For Windows users, use
+                        All bundled datasets are compressed (*.tar.gz) and need to be extracted upon
+                        downloading. For Mac users, double-clicking a downloaded *.tar.gz file will
+                        extract it. For Windows users, use
                         {' '}
                         <ExternalLink to="https://www.winzip.com" label="WinZip" />
                         {' '}
@@ -376,7 +378,7 @@ function ReleaseEntry({ profile, currentView }) {
                       {renderModal(release.version)}
                     </div>
                   </div>
-                  {currentView === 'internal'
+                  {currentView === 'internal' && release.raw_files
                     ? (
                       <>
                         <h6 className="additional-release-download-header">Additional Downloads</h6>
