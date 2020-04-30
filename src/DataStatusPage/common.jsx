009a3fe271564c85206e06f7938b6c6556810547
Jimmy Zhen <jzhen@stanford.edu>
2020-04-29 19:31:49 -0700

    Added 2 more shared pagination UI rendering functions

--- src/DataStatusPage/common.jsx
+++ src/DataStatusPage/common.jsx
@@ -46,8 +46,7 @@ export const tableColumns = [
 
 /**
  * page count and page index rendering function
- * common to rna-seq, rrbs, and atac-seq
- * data qc status reports
+ * common to all data qc status reports
 */
 export const PageIndex = ({ pageIndex, pageOptions }) => (
   <span className="page-index">
@@ -67,6 +66,96 @@ PageIndex.propTypes = {
   pageOptions: PropTypes.shape({
     length: PropTypes.number,
   }).isRequired,
+};
+
+/**
+ * page size control rendering function
+ * common to all data qc status reports
+*/
+export const PageSize = ({ pageSize, setPageSize }) => (
+  <div className="pagination-page-size d-flex align-items-center justify-content-start">
+    <label htmlFor="pageSizeSelect">Show:</label>
+    <select
+      className="form-control"
+      id="pageSizeSelect"
+      value={pageSize}
+      onChange={(e) => {
+        setPageSize(Number(e.target.value));
+      }}
+    >
+      {[10, 20, 30, 40, 50, 60, 70].map((size) => (
+        <option key={size} value={size}>
+          {size}
+        </option>
+      ))}
+    </select>
+    <span>entries</span>
+  </div>
+);
+
+PageSize.propTypes = {
+  pageSize: PropTypes.number.isRequired,
+  setPageSize: PropTypes.func.isRequired,
+};
+
+/**
+ * page navigation control rendering function
+ * common to all data qc status reports
+*/
+export const PageNavigationControl = ({
+  canPreviousPage,
+  canNextPage,
+  previousPage,
+  nextPage,
+  gotoPage,
+  pageCount,
+}) => (
+  <div className="btn-group pagination-navigation-control" role="group">
+    <button
+      type="button"
+      className={`btn btn-sm btn-outline-primary ${!canPreviousPage ? 'disabled-btn' : ''}`}
+      onClick={() => gotoPage(0)}
+      disabled={!canPreviousPage}
+    >
+      First
+    </button>
+    {' '}
+    <button
+      type="button"
+      className={`btn btn-sm btn-outline-primary ${!canPreviousPage ? 'disabled-btn' : ''}`}
+      onClick={() => previousPage()}
+      disabled={!canPreviousPage}
+    >
+      Previous
+    </button>
+    {' '}
+    <button
+      type="button"
+      className={`btn btn-sm btn-outline-primary ${!canNextPage ? 'disabled-btn' : ''}`}
+      onClick={() => nextPage()}
+      disabled={!canNextPage}
+    >
+      Next
+    </button>
+    {' '}
+    <button
+      type="button"
+      className={`btn btn-sm btn-outline-primary ${!canNextPage ? 'disabled-btn' : ''}`}
+      onClick={() => gotoPage(pageCount - 1)}
+      disabled={!canNextPage}
+    >
+      Last
+    </button>
+  </div>
+);
+
+PageNavigationControl.propTypes = {
+  canPreviousPage: PropTypes.bool.isRequired,
+  canNextPage: PropTypes.bool.isRequired,
+  previousPage: PropTypes.func.isRequired,
+  nextPage: PropTypes.func.isRequired,
+  gotoPage: PropTypes.func.isRequired,
+  pageCount: PropTypes.number.isRequired,
 };
 
 /**
