b0379b6e38fe2cd3a4b314839ef7a83e747e89f9
Jimmy Zhen <jzhen@stanford.edu>
2020-08-24 22:57:41 -0700

    Configured DOM element width to support collapsible sidebar
    Also reformatted code using prettier

--- src/LinkoutPage/linkoutPage.jsx
+++ src/LinkoutPage/linkoutPage.jsx
@@ -6,25 +6,9 @@ import NIHLogo from '../assets/ExternalLogos/NIHCommonFund.png';
 import ENCODELogo from '../assets/ExternalLogos/ENCODE.png';
 import MWLogo from '../assets/ExternalLogos/MetabolomicsWorkbench.jpeg';
 import GTExLogo from '../assets/ExternalLogos/GTEx.png';
+import AuthContentContainer from '../lib/ui/authContentContainer';
 
 const linkList = [
-/* Currently not desired to be on this page
-  {
-    name: 'Source Code',
-    links: [
-      {
-        protocol: 'https',
-        url: 'Github.com/AshleyLab/motrpac-frontend',
-        text: 'Github Repository for code creating this sites front end',
-      },
-      {
-        protocol: 'https',
-        url: 'Github.com/AshleyLab/motrpac_metaanalysis',
-        text: 'Github Repository for code used in meta-analysis',
-      },
-    ],
-  },
-*/
   {
     name: 'Partners',
     links: [
@@ -65,48 +49,59 @@ const linkList = [
  * unauthenticated and authenticated states.
  *
  * @param {Boolean} isAuthenticated Redux state for user's authentication status.
+ * @param {Boolean} expanded        Redux state for sidebar
  *
  * @returns {Object} JSX representation of the External Links page.
  */
-export function LinkoutPage({ isAuthenticated }) {
-  const links = linkList.map(category => (
+export function LinkoutPage({ isAuthenticated, expanded }) {
+  const links = linkList.map((category) => (
     <div key={category.name} className="LinkCategory">
       <h4>{category.name}</h4>
       <div className="card-deck">
-        {
-          category.links.map(link => <UsefulLink key={link.url} link={link} />)
-        }
+        {category.links.map((link) => (
+          <UsefulLink key={link.url} link={link} />
+        ))}
       </div>
     </div>
   ));
 
-  return (
-    <div className={`col-md-9 ${isAuthenticated ? 'ml-sm-auto' : ''} col-lg-10 px-4 linkoutPage`}>
-      <div className={`${!isAuthenticated ? 'container' : ''}`}>
-        <div className="page-title pt-3 pb-2 border-bottom">
-          <h3>Useful Links</h3>
+  const pageContent = (
+    <>
+      <div className="page-title pt-3 pb-2 border-bottom">
+        <h3>Useful Links</h3>
+      </div>
+      <div className="row align-items-center justify-content-center motrLink">
+        <div className="col-12 col-md-5 centered">
+          <img src={MOTRLogo} className="img-fluid" alt="MoTrPAC Logo" />
         </div>
-        <div className="row align-items-center justify-content-center motrLink">
-          <div className="col-12 col-md-5 centered">
-            <img src={MOTRLogo} className="img-fluid" alt="MoTrPAC Logo" />
-          </div>
-          <div className="col MoTrLinkInfo h5">
-            <a href="http://MoTrPAC.org">
-              MoTrPAC Main Site
-              &nbsp;
-              <span className="oi oi-external-link" />
-            </a>
-            <p>
-              Primary entrance point for overarching MoTrPAC study of which the
-              Bioinformatic Datahub is a component.
-            </p>
-          </div>
-        </div>
-        <div className="externalLinks">
-          {links}
+        <div className="col MoTrLinkInfo h5">
+          <a href="http://MoTrPAC.org">
+            MoTrPAC Main Site
+            {' '}
+            <span className="oi oi-external-link" />
+          </a>
+          <p>
+            Primary entrance point for overarching MoTrPAC study of which the
+            Bioinformatic Datahub is a component.
+          </p>
         </div>
       </div>
-    </div>
+      <div className="externalLinks">{links}</div>
+    </>
+  );
+
+  if (!isAuthenticated) {
+    return (
+      <div className="col-md-9 col-lg-10 px-4 linkoutPage">
+        <div className="container">{pageContent}</div>
+      </div>
+    );
+  }
+
+  return (
+    <AuthContentContainer classes="linkoutPage" expanded={expanded}>
+      <div>{pageContent}</div>
+    </AuthContentContainer>
   );
 }
 
@@ -117,7 +112,10 @@ function UsefulLink({ link }) {
   }
   return (
     <div className="card mb-4 shadow-sm">
-      <div className="card-img-top" style={{ backgroundImage: `url("${imgUrl}")` }} />
+      <div
+        className="card-img-top"
+        style={{ backgroundImage: `url("${imgUrl}")` }}
+      />
       <div className="card-body">
         <h6 className="card-title">
           <a href={`${link.protocol}://www.${link.url}`} target="_new">
@@ -126,9 +124,7 @@ function UsefulLink({ link }) {
             <span className="oi oi-external-link" />
           </a>
         </h6>
-        <p className="card-text">
-          {link.text}
-        </p>
+        <p className="card-text">{link.text}</p>
       </div>
     </div>
   );
@@ -144,14 +140,17 @@ UsefulLink.propTypes = {
 
 LinkoutPage.propTypes = {
   isAuthenticated: PropTypes.bool,
+  expanded: PropTypes.bool,
 };
 
 LinkoutPage.defaultProps = {
   isAuthenticated: false,
+  expanded: false,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated,
+  expanded: state.sidebar.expanded,
 });
 
 export default connect(mapStateToProps)(LinkoutPage);
