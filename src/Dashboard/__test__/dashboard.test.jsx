00a82a0158d1b267683a3b43fa165d8c8d273955
Jimmy Zhen <jzhen@stanford.edu>
2020-09-21 11:34:31 -0700

    Updated unit test assertions due to UI changes

--- src/Dashboard/__test__/dashboard.test.jsx
+++ src/Dashboard/__test__/dashboard.test.jsx
@@ -6,16 +6,20 @@ import { Provider } from 'react-redux';
 import thunkMiddleWare from 'redux-thunk';
 import { Router } from 'react-router-dom';
 import rootReducer, { defaultRootState } from '../../App/reducers';
-import { defaultUploadState } from '../../UploadPage/uploadReducer';
+import { defaultDashboardState } from '../dashboardReducer';
+import { defaultSidebarState } from '../../Sidebar/sidebarReducer';
 import DashboardConnected, { Dashboard } from '../dashboard';
 import History from '../../App/history';
 
 Enzyme.configure({ adapter: new Adapter() });
-const testPreviousUploads = require('../../testData/testPreviousUploads');
+
 const testUser = require('../../testData/testUser');
 
-const buttonActions = {
-  clearForm: jest.fn(),
+const controlActions = {
+  toggleRelease: jest.fn(),
+  togglePhase: jest.fn(),
+  togglePlot: jest.fn(),
+  toggleSort: jest.fn(),
 };
 
 describe('Shallow Dashboard', () => {
@@ -23,9 +27,10 @@ describe('Shallow Dashboard', () => {
     <Dashboard
       isAuthenticated
       profile={testUser}
-      previousUploads={testPreviousUploads}
-      {...buttonActions}
-    />,
+      {...defaultDashboardState}
+      {...controlActions}
+      {...defaultSidebarState}
+    />
   );
   const loggedInRootState = {
     ...defaultRootState,
@@ -34,29 +39,31 @@ describe('Shallow Dashboard', () => {
       isAuthenticated: true,
       profile: testUser,
     },
-    upload: {
-      ...defaultUploadState,
-      previousUploads: testPreviousUploads,
-    },
   };
   const mountDash = mount((
-    <Provider store={createStore(rootReducer, loggedInRootState, applyMiddleware(thunkMiddleWare))}>
+    <Provider
+      store={createStore(
+        rootReducer,
+        loggedInRootState,
+        applyMiddleware(thunkMiddleWare)
+      )}
+    >
       <Router history={History}>
         <DashboardConnected />
       </Router>
     </Provider>
   ));
   test('Has expected widgets', () => {
-    expect(shallowDash.find('Connect(PreviousUploadsTable)')).toHaveLength(1);
-    expect(shallowDash.find('PreviousUploadsGraph')).toHaveLength(1);
-    expect(shallowDash.find('AllUploadsDoughnut')).toHaveLength(1);
-    expect(shallowDash.find('AllUploadStats')).toHaveLength(1);
+    expect(shallowDash.find('ReleasedSampleHighlight')).toHaveLength(1);
+    expect(shallowDash.find('ReleasedSamplePlot')).toHaveLength(1);
+    expect(shallowDash.find('ReleasedSampleTable')).toHaveLength(1);
+    expect(shallowDash.find('ReleasedSampleSummary')).toHaveLength(1);
+    expect(shallowDash.find('PlotControls')).toHaveLength(1);
+    expect(shallowDash.find('TableControls')).toHaveLength(1);
   });
 
-  test('Edit button appears after expanding an upload on the dashboard', () => {
-    expect(mountDash.find('.editUploadBtn')).toHaveLength(0);
-    mountDash.find('Caret').first().simulate('click');
-    mountDash.update();
-    expect(mountDash.find('.editUploadBtn')).toHaveLength(1);
+  test('Has 4 expected release sample filter buttons', () => {
+    expect(mountDash.find('.btn-outline-primary')).toHaveLength(4);
+    expect(mountDash.find('.btn-outline-primary.active')).toHaveLength(2);
   });
 });
