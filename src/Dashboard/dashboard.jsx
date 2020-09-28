c4621c936e14e0c301f608dd3e9d8283c6c8ea36
Jimmy Zhen <jzhen@stanford.edu>
2020-09-28 11:02:48 -0700

    Connects to #84. Connects to #83. UI enhancement to highlighted metrics boxes (#85)
    * UI enhancement to highlighted metrics boxes
    * Added handling to resize plot x-axis labels to address overlapping issues in smaller mobile phone viewports
    * Added responsive stylings for dashboard pertinent to different viewport widths

--- src/Dashboard/dashboard.jsx
+++ src/Dashboard/dashboard.jsx
@@ -56,7 +56,7 @@ export function Dashboard({
 
   return (
     <AuthContentContainer classes="Dashboard" expanded={expanded}>
-      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom page-header">
         <div className="page-title">
           <h3>Summary of Animal Study Assays</h3>
         </div>
