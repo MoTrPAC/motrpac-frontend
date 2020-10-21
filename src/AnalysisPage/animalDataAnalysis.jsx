cfc87f1c49b89e274d7605757a3d6cb6805bd525
Jimmy Zhen <jzhen@stanford.edu>
2020-10-20 22:45:03 -0700

    :wrench: Refactored due to UI changes

--- src/AnalysisPage/animalDataAnalysis.jsx
+++ src/AnalysisPage/animalDataAnalysis.jsx
@@ -2,21 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AnimalPhenotypeData from './animalPhenotypeData';
 
-export default function AnimalDataAnalysis({
-  analysis,
-  subAnalysis,
-}) {
-  if (analysis === 'PD') {
-    switch (subAnalysis) {
-      case 'APD':
-        return <AnimalPhenotypeData />;
-      default:
-        return null;
-    }
+export default function AnimalDataAnalysis({ analysis }) {
+  if (analysis === 'PHENOTYPE') {
+    return <AnimalPhenotypeData />;
   }
+
+  return null;
 }
 
 AnimalDataAnalysis.propTypes = {
   analysis: PropTypes.string.isRequired,
-  subAnalysis: PropTypes.string.isRequired,
 };
