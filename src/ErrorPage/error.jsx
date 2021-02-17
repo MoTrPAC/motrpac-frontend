aecc77314b6f188339a7bec5fdebdb4b8aec4440
Jimmy Zhen <jzhen@stanford.edu>
2021-02-17 14:55:50 -0800

    Refactored and simplified component
    The logic to handle Auth0-authenticated users who are not authorized to access data has been moved to the implementation of a Auth0 rule

--- src/ErrorPage/error.jsx
+++ src/ErrorPage/error.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import actions from '../Auth/authActions';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import ContactHelpdesk from '../lib/ui/contactHelpdesk';
 
 /**
@@ -10,17 +8,13 @@ import ContactHelpdesk from '../lib/ui/contactHelpdesk';
  *
  * @returns {Object} JSX representation of the Error page.
  */
-export function ErrorPage({ isAuthenticated, profile, logout }) {
-  const hasAccess = profile.user_metadata && profile.user_metadata.hasAccess;
+export function ErrorPage() {
+  const { isAuthenticated, profile } = useSelector((state) => state.auth);
+  const history = useHistory();
 
-  if (isAuthenticated && hasAccess) {
-    return <Redirect to="/dashboard" />;
+  if (isAuthenticated && profile.user_metadata) {
+    return history.goBack();
   }
-
-  const handleLogout = () => {
-    logout();
-    return <Redirect to="/" />;
-  };
 
   return (
     <div className="col-md-9 col-lg-10 px-4 errorPage">
@@ -57,41 +51,9 @@ export function ErrorPage({ isAuthenticated, profile, logout }) {
             </p>
           </div>
         </div>
-        <div className="home-link">
-          <button
-            type="button"
-            onClick={handleLogout}
-            className="logOutBtn btn btn-primary"
-          >
-            Return home
-          </button>
-        </div>
       </div>
     </div>
   );
 }
 
-ErrorPage.propTypes = {
-  profile: PropTypes.shape({
-    user_metadata: PropTypes.object,
-  }),
-  isAuthenticated: PropTypes.bool,
-  logout: PropTypes.func,
-};
-
-ErrorPage.defaultProps = {
-  profile: {},
-  isAuthenticated: false,
-  logout: null,
-};
-
-const mapStateToProps = (state) => ({
-  profile: state.auth.profile,
-  isAuthenticated: state.auth.isAuthenticated,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  logout: () => dispatch(actions.logout()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ErrorPage);
+export default ErrorPage;
