e300230568db175b5ba04e10c0ac778a92a98226
Jimmy Zhen <jzhen@stanford.edu>
2021-02-18 16:14:10 -0800

    Addressed react-chartjs-2 related test errors upon upgrading react-scripts from v3.4.1 to v4.0.1
    The test error message - Failed to create chart: can't acquire context from the given item

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,9 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 configure({ adapter: new Adapter() });
 
 require('jest-canvas-mock');
+
+jest.mock('react-chartjs-2', () => ({
+  Bar: () => null,
+  Pie: () => null,
+  Doughnut: () => null
+}));
