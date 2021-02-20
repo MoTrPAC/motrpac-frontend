d3a0e68f6c201dbb5136f92b4b7af0ac4e79063f
Jimmy Zhen <jzhen@stanford.edu>
2021-02-19 21:22:36 -0800

    Removed Redux store configs

--- src/ErrorPage/__test__/error.stories.jsx
+++ src/ErrorPage/__test__/error.stories.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { Provider } from 'react-redux';
-import configureStore from '../../App/configureStore';
-import ErrorPage from '../error';
+import { ErrorPage } from '../error';
 import { Navbar } from '../../Navbar/navbar';
 import { Footer } from '../../Footer/footer';
-
-const store = configureStore();
 
 const testUser = require('../../testData/testUser');
 
@@ -17,16 +13,16 @@ const loginAction = {
 
 storiesOf('Error Page', module)
   .addDecorator(story => (
-    <Provider store={store}>
+    <>
       <div className="App">
         <header>
-          <Navbar {...loginAction} profile={testUser} />
+          <Navbar {...loginAction} />
         </header>
         <div className="row justify-content-center mt-5">
           {story()}
         </div>
       </div>
-      <Footer profile={testUser} {...loginAction} />
-    </Provider>
+      <Footer {...loginAction} />
+    </>
   ))
   .add('Default', () => <ErrorPage />);
