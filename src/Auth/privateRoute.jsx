683cc2b62a163351c930343ad944b13ef85e9756
Jimmy Zhen <jzhen@stanford.edu>
2021-02-19 23:02:05 -0800

    Refactored UI handling while fetching is still in progress

--- src/Auth/privateRoute.jsx
+++ src/Auth/privateRoute.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import AnimatedLoadingIcon from '../lib/ui/loading';
 
 function PrivateRoute({ children, ...args }) {
   const { isAuthenticated, isFetching, profile } = useSelector((state) => state.auth);
 
+  function handleAccess() {
+    if (!isAuthenticated && !profile.user_metadata) {
+      return <Redirect exact to="/" />;
+    }
+
+    return <Route component={children} {...args} />;
+  }
+
   return (
     <>
-      {isAuthenticated && !isFetching && profile.user_metadata ? (
-        <Route component={children} {...args} />
-      ) : (
-        <Redirect exact to="/" />
+      {!isFetching ? handleAccess() : (
+        <div className="mt-5 py-5">
+          <AnimatedLoadingIcon isFetching={isFetching} />
+        </div>
       )}
     </>
   );
