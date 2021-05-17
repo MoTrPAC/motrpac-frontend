56bf3fa8bf554f4d2823d72cb8d7e9972fd2fadd
Jimmy Zhen <jzhen@stanford.edu>
2021-05-17 16:44:20 -0700

    Initial commits for components and dictionaries used in pass1b-06 rat training DE genes visualization

---
+++ src/DataExploration/DifferentialExpression/differentialExpressionPage.jsx
@@ -0,0 +1,175 @@
+import React, { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import * as d3 from 'd3';
+import AuthContentContainer from '../../lib/ui/authContentContainer';
+import figureRat from '../../assets/figures/rat-figure-pass1b.svg';
+import FigureAssayLegends from './figureAssayLegends';
+import HeatmapModal from './heatmapModal';
+
+const figureTissueProps = require('./figureTissueProps');
+
+/**
+ * Renders the data exploration page in both
+ * unauthenticated and authenticated states.
+ *
+ * @param {Boolean} isAuthenticated Redux state for user's authentication status.
+ * @param {Boolean} expanded        Redux state for sidebar
+ *
+ * @returns {Object} JSX representation of the data exploration page.
+ */
+function DifferenrialExpression({ isAuthenticated, expanded }) {
+  const [tissue, setTissue] = useState();
+  const [assay, setAssay] = useState();
+  const [showModal, setShowModal] = useState(false);
+  const ref = useRef();
+
+  // Handler to open modal
+  function activateModal(tissueAbbr, assayType) {
+    setTissue(tissueAbbr);
+    setAssay(assayType);
+    setShowModal(true);
+    const body = document.querySelector('body');
+    body.classList.add('modal-open');
+  }
+
+  // Handle to close heatmap modal
+  function dismissModal() {
+    setShowModal(false);
+    const body = document.querySelector('body');
+    body.classList.remove('modal-open');
+  }
+
+  // Draw svg rat figure with labels
+  function renderFigure() {
+    // import svg rat figure as xml
+    d3.xml(figureRat)
+      .then((data) => {
+        const svgFigure = data.documentElement;
+        d3.select(ref.current).node().append(svgFigure);
+        return Promise.resolve(svgFigure);
+      })
+      .then((figure) => {
+        // handle click event on label 'rect'
+        const labelRects = Array.from(
+          document.querySelectorAll('#TissueLabels #Labels rect')
+        );
+        labelRects.forEach((rect) => {
+          const selectedLabel = d3.select(rect);
+          selectedLabel.on('click', (event) => {
+            setTissue(figureTissueProps[selectedLabel.attr('class')].label);
+            setAssay('');
+          });
+        });
+        // handle click event on label text 'path'
+        const labelTextGroups = Array.from(
+          document.querySelectorAll('#TissueLabels #Labels g')
+        );
+        labelTextGroups.forEach((group) => {
+          const selectedLabelText = d3.select(group);
+          selectedLabelText.selectAll('path').on('click', (event) => {
+            setTissue(figureTissueProps[selectedLabelText.attr('class')].label);
+            setAssay('');
+          });
+          selectedLabelText.selectAll('path').on('mouseover', () => {
+            const rect = document.querySelector(
+              `#TissueLabels #Labels rect[data-name=${selectedLabelText.attr(
+                'class'
+              )}]`
+            );
+            d3.select(rect).classed('hover', true);
+          });
+          selectedLabelText.selectAll('path').on('mouseout', () => {
+            const rect = document.querySelector(
+              `#TissueLabels #Labels rect[data-name=${selectedLabelText.attr(
+                'class'
+              )}]`
+            );
+            d3.select(rect).classed('hover', false);
+          });
+        });
+        // handle click event on assay legend symbols
+        const assaySymbols = Array.from(
+          document.querySelectorAll(
+            '#AssayLegends rect, #AssayLegends circle, #AssayLegends polygon'
+          )
+        );
+        assaySymbols.forEach((symbol) => {
+          const selectedAssay = d3.select(symbol);
+          selectedAssay.on('click', (event) => {
+            activateModal(
+              d3.select(event.currentTarget.parentNode).attr('class'),
+              selectedAssay.attr('class')
+            );
+          });
+        });
+      });
+  }
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    renderFigure();
+  }, []);
+
+  const pageContent = (
+    <>
+      <div className="page-title pt-3 pb-2 border-bottom">
+        <h3>Differential Expression</h3>
+      </div>
+      <div className="main-content-container d-flex align-items-start">
+        <div
+          className="figure-svg-container my-3 text-left"
+          id="figure-svg-container"
+          ref={ref}
+        />
+        <FigureAssayLegends />
+        <div className="results-container mt-5 ml-5 p-3 border border-dark rounded">
+          <p>Selected tissue: {tissue}</p>
+          <p>Selected assay: {assay}</p>
+        </div>
+      </div>
+      {showModal && (
+        <HeatmapModal
+          tissue={tissue}
+          assay={assay}
+          showModal={showModal}
+          dismissModal={dismissModal}
+        />
+      )}
+    </>
+  );
+
+  if (!isAuthenticated) {
+    return (
+      <div className="w-100 px-4 differentialExpressionPage">
+        <div className="container-fluid">{pageContent}</div>
+      </div>
+    );
+  }
+
+  return (
+    <AuthContentContainer
+      classes="differentialExpressionPage"
+      expanded={expanded}
+    >
+      <div>{pageContent}</div>
+    </AuthContentContainer>
+  );
+}
+
+DifferenrialExpression.propTypes = {
+  isAuthenticated: PropTypes.bool,
+  expanded: PropTypes.bool,
+};
+
+DifferenrialExpression.defaultProps = {
+  isAuthenticated: false,
+  expanded: false,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  expanded: state.sidebar.expanded,
+});
+
+export default connect(mapStateToProps)(DifferenrialExpression);
---
+++ src/DataExploration/DifferentialExpression/heatmapModal.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Heatmap from './heatmap';
+
+const figureTissueProps = require('./figureTissueProps');
+const figureAssayProps = require('./figureAssayProps');
+
+/**
+ * Renders bootstrap modal containing heatmap of top 50 DE genes
+ * in pass1b-06 rat training data
+ *
+ * @param {String} tissue         Tissue abbreviation
+ * @param {String} assay          Assay
+ * @param {Function} dismissModal Handler to close modal
+ *
+ * @returns {Object} JSX representation of the heatmap modal
+ */
+function HeatmapModal({ tissue, assay, dismissModal }) {
+  // Transform tissue abbreviation to full name
+  function renderTissueName() {
+    let tissueName = '';
+    if (tissue && tissue.length && tissue in figureTissueProps) {
+      tissueName = figureTissueProps[tissue].label;
+    }
+    return tissueName;
+  }
+
+  // Transform assay type to descriptive label
+  function renderAssayLabel() {
+    let assayLabel = '';
+    if (assay && assay.length && assay in figureAssayProps) {
+      assayLabel = figureAssayProps[assay].label;
+    }
+    return assayLabel;
+  }
+
+  return (
+    <>
+      <div
+        className="modal fade show"
+        id="heatmapModal"
+        tabIndex="-1"
+        aria-labelledby="heatmapModalTitle"
+        aria-hidden="false"
+        aria-modal="true"
+        role="dialog"
+        data-backdrop="static"
+        data-keyboard="false"
+      >
+        <div className="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title" id="heatmapModalTitle">
+                Top 50 Expressed Genes in{' '}
+                {`${renderAssayLabel()} for ${renderTissueName()}`}
+              </h4>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+                onClick={dismissModal}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <Heatmap tissue={tissue} assay={assay} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="modal-backdrop fade show" />
+    </>
+  );
+}
+
+HeatmapModal.propTypes = {
+  tissue: PropTypes.string,
+  assay: PropTypes.string,
+  showModal: PropTypes.bool,
+  dismissModal: PropTypes.func,
+};
+
+HeatmapModal.defaultProps = {
+  tissue: '',
+  assay: '',
+  showModal: false,
+  dismissModal: null,
+};
+
+export default HeatmapModal;
