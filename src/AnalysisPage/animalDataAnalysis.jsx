4407e873b1a9f47462b2f33fab7af98b6d1f213b
Jimmy Zhen <jzhen@stanford.edu>
2022-01-04 23:04:17 -0800

    Added gene-centric results module

--- src/AnalysisPage/animalDataAnalysis.jsx
+++ src/AnalysisPage/animalDataAnalysis.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Pass1b06PhenotypeAnimalConnected from './pass1b06PhenotypeAnimal';
 import DifferenrialExpressionConnected from '../DataExploration/DifferentialExpression/differentialExpressionPage';
+import GeneCentricSearch from '../DataExploration/GeneCentricResults/geneCentricSearch';
 
 export default function AnimalDataAnalysis({ analysis }) {
   switch (analysis) {
@@ -9,6 +10,8 @@ export default function AnimalDataAnalysis({ analysis }) {
       return <Pass1b06PhenotypeAnimalConnected />;
     case 'DEA':
       return <DifferenrialExpressionConnected />;
+    case 'GENE_CENTRIC_RESULTS':
+      return <GeneCentricSearch />;
     default:
       return null;
   }
