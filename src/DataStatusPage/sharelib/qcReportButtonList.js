10fb3371d0bdb59ece866a4bb7960d32b2c36f31
Jimmy Zhen <jzhen@stanford.edu>
2022-02-10 00:09:28 -0800

    :tada: Initial commit of QC report nav button group list

---
+++ src/DataStatusPage/sharelib/qcReportButtonList.js
@@ -0,0 +1,32 @@
+const qcReportButtonList = [
+  {
+    qcReport: 'metabolomics',
+    buttonLabel: 'Metabolomics',
+  },
+  {
+    qcReport: 'proteomics',
+    buttonLabel: 'Proteomics',
+  },
+  {
+    qcReport: 'rnaseq',
+    buttonLabel: 'RNA-seq',
+  },
+  {
+    qcReport: 'rrbs',
+    buttonLabel: 'RRBS',
+  },
+  {
+    qcReport: 'atacseq',
+    buttonLabel: 'ATAC-seq',
+  },
+  {
+    qcReport: 'immunoassay',
+    buttonLabel: 'Immunoassay',
+  },
+  {
+    qcReport: 'help',
+    buttonLabel: 'Help',
+  },
+];
+
+export default qcReportButtonList;
