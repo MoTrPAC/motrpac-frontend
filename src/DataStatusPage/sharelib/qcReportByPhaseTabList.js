af043c105d3f08634fb6683ebca63ad3304f9aec
Jimmy Zhen <jzhen@stanford.edu>
2022-02-15 15:55:22 -0800

    :tada: Initial commit of list of phase metadata for rendering tab UIs

---
+++ src/DataStatusPage/sharelib/qcReportByPhaseTabList.js
@@ -0,0 +1,24 @@
+const qcReportByPhaseTabList = [
+  {
+    navLinkId: 'nav_link_pass1a',
+    navLinkHref: 'tab_pane_pass1a',
+    navLinkLabel: 'PASS1A',
+  },
+  {
+    navLinkId: 'nav_link_pass1b',
+    navLinkHref: 'tab_pane_pass1b',
+    navLinkLabel: 'PASS1B',
+  },
+  {
+    navLinkId: 'nav_link_pass1c',
+    navLinkHref: 'tab_pane_pass1c',
+    navLinkLabel: 'PASS1C',
+  },
+  {
+    navLinkId: 'nav_link_human',
+    navLinkHref: 'tab_pane_human',
+    navLinkLabel: 'Human',
+  },
+];
+
+export default qcReportByPhaseTabList;
