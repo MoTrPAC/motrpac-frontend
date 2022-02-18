bd913c4c6b1c7321d3d6323f6a41f83fe6049746
Jimmy Zhen <jzhen@stanford.edu>
2022-02-17 17:55:14 -0800

    :tada: Initial commit of QC report help link component

---
+++ src/DataStatusPage/sharelib/qcReportHelpLink.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function QcReportHelpLink({ qcReportViewChange }) {
+  return (
+    <div className="help-page-link d-flex align-items-center mb-4">
+      <span className="material-icons">help_center</span>
+      <button
+        type="button"
+        className="btn btn-link ml-1"
+        onClick={() => qcReportViewChange('help')}
+      >
+        See terms and definitions
+      </button>
+    </div>
+  );
+}
+
+QcReportHelpLink.propTypes = {
+  qcReportViewChange: PropTypes.func,
+};
+
+QcReportHelpLink.defaultProps = {
+  qcReportViewChange: null,
+};
+
+export default QcReportHelpLink;
