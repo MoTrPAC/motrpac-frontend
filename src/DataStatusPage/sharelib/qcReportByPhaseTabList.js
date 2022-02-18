e7acded041cf14f8c2a8b296dbe92a2b9cc42c1d
Jimmy Zhen <jzhen@stanford.edu>
2022-02-17 16:21:43 -0800

    Updated phase tabs' metadata

--- src/DataStatusPage/sharelib/qcReportByPhaseTabList.js
+++ src/DataStatusPage/sharelib/qcReportByPhaseTabList.js
@@ -1,23 +1,21 @@
 const qcReportByPhaseTabList = [
   {
-    navLinkId: 'nav_link_pass1a',
-    navLinkHref: 'tab_pane_pass1a',
-    navLinkLabel: 'PASS1A',
-  },
-  {
     navLinkId: 'nav_link_pass1b',
     navLinkHref: 'tab_pane_pass1b',
     navLinkLabel: 'PASS1B',
+    phases: ['pass1b'],
   },
   {
-    navLinkId: 'nav_link_pass1c',
-    navLinkHref: 'tab_pane_pass1c',
-    navLinkLabel: 'PASS1C',
+    navLinkId: 'nav_link_pass1ac',
+    navLinkHref: 'tab_pane_pass1ac',
+    navLinkLabel: 'PASS1A/1C',
+    phases: ['pass1a', 'pass1c'],
   },
   {
     navLinkId: 'nav_link_human',
     navLinkHref: 'tab_pane_human',
     navLinkLabel: 'Human',
+    phases: ['human'],
   },
 ];
 
