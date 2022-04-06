194f564e2db498d5b169f3cc77ad1c11fbebdec6
Jimmy Zhen <jzhen@stanford.edu>
2022-04-06 02:31:12 -0700

    Initial commit of TissueComparison component temporarily used to show pass1b-06 graphical analysis reports

---
+++ src/AnalysisPage/TissueComparison/tissueComparisonPage.jsx
@@ -0,0 +1,81 @@
+import React, { useRef, useState } from 'react';
+import IframeResizer from 'iframe-resizer-react';
+
+const reportTissues = {
+  HYPOTH: 'Hypothalamus',
+  HIPPOC: 'Hippocampus',
+  SKM_GN: 'Gastrocnemius',
+  KIDNEY: 'Kidney',
+  PLASMA: 'Blood Plasma',
+  BLOOD: 'Whole Blood',
+};
+
+export default function TissueComparison() {
+  const iframeRef = useRef(null);
+  const [tissue, setTissue] = useState('HYPOTH');
+
+  return (
+    <div className="tissueComparisonPage">
+      <div className="main-content-container d-flex align-items-start">
+        <ReportControls tissue={tissue} toggleReport={setTissue} />
+        <IframeResizer
+          forwardRef={iframeRef}
+          heightCalculationMethod="max"
+          src={`/static-assets/graphical-analysis-reports/graphical-analysis-results_${tissue}.html`}
+          style={{
+            height: '75vh',
+            width: '1px',
+            minWidth: '1400px',
+            border: 'none',
+          }}
+          scrolling
+          sizeHeight
+          sizeWidth
+        />
+      </div>
+    </div>
+  );
+}
+
+function ReportControls({ tissue, toggleReport }) {
+  const tissueKeys = Object.keys(reportTissues);
+
+  return (
+    <div className="controlPanelContainer mr-5 mt-3">
+      <div className="controlPanel">
+        <div className="controlRow d-flex align-items-center">
+          <div className="controlLabel mr-2">Tissues:</div>
+          <div className="dropdown">
+            <button
+              className="btn btn-sm btn-primary dropdown-toggle"
+              type="button"
+              id="reportViewMenu"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              {reportTissues[tissue]}
+            </button>
+            <div
+              className="dropdown-menu animate slideIn"
+              aria-labelledby="reportViewMenu"
+            >
+              {tissueKeys.map((key) => {
+                return (
+                  <button
+                    key={key}
+                    className="dropdown-item"
+                    type="button"
+                    onClick={toggleReport.bind(this, key)}
+                  >
+                    {reportTissues[key]}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
