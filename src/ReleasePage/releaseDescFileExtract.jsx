e4e1c310a682b85f41e1b6fd5ff298d7f27fd463
Jimmy Zhen <jzhen@stanford.edu>
2022-08-10 18:09:02 -0700

    Reinstated the compressed file extraction text for external data release description

--- src/ReleasePage/releaseDescFileExtract.jsx
+++ src/ReleasePage/releaseDescFileExtract.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import EmailLink from '../lib/ui/emailLink';
+import ExternalLink from '../lib/ui/externalLink';
 
 function ReleaseDescFileExtract() {
   return (
-    <p className="release-description">
-      To request access to raw files of the different omics data, please contact{' '}
-      <EmailLink
-        mailto="motrpac-data-requests@lists.stanford.edu"
-        label="MoTrPAC Data Requests"
-      />{' '}
-      and specify the omics, tissues, and assays in which you are interested.
-    </p>
+    <>
+      <p className="release-description file-extraction-instruction">
+        All bundled datasets are compressed (*.tar.gz) and need to be extracted
+        upon downloading. For Mac users, double-clicking a downloaded *.tar.gz
+        file will extract it. For Windows users, use{' '}
+        <ExternalLink to="https://www.winzip.com" label="WinZip" /> or{' '}
+        <ExternalLink to="https://www.7-zip.org" label="7-Zip" /> to extract the
+        compressed file.
+      </p>
+      <p className="release-description">
+        To request access to raw files of the different omics data, please
+        contact{' '}
+        <EmailLink
+          mailto="motrpac-data-requests@lists.stanford.edu"
+          label="MoTrPAC Data Requests"
+        />{' '}
+        and specify the omics, tissues, and assays in which you are interested.
+      </p>
+    </>
   );
 }
 
