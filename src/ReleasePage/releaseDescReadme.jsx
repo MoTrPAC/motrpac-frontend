4c896fb2b43ea5d31e44df5938bc70b3c8e99dd5
Jimmy Zhen <jzhen@stanford.edu>
2022-08-10 17:44:42 -0700

    Removed the custom README text for prior internal data release 2.0

--- src/ReleasePage/releaseDescReadme.jsx
+++ src/ReleasePage/releaseDescReadme.jsx
@@ -3,35 +3,8 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import EmailLink from '../lib/ui/emailLink';
 
-function ReleaseDescReadme({ currentView, releaseVersion, fileLocation }) {
-  if (currentView === 'internal' && releaseVersion === '2.0') {
-    return (
-      <p className="release-description">
-        A{' '}
-        <a
-          href={fileLocation}
-          className="inline-link-with-icon"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          README
-          <i className="material-icons readme-file-icon">description</i>
-        </a>{' '}
-        document has been provided summarizing the data available in this
-        release. To learn more about the prior released dataset, please see the{' '}
-        <Link to="/announcements" className="inline-link">
-          recent announcement
-        </Link>
-        . For any technical issues, please contact us at{' '}
-        <EmailLink
-          mailto="motrpac-helpdesk@lists.stanford.edu"
-          label="MoTrPAC Helpdesk"
-        />
-      </p>
-    );
-  }
-
-  if (currentView === 'internal' && releaseVersion !== '2.0') {
+function ReleaseDescReadme({ currentView, fileLocation }) {
+  if (currentView === 'internal') {
     return (
       <p className="release-description">
         Please refer to the{' '}
@@ -78,7 +51,6 @@ function ReleaseDescReadme({ currentView, releaseVersion, fileLocation }) {
 
 ReleaseDescReadme.propTypes = {
   currentView: PropTypes.string.isRequired,
-  releaseVersion: PropTypes.string.isRequired,
   fileLocation: PropTypes.string.isRequired,
 };
 
