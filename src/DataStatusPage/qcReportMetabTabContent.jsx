985d5cb15a57d92316da736d030aa3e6413e4dfa
Jimmy Zhen <jzhen@stanford.edu>
2022-10-18 18:24:10 -0700

    Initial commit of metabolomics QC data sub tabs content component

---
+++ src/DataStatusPage/qcReportMetabTabContent.jsx
@@ -0,0 +1,61 @@
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import {
+  transformData,
+  commonReportPropType,
+  metabProtReportPropType,
+  metabProtRawDataReportPropType,
+  metabProcessedDataTableColumns,
+  metabRawDataTableColumns,
+} from './common';
+import QcReportDataTable from './sharelib/qcReportDataTable';
+
+/**
+ * Sets up table column headers and renders the table component
+ *
+ * @returns {object} The data qc status table component
+ */
+function QcReportMetabTabContent({ qcData, qcDataRaw, qcFiles }) {
+  // Define table column headers
+  const processedDataColumns = useMemo(
+    () => metabProcessedDataTableColumns,
+    []
+  );
+  const processedQcData = useMemo(
+    () => transformData(qcData, 'processed', 'metab'),
+    [qcData]
+  );
+  const rawDataColumns = useMemo(() => metabRawDataTableColumns, []);
+  const rawQcData = useMemo(
+    () => transformData(qcDataRaw, 'raw', 'metab'),
+    [qcDataRaw]
+  );
+  return (
+    <QcReportDataTable
+      columns={qcFiles === 'raw' ? rawDataColumns : processedDataColumns}
+      data={qcFiles === 'raw' ? rawQcData : processedQcData}
+    />
+  );
+}
+
+QcReportMetabTabContent.propTypes = {
+  qcData: PropTypes.arrayOf(
+    PropTypes.shape({
+      ...commonReportPropType,
+      ...metabProtReportPropType,
+    })
+  ).isRequired,
+  qcDataRaw: PropTypes.arrayOf(
+    PropTypes.shape({
+      ...commonReportPropType,
+      ...metabProtRawDataReportPropType,
+    })
+  ).isRequired,
+  qcFiles: PropTypes.string,
+};
+
+QcReportMetabTabContent.defaultProps = {
+  qcFiles: 'raw',
+};
+
+export default QcReportMetabTabContent;
