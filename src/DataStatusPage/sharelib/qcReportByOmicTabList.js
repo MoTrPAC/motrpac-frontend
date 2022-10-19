20cd5c838c0037905919d13bda951a0d702f33ab
Jimmy Zhen <jzhen@stanford.edu>
2022-10-18 18:20:04 -0700

    Initial commit for metabolomics and proteomics QC data sub tabs

---
+++ src/DataStatusPage/sharelib/qcReportByOmicTabList.js
@@ -0,0 +1,35 @@
+export const qcReportMetabTabs = [
+  {
+    navLinkId: 'nav_link_raw',
+    navLinkHref: 'tab_pane_raw',
+    navLinkLabel: 'Raw',
+    qcFiles: 'raw',
+  },
+  {
+    navLinkId: 'nav_link_processed',
+    navLinkHref: 'tab_pane_processed',
+    navLinkLabel: 'Processed',
+    qcFiles: 'processed',
+  },
+];
+
+export const qcReportProtTabs = [
+  {
+    navLinkId: 'nav_link_raw',
+    navLinkHref: 'tab_pane_raw',
+    navLinkLabel: 'Raw',
+    qcFiles: 'raw',
+  },
+  {
+    navLinkId: 'nav_link_pass1ac',
+    navLinkHref: 'tab_pane_pass1ac',
+    navLinkLabel: 'Processed by CAS',
+    qcFiles: 'processed_by_cas',
+  },
+  {
+    navLinkId: 'nav_link_human',
+    navLinkHref: 'tab_pane_human',
+    navLinkLabel: 'Processed by BIC',
+    qcFiles: 'processed_by_bic',
+  },
+];
