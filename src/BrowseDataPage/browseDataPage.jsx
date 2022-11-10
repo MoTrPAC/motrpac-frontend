fe32827fc88717734cb7f705a265a4b90af1e464
Jimmy Zhen <jzhen@stanford.edu>
2022-11-09 23:43:46 -0800

    Added panel to feature links to JupyterHub

--- src/BrowseDataPage/browseDataPage.jsx
+++ src/BrowseDataPage/browseDataPage.jsx
@@ -41,77 +41,117 @@ export function BrowseDataPage({
           <h3 className="mb-0">Browse Data</h3>
         </div>
       </div>
-      <div className="mt-3 mb-4 browse-data-summary pb-3 border-bottom">
-        <span className="emphasis font-weight-bold">
-          Browse and download the experimental data of 6-month old rats by
-          tissue, assay, or omics. The files accessible and downloadable on this
-          page consist of a variety of data types for the animal phase focusing
-          on defining molecular changes that occur in rats after up to 8 weeks
-          of endurance training (PASS1B):
-        </span>
-        <ul className="mt-1 mb-2">
-          <li>Assay-specific differential analysis and normalized data</li>
-          <li>
-            Assay-specific quantitative results, experiment metadata, and QA/QC
-            reports
-          </li>
-          <li>
-            Cross-platform merged metabolomics data tables for named metabolites
-          </li>
-          <li>Phenotypic data</li>
-        </ul>
-        <div className="collapse mb-2" id="collapseSummary">
+      <div className="browse-data-summary-container row mb-2">
+        <div className="browse-data-summary pb-3 col-md-8">
           <span className="emphasis font-weight-bold">
-            The current version of the 6-month old rat data for endurance
-            training also include:
+            Browse and download the experimental data of 6-month old rats by
+            tissue, assay, or omics. The files accessible and downloadable on
+            this page consist of a variety of data types for the animal phase
+            focusing on defining molecular changes that occur in rats after up
+            to 8 weeksof endurance training (PASS1B):
           </span>
           <ul className="mt-1 mb-2">
+            <li>Assay-specific differential analysis and normalized data</li>
             <li>
-              All PASS1B 6-month experimental/sample metadata from the very last
-              consortium release
+              Assay-specific quantitative results, experiment metadata, and
+              QA/QC reports
             </li>
             <li>
-              Updated PASS1B 6-month phenotypic data since the very last
-              consortium release
+              Cross-platform merged metabolomics data tables for named
+              metabolites
             </li>
-            <li>
-              Experimental data of additional tissues and assays not available
-              in the very last consortium release
-            </li>
+            <li>Phenotypic data</li>
           </ul>
-          While the raw files are not available for direct download through the
-          Data Hub portal, they can be accessed through command-line with
-          granted permission. Please contact{' '}
-          <a href="mailto:motrpac-data-requests@lists.stanford.edu">
-            MoTrPAC Data Requests
-          </a>{' '}
-          if you need access to the raw files. A{' '}
-          <a
-            href="https://docs.google.com/document/d/1bdXcYQLZ65GpJKTjf9XwRxhrfHJSD9NIqCxhG6icL8U"
-            className="inline-link-with-icon"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            README
-            <i className="material-icons readme-file-icon">description</i>
-          </a>{' '}
-          document is available for reference on the data included in the very
-          last consortium release.
+          <div className="collapse mb-2" id="collapseSummary">
+            <span className="emphasis font-weight-bold">
+              The current version of the 6-month old rat data for endurance
+              training also include:
+            </span>
+            <ul className="mt-1 mb-2">
+              <li>
+                All PASS1B 6-month experimental/sample metadata from the very
+                last consortium release
+              </li>
+              <li>
+                Updated PASS1B 6-month phenotypic data since the very last
+                consortium release
+              </li>
+              <li>
+                Experimental data of additional tissues and assays not available
+                in the very last consortium release
+              </li>
+            </ul>
+            While the raw files are not available for direct download through
+            the Data Hub portal, they can be accessed through command-line with
+            granted permission. Please contact{' '}
+            <a href="mailto:motrpac-data-requests@lists.stanford.edu">
+              MoTrPAC Data Requests
+            </a>{' '}
+            if you need access to the raw files. A{' '}
+            <a
+              href="https://docs.google.com/document/d/1bdXcYQLZ65GpJKTjf9XwRxhrfHJSD9NIqCxhG6icL8U"
+              className="inline-link-with-icon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              README
+              <i className="material-icons readme-file-icon">description</i>
+            </a>{' '}
+            document is available for reference on the data included in the very
+            last consortium release.
+          </div>
+          <div className="border-bottom pb-3">
+            <button
+              className="btn btn-link btn-sm show-more-link d-flex align-items-center"
+              type="button"
+              data-toggle="collapse"
+              data-target="#collapseSummary"
+              aria-expanded="false"
+              aria-controls="collapseSummary"
+              onClick={toggleShowMoreSummary}
+            >
+              <span>Show {!showMoreSummary ? 'more' : 'less'}</span>
+              <i className="material-icons">
+                {!showMoreSummary ? 'expand_more' : 'expand_less'}
+              </i>
+            </button>
+          </div>
         </div>
-        <button
-          className="btn btn-link btn-sm show-more-link d-flex align-items-center"
-          type="button"
-          data-toggle="collapse"
-          data-target="#collapseSummary"
-          aria-expanded="false"
-          aria-controls="collapseSummary"
-          onClick={toggleShowMoreSummary}
-        >
-          <span>Show {!showMoreSummary ? 'more' : 'less'}</span>
-          <i className="material-icons">
-            {!showMoreSummary ? 'expand_more' : 'expand_less'}
-          </i>
-        </button>
+        <div className="col-md-4 mt-1">
+          <div className="card p-3 shadow-sm">
+            <div className="card-body">
+              <div className="h-100 d-flex align-items-start">
+                <div className="feature-icon mr-3">
+                  <span className="material-icons">hub</span>
+                </div>
+                <div className="feature-summary">
+                  <h4 className="card-title">
+                    <a
+                      href="https://collab.motrpac-data.org/hub/oauth_login?next=%2Fhub%2Fhome"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      MoTrPAC Collab
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    A multi-user Jupyter notebook workspace containing a
+                    collection of notebooks and visualizations for in-depth data
+                    exploration. Read the{' '}
+                    <a
+                      href="/static-assets/MoTrPAC_Collab_User_Guide.pdf"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      user guide
+                    </a>{' '}
+                    to learn more.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="browse-data-container row">
         <BrowseDataFilter
