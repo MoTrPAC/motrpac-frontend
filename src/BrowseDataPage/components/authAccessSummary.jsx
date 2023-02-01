450d42b57fed7aaea0459aa7f7d032c6e55a08fe
Jimmy Zhen <jzhen@stanford.edu>
2023-01-31 23:06:04 -0800

    Updated summary content

--- src/BrowseDataPage/components/authAccessSummary.jsx
+++ src/BrowseDataPage/components/authAccessSummary.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import EmailLink from '../../lib/ui/emailLink';
+import ExternalLink from '../../lib/ui/externalLink';
 
 function AuthAccessBrowseDataSummary() {
   return (
@@ -8,7 +11,18 @@ function AuthAccessBrowseDataSummary() {
         assay, or omics. The files accessible and downloadable here consist of a
         variety of data types for the animal's acute exercise (PASS1A) and
         endurance training (PASS1B) phases focusing on defining molecular
-        changes that occur in rats.
+        changes that occur in rats. To learn more about PASS1B study, see the{' '}
+        <ExternalLink
+          to="https://www.biorxiv.org/content/10.1101/2022.09.21.508770v1"
+          label="MoTrPAC Endurance Exercise Training Animal Study Landscape Preprint"
+        />{' '}
+        as well as the <Link to="/methods">documentation</Link> on animal study
+        protocols. Also check out the{' '}
+        <ExternalLink
+          to="https://www.cell.com/cell/fulltext/S0092-8674(20)30691-7"
+          label="first published MoTrPAC paper"
+        />{' '}
+        that provides more information on the entire study.
       </div>
       <div className="browse-data-summary-content col-4 col-md-4">
         <div className="bd-callout bd-callout-info">
@@ -29,9 +43,10 @@ function AuthAccessBrowseDataSummary() {
             <span className="font-weight-bold">Note:</span> Raw files are not
             currently available for direct download through the Data Hub portal.
             Please submit your requests to{' '}
-            <a href="mailto:motrpac-data-requests@lists.stanford.edu">
-              MoTrPAC Data Requests
-            </a>{' '}
+            <EmailLink
+              mailto="motrpac-data-requests@lists.stanford.edu"
+              label="MoTrPAC Data Requests"
+            />{' '}
             and specify the relevant tissues/assays if you would like to get
             access to the raw files.
           </p>
--- src/BrowseDataPage/components/openAccessSummary.jsx
+++ src/BrowseDataPage/components/openAccessSummary.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import EmailLink from '../../lib/ui/emailLink';
+import ExternalLink from '../../lib/ui/externalLink';
 import OpenAccessBundleDownloads from './openAccessBundleDownloads';
 
 function OpenAccessBrowseDataSummary() {
@@ -7,8 +10,20 @@ function OpenAccessBrowseDataSummary() {
       <div className="lead col-12">
         Browse and download the experimental data of 6-month old rats by tissue,
         assay, or omics. The files accessible and downloadable here consist of a
-        variety of data types for the animal's endurance training (PASS1B)
-        phases focusing on defining molecular changes that occur in rats.
+        variety of data types for the animal's endurance training phases
+        focusing on defining molecular changes that occur in rats. To learn more
+        about this study, see the{' '}
+        <ExternalLink
+          to="https://www.biorxiv.org/content/10.1101/2022.09.21.508770v1"
+          label="MoTrPAC Endurance Exercise Training Animal Study Landscape Preprint"
+        />{' '}
+        as well as the <Link to="/methods">documentation</Link> on animal study
+        protocols. Also check out the{' '}
+        <ExternalLink
+          to="https://www.cell.com/cell/fulltext/S0092-8674(20)30691-7"
+          label="first published MoTrPAC paper"
+        />{' '}
+        that provides more information on the entire study.
       </div>
       <div className="browse-data-summary-content col-6 col-md-6">
         <div className="bd-callout bd-callout-info">
@@ -29,9 +44,10 @@ function OpenAccessBrowseDataSummary() {
             <span className="font-weight-bold">Note:</span> Raw files are not
             currently available for direct download through the Data Hub portal.
             Please submit your requests to{' '}
-            <a href="mailto:motrpac-data-requests@lists.stanford.edu">
-              MoTrPAC Data Requests
-            </a>{' '}
+            <EmailLink
+              mailto="motrpac-data-requests@lists.stanford.edu"
+              label="MoTrPAC Data Requests"
+            />{' '}
             and specify the relevant tissues/assays if you would like to get
             access to the raw files.
           </p>
