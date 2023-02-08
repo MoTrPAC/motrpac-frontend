d9cf2cf1c58c2b9a55046cd4550b03b508048bcb
Jimmy Zhen <jzhen@stanford.edu>
2023-02-08 03:10:21 -0800

    Updated page layout

--- src/ContactPage/contact.jsx
+++ src/ContactPage/contact.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import PageTitle from '../lib/ui/pageTitle';
 import ContactHelpdesk from '../lib/ui/contactHelpdesk';
-import AuthContentContainer from '../lib/ui/authContentContainer';
 
 /**
  * Renders the Contact Us page in both
@@ -13,12 +11,10 @@ import AuthContentContainer from '../lib/ui/authContentContainer';
  *
  * @returns {Object} JSX representation of the Contact Us page.
  */
-export function Contact({ isAuthenticated, expanded }) {
+function Contact() {
   const pageContent = (
     <>
-      <div className="page-title pt-3 pb-2 border-bottom">
-        <h3>Contact Us</h3>
-      </div>
+      <PageTitle title="Contact Us" />
       <div className="card-deck contact-content-container">
         <div className="card mb-4 shadow-sm">
           <h5 className="card-header">Accessing Data</h5>
@@ -63,34 +59,11 @@ export function Contact({ isAuthenticated, expanded }) {
     </>
   );
 
-  if (!isAuthenticated) {
-    return (
-      <div className="col-md-9 col-lg-10 px-4 contactPage">
-        <div className="container">{pageContent}</div>
-      </div>
-    );
-  }
-
   return (
-    <AuthContentContainer classes="contactPage" expanded={expanded}>
+    <div className="contactPage px-3 px-md-4 mb-3 container">
       <div>{pageContent}</div>
-    </AuthContentContainer>
+    </div>
   );
 }
 
-Contact.propTypes = {
-  isAuthenticated: PropTypes.bool,
-  expanded: PropTypes.bool,
-};
-
-Contact.defaultProps = {
-  isAuthenticated: false,
-  expanded: false,
-};
-
-const mapStateToProps = (state) => ({
-  isAuthenticated: state.auth.isAuthenticated,
-  expanded: state.sidebar.expanded,
-});
-
-export default connect(mapStateToProps)(Contact);
+export default Contact;
