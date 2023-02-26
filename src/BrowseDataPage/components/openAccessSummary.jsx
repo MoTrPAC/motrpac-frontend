3905742399f8ca449bf0c2d87d7f6f5690dd523b
Jimmy Zhen <jzhen@stanford.edu>
2023-02-26 14:14:43 -0800

    Reimplemented bundled data sets download UIs with modal to show additional bundles

--- src/BrowseDataPage/components/openAccessSummary.jsx
+++ src/BrowseDataPage/components/openAccessSummary.jsx
@@ -56,7 +56,58 @@ function OpenAccessBrowseDataSummary() {
       </div>
       <div className="browse-data-summary-content col-6 col-md-6">
         <div className="bd-callout bd-callout-warning">
-          <h4>Bundled Data Sets</h4>
+          <h4 className="bundle-data-callout-title-container d-flex align-items-center justify-content-between">
+            <div className="bundle-data-callout-title">Bundled Data Sets</div>
+            <div className="bundle-data-callout-show-more-button">
+              <button
+                type="button"
+                className="btn btn-link"
+                data-toggle="modal"
+                data-target="#openAccessBundleDataDownloadModal"
+              >
+                Show more
+              </button>
+            </div>
+          </h4>
+          {/* bundle data download modal */}
+          <div
+            className="modal fade"
+            id="openAccessBundleDataDownloadModal"
+            data-backdrop="static"
+            data-keyboard="false"
+            tabIndex="-1"
+            aria-labelledby="openAccessBundleDataDownloadModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog modal-dialog-scrollable modal-xl">
+              <div className="modal-content">
+                <div className="modal-header d-flex align-items-center pb-3">
+                  <div>
+                    <h3
+                      className="modal-title text-black"
+                      id="openAccessBundleDataDownloadModalLabel"
+                    >
+                      Bundled Data Sets
+                    </h3>
+                    <p className="lead">
+                      Endurance Exercise Training Young Adult Rats (6 months)
+                    </p>
+                  </div>
+                  <button
+                    type="button"
+                    className="btn btn-secondary d-flex align-items-center modal-close-button"
+                    data-dismiss="modal"
+                  >
+                    <span className="material-icons">close</span>
+                    <span className="ml-1 button-text">Close</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                  <OpenAccessBundleDownloads sets="all" />
+                </div>
+              </div>
+            </div>
+          </div>
           <OpenAccessBundleDownloads />
         </div>
       </div>
