be3ba9717923b606fb028332feab47f883c9993d
Jimmy Zhen <jzhen@stanford.edu>
2023-02-26 14:12:55 -0800

    Reimplemented UIs for bundled data sets on data download page and moved bundled data sets metadata to external file

--- src/BrowseDataPage/components/openAccessBundleDownloads.jsx
+++ src/BrowseDataPage/components/openAccessBundleDownloads.jsx
@@ -1,82 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import BundleDataTypes from './bundleDataTypes';
+import BundleDownloadButton from './bundleDownloadButton';
 
-const bundleData = [
-  {
-    type: 'transcriptomics',
-    phase: 'pass1b-06',
-    title: 'Transcriptomics data (metadata, QC and quantitative results)',
-    object_zipfile: 'motrpac_pass1b-06_transcriptomics.tar.gz',
-    object_zipfile_size: '176.34 MB',
-  },
-  {
-    type: 'epigenomics',
-    phase: 'pass1b-06',
-    title:
-      'Epigenomics data (RRBS and ATAC-seq metadata, QC and quantitative results)',
-    object_zipfile: 'motrpac_pass1b-06_epigenomics.tar.gz',
-    object_zipfile_size: '17.90 MB',
-  },
-  {
-    type: 'metabolomics-targeted',
-    phase: 'pass1b-06',
-    title: 'Metabolomics Targeted data (metadata, QC and quantitative results)',
-    object_zipfile: 'motrpac_pass1b-06_metabolomics-targeted.tar.gz',
-    object_zipfile_size: '36.93 MB',
-  },
-  {
-    type: 'metabolomics-untargeted',
-    phase: 'pass1b-06',
-    title:
-      'Metabolomics Untargeted data (metadata, QC and quantitative results)',
-    object_zipfile: 'motrpac_pass1b-06_metabolomics-untargeted.tar.gz',
-    object_zipfile_size: '479.90 MB',
-  },
-  {
-    type: 'proteomics',
-    phase: 'pass1b-06',
-    title: 'Proteomics data (metadata, QC and quantitative results)',
-    object_zipfile: 'motrpac_pass1b-06_proteomics.tar.gz',
-    object_zipfile_size: '349.67 MB',
-  },
-  {
-    type: 'phenotype',
-    phase: 'pass1b-06',
-    title: 'Phenotypic data (including raw data)',
-    object_zipfile: 'motrpac_pass1b-06_phenotype.tar.gz',
-    object_zipfile_size: '5.80 MB',
-  },
-];
+function OpenAccessBundleDownloads({ sets }) {
+  const numOfSets = sets === 'all' ? BundleDataTypes.length : 2;
 
-function OpenAccessBundleDownloads() {
   return (
-    <div className="table-responsive">
-      <table className="table table-striped table-sm">
-        <tbody>
-          {bundleData.map((item) => {
-            return (
-              <tr key={`${item.type}-${item.phase}`}>
-                <td className="pt-1 px-2">
-                  <span>{item.title}</span>
-                </td>
-                <td className="open-access-bundle-data-download-link pt-1 px-2">
-                  <button
-                    type="button"
-                    className="btn-data-download"
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    <i className="material-icons open-access-bundle-data-download-icon">
-                      save_alt
-                    </i>
-                  </button>
-                  <span className="file-size">{`(${item.object_zipfile_size})`}</span>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <div className="row row-cols-1 row-cols-md-2 open-access-bundle-data-sets">
+      {BundleDataTypes.slice(0, numOfSets).map((item) => {
+        return (
+          <div
+            key={`${item.type}-${item.object_zipfile}`}
+            className={`open-access-bundle-data-sets-item col ${
+              sets === 'all' ? 'mb-4' : null
+            }`}
+          >
+            <div className="card">
+              <div className="card-body">
+                <h6 className="card-title d-flex align-items-center justify-content-between open-access-bundle-data-title-container">
+                  <span className="open-access-bundle-data-title font-weight-bolder">
+                    {item.title}
+                  </span>
+                  <span className="open-access-bundle-data-size">{`${item.object_zipfile_size}`}</span>
+                </h6>
+                <p>{item.description}</p>
+                <BundleDownloadButton bundlefile={item.object_zipfile} />
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
+
+OpenAccessBundleDownloads.propTypes = {
+  sets: PropTypes.string,
+};
+
+OpenAccessBundleDownloads.defaultProps = {
+  sets: '',
+};
 
 export default OpenAccessBundleDownloads;
