dbc00352d463982993e7d19787215305a2363b80
Jimmy Zhen <jzhen@stanford.edu>
2023-04-14 15:40:36 -0700

    Update props and handling for has-result-filters as returned search response shape changed

--- src/Search/deaSearchResultFilters.jsx
+++ src/Search/deaSearchResultFilters.jsx
@@ -78,9 +78,12 @@ function SearchResultFilters({
                 -1;
             const resultCount =
               hasResultFilters &&
-              hasResultFilters[item.keyName] &&
-              Object.keys(hasResultFilters[item.keyName]).length &&
-              hasResultFilters[item.keyName][filter.filter_value.toLowerCase()];
+              hasResultFilters.timewise &&
+              hasResultFilters.timewise[item.keyName] &&
+              Object.keys(hasResultFilters.timewise[item.keyName]).length &&
+              hasResultFilters.timewise[item.keyName][
+                filter.filter_value.toLowerCase()
+              ];
             return (
               <button
                 key={filter.filter_label}
@@ -195,10 +198,16 @@ SearchResultFilters.propTypes = {
   handleSearch: PropTypes.func.isRequired,
   resetSearch: PropTypes.func.isRequired,
   hasResultFilters: PropTypes.shape({
-    assay: PropTypes.object,
-    comparison_group: PropTypes.object,
-    sex: PropTypes.object,
-    tissue: PropTypes.object,
+    timewise: PropTypes.shape({
+      assay: PropTypes.object,
+      comparison_group: PropTypes.object,
+      sex: PropTypes.object,
+      tissue: PropTypes.object,
+    }),
+    training: PropTypes.shape({
+      assay: PropTypes.object,
+      tissue: PropTypes.object,
+    }),
   }),
 };
 
