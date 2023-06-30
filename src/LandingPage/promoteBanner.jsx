e0815f3d14ed372c81a7335eb80422e70098489e
Jimmy Zhen <jzhen@stanford.edu>
2023-06-30 00:32:35 -0700

    Update banner content and UIs for upcoming July office hour

--- src/LandingPage/promoteBanner.jsx
+++ src/LandingPage/promoteBanner.jsx
@@ -4,25 +4,26 @@ import { trackEvent } from '../GoogleAnalytics/googleAnalytics';
 // Function to render landing page announcement
 function PromoteBanner() {
   return (
-    <div className="promote-banner d-flex align-items-center justify-content-center w-100 text-light px-3 py-2">
-      <span>
-        The Bioinformatics Center of MoTrPAC will be hosting a virtual Office
-        Hour on Thursday, June 22, 2023 at 11:00 am Pacific Time.
-      </span>
+    <div className="office-hour-banner border w-100 px-3 py-4 mb-4">
+      <h3 className="office-hour-title">Join Us</h3>
+      <div className="office-hour-content mb-3">
+        The Bioinformatics Center of MoTrPAC will be hosting the next virtual
+        Office Hour on Tuesday, July 18, 2023 at 11:00 am Pacific Time.
+      </div>
       <a
-        href="https://docs.google.com/forms/d/e/1FAIpQLSf2X3PEzEibaPnxo2mSwPwV0Rc74dzoqZVtYwLssLSmo3DypQ/viewform"
-        className="btn btn-light btn-sm ml-2"
+        href="https://docs.google.com/forms/d/e/1FAIpQLSekaYf_S0P1Mc_tqEW4RENvaicr5vBXgKObbeAB4VIusQlSIg/viewform"
+        className="btn btn-primary"
         role="button"
         target="_blank"
         rel="noopener noreferrer"
         onClick={trackEvent.bind(
           this,
           'MoTrPAC Office Hour',
-          'June 22, 2023',
+          'July 18, 2023',
           'Landing Page'
         )}
       >
-        Sign up
+        SIGN UP
       </a>
     </div>
   );
