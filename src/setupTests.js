b0e31a403ce96b6e892282aed3656ccfce7107b2
Jimmy Zhen <jzhen@stanford.edu>
2023-10-22 23:08:36 -0700

    Add global Jest test config to resolve error related to <video> element

--- src/setupTests.js
+++ src/setupTests.js
@@ -13,8 +13,16 @@ jest.mock('react-chartjs-2', () => ({
   Doughnut: () => null,
 }));
 
+// fix for 'crypto.getRandomValues() not supported' error
 Object.defineProperty(globalThis, 'crypto', {
   value: {
     getRandomValues: (arr) => crypto.randomBytes(arr.length),
   },
+});
+
+// fix for 'unstable_flushDiscreteUpdates: Cannot flush updates when React is already rendering' error
+// when <video> is used in a component
+// (https://github.com/testing-library/react-testing-library/issues/470#issuecomment-761821103)
+Object.defineProperty(HTMLMediaElement.prototype, 'muted', {
+  set: () => {},
 });
