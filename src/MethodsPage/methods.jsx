e6a617a085ca9963d984f06cd3e645a39893c9ef
Jimmy Zhen <jzhen@stanford.edu>
2024-01-22 04:34:02 -0800

    Add external links to animal/adult/pediatric protocols

--- src/MethodsPage/methods.jsx
+++ src/MethodsPage/methods.jsx
@@ -23,20 +23,33 @@ export function Methods({ profile }) {
           <div className="lead col-12">
             The MoTrPAC study is divided into two main parts; human and animal
             (rats). Preclinical Animal Study Sites (PASSs) conduct the endurance
-            exercise and training intervention in rats, and Human Clinical
-            Exercise Sites conduct the human endurance and resistance training
-            interventions. Multiple biospecimen samples are collected at
-            different time points after acute and chronic exercise. Please see
-            the Animal protocol below for details on the animal training
-            intervention and sample collection. The biospecimen samples are
-            distributed from a central biorepository to various Chemical
-            Analysis Sites (CASs) for molecular ‘omics analysis. Specific
-            methods for the different molecular assays are described in the
-            Manual Of Procedures (MOPs). There is one for the genomic,
-            transcriptomic and epigenomic data (GET), one for proteomics and one
-            for metabolomics. In addition, the assay-specific quality control
-            (QC) procedures are described in the QC Standard Operating
-            Procedures (SOPs) documents.
+            exercise and training intervention in rats (see{' '}
+            <ExternalLink
+              to="https://motrpac.org/actDocumentDownload.cfm?docGUID=A31CDD1F-8A59-41D9-BABA-125B37A39BF5"
+              label="animal protocol"
+            />
+            ), and Human Clinical Exercise Sites conduct the human endurance and
+            resistance training interventions (see{' '}
+            <ExternalLink
+              to="https://motrpac.org/actDocumentDownload.cfm?docGUID=8120CEC8-5761-4C74-9EB3-4544EEC99FA4"
+              label="adult protocol"
+            />{' '}
+            and{' '}
+            <ExternalLink
+              to="https://motrpac.org/actDocumentDownload.cfm?docGUID=67F818DF-22CC-4C56-88F4-B07FDA8BA6EE"
+              label="pediatric protocol"
+            />
+            ). Multiple biospecimen samples are collected at different time
+            points after acute and chronic exercise. Please see the Animal
+            protocol below for details on the animal training intervention and
+            sample collection. The biospecimen samples are distributed from a
+            central biorepository to various Chemical Analysis Sites (CASs) for
+            molecular ‘omics analysis. Specific methods for the different
+            molecular assays are described in the Manual Of Procedures (MOPs).
+            There is one for the genomic, transcriptomic and epigenomic data
+            (GET), one for proteomics and one for metabolomics. In addition, the
+            assay-specific quality control (QC) procedures are described in the
+            QC Standard Operating Procedures (SOPs) documents.
           </div>
         </div>
         {userType && userType === 'internal' ? (
