89c99fa2103ff559157456f24491951ae104fd52
Jimmy Zhen <jzhen@stanford.edu>
2024-04-20 23:48:16 -0700

    New component to render list of selective download cards for different study data (e.g. pass1b-06, pass1a-06)

---
+++ src/BrowseDataPage/components/selectiveDataDownloadsCard.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+
+function SelectiveDataDownloadsCard({
+  cardIcon,
+  cardTitle,
+  dataSelectHandler,
+  selectedData,
+  cssSelector,
+  children,
+}) {
+  const history = useHistory();
+
+  function handleDataSelect(e) {
+    e.preventDefault();
+    dataSelectHandler();
+    history.push('/data-download/file-browser', {
+      state: { selectedData },
+    });
+  }
+
+  return (
+    <div
+      className={`card mb-4 shadow-sm selective-data-downloads-card ${cssSelector}`}
+    >
+      <div className="card-header">
+        <h4 className="my-0 font-weight-normal d-flex align-items-center justify-content-center">
+          <span className="material-icons mr-1">{cardIcon}</span>
+          <span>{cardTitle}</span>
+        </h4>
+      </div>
+      <div className="card-body">
+        {children}
+        <button
+          type="button"
+          className="btn btn-lg btn-block btn-primary"
+          onClick={(e) => handleDataSelect(e)}
+        >
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+}
+
+SelectiveDataDownloadsCard.propTypes = {
+  cardIcon: PropTypes.string.isRequired,
+  cardTitle: PropTypes.string.isRequired,
+  dataSelectHandler: PropTypes.func.isRequired,
+  selectedData: PropTypes.string,
+  cssSelector: PropTypes.string,
+};
+
+SelectiveDataDownloadsCard.defaultProps = {
+  selectedData: '',
+  cssSelector: '',
+};
+
+export default SelectiveDataDownloadsCard;
