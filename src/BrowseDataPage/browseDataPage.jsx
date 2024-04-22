9f2d6a69c0531f35030d7d1bbc7309c3817a3288
Jimmy Zhen <jzhen@stanford.edu>
2024-04-21 19:33:02 -0700

    Restructured components due to data download page UI changes

--- src/BrowseDataPage/browseDataPage.jsx
+++ src/BrowseDataPage/browseDataPage.jsx
@@ -1,17 +1,12 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import PageTitle from '../lib/ui/pageTitle';
-import BrowseDataTable from './browseDataTable';
 import actions from './browseDataActions';
 import BrowseDataFilter from './browseDataFilter';
-import BootstrapSpinner from '../lib/ui/spinner';
-import OpenAccessBrowseDataSummary from './components/openAccessSummary';
-import AuthAccessBrowseDataSummary from './components/authAccessSummary';
 import dataDownloadStructuredData from '../lib/searchStructuredData/dataDownload';
 import UserSurveyModal from '../UserSurvey/userSurveyModal';
+import DataDownloadsMain from './components/dataDownloadsMain';
 
 export function BrowseDataPage({
   profile,
@@ -26,9 +21,6 @@ export function BrowseDataPage({
   showUserSurveyModal,
   surveyId,
 }) {
-  // anonymous user or authenticated user
-  const userType = profile.user_metadata && profile.user_metadata.userType;
-
   useEffect(() => {
     if (showUserSurveyModal) {
       const userSurveyModalRef = document.querySelector('body');
@@ -45,58 +37,29 @@ export function BrowseDataPage({
           {JSON.stringify(dataDownloadStructuredData)}
         </script>
       </Helmet>
-      <PageTitle
-        title={
-          userType && userType === 'internal'
-            ? 'Endurance Training and Acute Exercise Young Adult Rats Data'
-            : 'Endurance Exercise Training Young Adult Rats (6 months) Data'
+      <DataDownloadsMain
+        profile={profile}
+        filteredFiles={filteredFiles}
+        fetching={fetching}
+        activeFilters={activeFilters}
+        onChangeFilter={onChangeFilter}
+        onResetFilters={onResetFilters}
+        handleDownloadRequest={handleDownloadRequest}
+        downloadRequestResponse={downloadRequestResponse}
+        waitingForResponse={waitingForResponse}
+      />
+      <UserSurveyModal
+        userID={
+          profile && profile.email
+            ? profile.email
+            : surveyId
+              ? surveyId
+              : 'anonymous'
+        }
+        dataContext={
+          downloadRequestResponse.length ? 'selected_files' : 'bundled_files'
         }
       />
-      {!userType || (userType && userType === 'external') ? (
-        <OpenAccessBrowseDataSummary profile={profile} />
-      ) : null}
-      {userType && userType === 'internal' ? (
-        <AuthAccessBrowseDataSummary />
-      ) : null}
-      <div className="browse-data-container row">
-        <BrowseDataFilter
-          activeFilters={activeFilters}
-          onChangeFilter={onChangeFilter}
-          onResetFilters={onResetFilters}
-          userType={userType}
-        />
-        {fetching && (
-          <div className="col-md-9">
-            <BootstrapSpinner isFetching={fetching} />
-          </div>
-        )}
-        {!fetching && !filteredFiles.length && (
-          <div className="browse-data-table-wrapper col-md-9">
-            <p className="mt-4">
-              <span>
-                No matches found for the selected filters. Please refer to the{' '}
-                <Link to="/summary">Summary Table</Link> for data that are
-                available.
-              </span>
-            </p>
-          </div>
-        )}
-        {!fetching && filteredFiles.length && (
-          <BrowseDataTable
-            filteredFiles={filteredFiles}
-            handleDownloadRequest={handleDownloadRequest}
-            downloadRequestResponse={downloadRequestResponse}
-            waitingForResponse={waitingForResponse}
-            profile={profile}
-          />
-        )}
-        <UserSurveyModal
-          userID={profile && profile.email ? profile.email : (surveyId ? surveyId : 'anonymous')}
-          dataContext={
-            downloadRequestResponse.length ? 'selected_files' : 'bundled_files'
-          }
-        />
-      </div>
     </div>
   );
 }
