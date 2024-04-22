58eb51f6118aa77bd46396e4f5265a069f442519
Jimmy Zhen <jzhen@stanford.edu>
2024-04-22 14:41:05 -0700

    Updated unit test assertion

--- src/App/__test__/App.test.jsx
+++ src/App/__test__/App.test.jsx
@@ -23,7 +23,7 @@ describe('<App />', () => {
   });
 
   test('It should contain fifteen <Route /> children', () => {
-    expect(component.find('Route').length).toBe(20);
+    expect(component.find('Route').length).toBe(21);
   });
 
   test('It should contain four <PrivateRoute /> children', () => {
