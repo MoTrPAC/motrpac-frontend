db69b6123df05a1f0d43f208e138a692720cbc86
Jimmy Zhen <jzhen@stanford.edu>
2024-04-23 01:05:49 -0700

    Restrict the display of additional consortium data info to internal users

--- src/BrowseDataPage/components/dataDownloadsMain.jsx
+++ src/BrowseDataPage/components/dataDownloadsMain.jsx
@@ -242,33 +242,41 @@ function DataDownloadsMain({
           </div>
         </div>
         {/* Additional data information */}
-        <DataTypeInfo grid="col-6 col-md-6" />
-        <div className="browse-data-summary-content col-6 col-md-6">
-          <div className="bd-callout bd-callout-info">
-            <h4>Additional Information</h4>
-            <p>
-              The currently available young adult rats experimental data for
-              acute exercise and endurance training include all tissues and
-              assays from the very last consortium data release, as well as
-              additional tissues and assays made available afterwards. The
-              phenotypic data sets have also been updated since then.
-            </p>
-            <p>
-              Please refer to this{' '}
-              <a
-                href="https://docs.google.com/document/d/1bdXcYQLZ65GpJKTjf9XwRxhrfHJSD9NIqCxhG6icL8U"
-                className="inline-link-with-icon"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                README
-                <i className="material-icons readme-file-icon">description</i>
-              </a>{' '}
-              document for the data included in the very last consortium data
-              release.
-            </p>
-          </div>
-        </div>
+        {userType && userType === 'internal' ? (
+          <>
+            <DataTypeInfo grid="col-6 col-md-6" />
+            <div className="browse-data-summary-content col-6 col-md-6">
+              <div className="bd-callout bd-callout-info">
+                <h4>Additional Information</h4>
+                <p>
+                  The currently available young adult rats experimental data for
+                  acute exercise and endurance training include all tissues and
+                  assays from the very last consortium data release, as well as
+                  additional tissues and assays made available afterwards. The
+                  phenotypic data sets have also been updated since then.
+                </p>
+                <p>
+                  Please refer to this{' '}
+                  <a
+                    href="https://docs.google.com/document/d/1bdXcYQLZ65GpJKTjf9XwRxhrfHJSD9NIqCxhG6icL8U"
+                    className="inline-link-with-icon"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    README
+                    <i className="material-icons readme-file-icon">
+                      description
+                    </i>
+                  </a>{' '}
+                  document for the data included in the very last consortium
+                  data release.
+                </p>
+              </div>
+            </div>
+          </>
+        ) : (
+          <DataTypeInfo grid="col-12 col-md-12" />
+        )}
       </div>
     </div>
   );
