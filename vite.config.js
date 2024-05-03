6ea174cbaebdbe6d3753d5933c890bc726447eb5
Mihir Samdarshi <msamdars@stanford.edu>
2024-05-03 16:41:05 -0700

    feat: add rollup-plugin-visualizer

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react-swc';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(), visualizer()],
   build: {
     outDir: 'build',
   },
