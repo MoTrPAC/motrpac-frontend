ca6b6f613689593c4454a861effe08d13976f0d9
Mihir Samdarshi <msamdars@stanford.edu>
2024-07-23 15:20:36 -0700

    fix: only run purgeCSS during build

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,8 @@
 const purgecss = require('@fullhuman/postcss-purgecss')
 
+console.log("NODE_ENV", process.env.NODE_ENV)
 module.exports = {
-  plugins: [
-    purgecss({
-      content: ['build/index.html', 'build/assets/*.js', "build/assets/*.css"],
-    })
-  ]
+  plugins: process.env.NODE_ENV === "production" ? [purgecss({
+    content: ['build/index.html', 'build/assets/*.js', "build/assets/*.css"],
+  })] : []
 }
