e513730f071245b9446bbe6f49021b347246712d
Mihir Samdarshi <msamdars@stanford.edu>
2024-07-23 22:05:18 -0700

    fix: add back React Helmet to Project Overview page

--- src/MainStudy/mainStudy.jsx
+++ src/MainStudy/mainStudy.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
+import { Helmet } from "react-helmet";
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
