e9ccc5e25eafc8d480d99292de9cce394da32b1c
Jimmy Zhen <jzhen@stanford.edu>
2024-11-11 15:57:35 -0800

    Complete adding metabolomics and proteomics data deposition info to component

--- src/DataDeposition/dataDepositionPage.jsx
+++ src/DataDeposition/dataDepositionPage.jsx
@@ -32,14 +32,18 @@ function DataDeposition() {
               </thead>
               <tbody>
                 <tr>
-                  <td>SRA</td>
+                  <td>
+                    <ExternalLink to="https://www.ncbi.nlm.nih.gov/sra" label="SRA" />
+                  </td>
                   <td>Raw data</td>
                   <td>
                     <ExternalLink to="https://www.ncbi.nlm.nih.gov/bioproject/PRJNA908279" label="PRJNA908279" />
                   </td>
                 </tr>
                 <tr>
-                  <td>GEO</td>
+                  <td>
+                    <ExternalLink to="https://www.ncbi.nlm.nih.gov/geo/" label="GEO" />
+                  </td>
                   <td>
                     Processed pipeline outputs (e.g., raw counts, merged peakset, bismark
                     coverage files)
@@ -64,10 +68,115 @@ function DataDeposition() {
               </thead>
               <tbody>
                 <tr>
-                  <td>IMMPORT</td>
+                  <td>
+                    <ExternalLink to="https://www.immport.org/home" label="IMMPORT" />
+                  </td>
                   <td>Readouts, results</td>
                   <td>
-                    <ExternalLink to="https://www.immport.org/shared/home" label="SDY2193" />
+                    <ExternalLink to="https://immport.org/shared/study/SDY2193" label="SDY2193" />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          {/* Metabolomics data deposition table */}
+          <h4 className="mt-4">Metabolomics (Targeted and Untargeted)</h4>
+          <div className="table-responsive">
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Repository</th>
+                  <th>Data type</th>
+                  <th>Project</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <ExternalLink to="https://www.metabolomicsworkbench.org/" label="Metabolomics Workbench" />
+                  </td>
+                  <td>Metabolomics raw + results files (identification and quantification)</td>
+                  <td>
+                    <ul className="list-unstyled">
+                      <li>
+                        <ExternalLink to="https://www.metabolomicsworkbench.org/data/DRCCMetadata.php?Mode=Project&ProjectID=PR001020" label="PR001020" />
+                      </li>
+                      <li>
+                        <ExternalLink to="http://dx.doi.org/10.21228/M8V97D" label="DOI: 10.21228/M8V97D" />
+                      </li>
+                    </ul>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          {/* Proteomics data deposition table */}
+          <h4 className="mt-4">Proteomics</h4>
+          <div className="table-responsive">
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Repository</th>
+                  <th>Data type</th>
+                  <th>Metadata</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <ExternalLink to="https://massive.ucsd.edu/ProteoSAFe/static/massive.jsp" label="MassIVE" />
+                  </td>
+                  <td>
+                    <ul className="proteomics-data-submissions">
+                      <li>
+                        <span>
+                          Global Protein Abundance in Gastrocnemius, White Adipose, Cortex,
+                          Lung, and Kidney
+                        </span>
+                        <span className="text-muted ml-1">(Dataset ID: MSV000092911)</span>
+                      </li>
+                      <li>
+                        <span>
+                          Protein Phosphorylation in Gastrocnemius, White Adipose Tissue,
+                          Cortex, Lung, and Kidney
+                        </span>
+                        <span className="text-muted ml-1">(Dataset ID: MSV000092925)</span>
+                      </li>
+                      <li>
+                        <span>
+                          Global Protein Abundance in Heart and Liver
+                        </span>
+                        <span className="text-muted ml-1">(Dataset ID: MSV000092922)</span>
+                      </li>
+                      <li>
+                        <span>
+                          Protein Acetylation in Heart and Liver
+                        </span>
+                        <span className="text-muted ml-1">(Dataset ID: MSV000092924)</span>
+                      </li>
+                      <li>
+                        <span>
+                          Protein Phosphorylation in Heart and Liver
+                        </span>
+                        <span className="text-muted ml-1">(Dataset ID: MSV000092923)</span>
+                      </li>
+                      <li>
+                        <span>
+                          Protein Ubiquitination in Heart and Liver
+                        </span>
+                        <span className="text-muted ml-1">(Dataset ID: MSV000092931)</span>
+                      </li>
+                    </ul>
+                  </td>
+                  <td>
+                    <ul className="list-unstyled">
+                      <li>
+                        <ExternalLink to="https://drive.google.com/file/d/1d7etTfATHkATuOXM3SuLV7IwrpFJlYS0/view?usp=drive_link" label="Results files" />
+                      </li>
+                      <li>
+                        <ExternalLink to="https://docs.google.com/document/d/1Kj81cOGdDUCYos7FRwHx8bBxY6Hs7EINDJ4I7zflMOM/edit?usp=drive_link" label="Methods" />
+                      </li>
+                    </ul>
                   </td>
                 </tr>
               </tbody>
