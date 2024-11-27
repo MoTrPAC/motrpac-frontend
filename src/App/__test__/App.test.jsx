d6b30d48f269d1b0d6ae5765c823afadb31bee9d
Jimmy Zhen <jzhen@stanford.edu>
2024-11-26 22:01:57 -0800

    Add new public route to phenotype data technical guide

--- src/App/__test__/App.test.jsx
+++ src/App/__test__/App.test.jsx
@@ -23,7 +23,7 @@ describe('<App />', () => {
   });
 
   test('It should contain fifteen <Route /> children', () => {
-    expect(component.find('Route').length).toBe(25);
+    expect(component.find('Route').length).toBe(26);
   });
 
   test('It should contain four <PrivateRoute /> children', () => {
