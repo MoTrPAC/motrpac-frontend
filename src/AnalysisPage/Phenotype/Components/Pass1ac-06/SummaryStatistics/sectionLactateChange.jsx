605412136d08c79712594ff1799ff7b4060f6ef3
Jimmy Zhen <jzhen@stanford.edu>
2024-12-18 23:38:44 -0800

    Initial commit of pass1a-06 pheno data anaylsis section content wrapper components

---
+++ src/AnalysisPage/Phenotype/Components/Pass1ac-06/SummaryStatistics/sectionLactateChange.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PlotLactateChange from './plotLactateChange';
+
+/**
+ * Function component for lactate change section of summary statistics category
+ * in the PASS1AC-06 phenotypic data analysis
+ *
+ * @param {array} phenoData Curated phenotypic data
+ *
+ * @returns JSX element of the lactate change section
+ */
+function SectionLactateChange() {
+  return (
+    <div className="analysis-section-content-container w-100">
+      <h3>Lactate Change (before / after) Due to Acute Exercise</h3>
+      <div className="analysis-section-plot-container row mb-4">
+        {/* phenotpypic data analysis plots */}
+        <PlotLactateChange />
+      </div>
+    </div>
+  );
+}
+
+export default SectionLactateChange;
