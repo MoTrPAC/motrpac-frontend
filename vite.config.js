89663b130855d0e95423ed28c0b8e65a5fceaf7f
Jimmy Zhen <jzhen@stanford.edu>
2025-02-20 10:57:46 -0800

    Rename setupTests file with *jsx extension

--- vite.config.js
+++ vite.config.js
@@ -51,7 +51,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
+    setupFiles: './src/setupTests.jsx',
     css: true,
     reporters: ['verbose'],
     coverage: {
