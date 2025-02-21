b0617cace1cb0f403ca7d042e5145a09283f6b77
Jimmy Zhen <jzhen@stanford.edu>
2025-02-21 14:23:41 -0800

    Refactor to update redux store provider implementation

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { install } from 'ga-gtag';
 import trackingId from './GoogleAnalytics/googleAnalytics';
 import App from './App/App';
+import configureStore from './App/configureStore';
 import * as serviceWorker from './serviceWorker';
 import './sass/main.scss';
 
 install(trackingId());
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+const store = configureStore();
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
