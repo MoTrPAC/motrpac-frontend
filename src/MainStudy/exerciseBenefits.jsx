e917e2a6859c3b28142ee16fc714ca60fec8b740
Jimmy Zhen <jzhen@stanford.edu>
2025-05-26 00:07:48 -0700

    chore: remove unused dependency import

--- src/MainStudy/exerciseBenefits.jsx
+++ src/MainStudy/exerciseBenefits.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import ReactWordcloud from 'react-wordcloud';
 import PageTitle from '../lib/ui/pageTitle';
