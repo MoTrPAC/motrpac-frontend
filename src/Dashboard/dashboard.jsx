e2585f43f226a27ae1186ba48f52d181ed21cf98
Jimmy Zhen <jzhen@stanford.edu>
2025-07-16 01:14:07 -0700

    refactor: update dashboard data release announcements
    Enhanced the internal user dashboard to separately highlight the release of human sedentary adults and young adult rat datasets, including new icons and improved layout. Updated messaging for clarity and added direct links to relevant consortium release documents.

--- src/Dashboard/dashboard.jsx
+++ src/Dashboard/dashboard.jsx
@@ -9,13 +9,17 @@ import DataStatusActions from '../DataStatusPage/dataStatusActions';
 import '@styles/dashboard.scss';
 
 /**
- * Renders the Dashboard page.
+ * Renders the Dashboard page
  *
  * @param {Object} profile  Redux state of authenticated user profile
  *
  * @returns {object} JSX representation of the Dashboard
  */
-export function Dashboard({ profile = {}, handleQCDataFetch, lastModified = '' }) {
+export function Dashboard({ 
+  profile = {}, 
+  handleQCDataFetch, 
+  lastModified = '',
+}) {
   const userType = profile.user_metadata && profile.user_metadata.userType;
 
   return (
@@ -24,36 +28,74 @@ export function Dashboard({ profile = {}, handleQCDataFetch, lastModified = '' }
         <html lang="en" />
         <title>Dashboard - MoTrPAC Data Hub</title>
       </Helmet>
+
       {userType && userType === 'internal' && (
         <div className="jumbotron jumbotron-fluid alert-data-release">
           <div className="container">
-            <h1 className="office-hour-title display-2">
+            <h1 className="highlight-title display-1 text-center mb-4">
               <i className="bi bi-rocket-takeoff mr-3" />
-              <span>It's here!</span>
+              <span>Available Now!</span>
             </h1>
-            <div className="w-100 lead">
-              <span className="data-release-text">
-                The pre-COVID human sedentary adults dataset is now available to consortium
-                users. You may
-                {' '}
-                <Link to="/data-download">download them</Link>
-                {' '}
-                or
-                {' '}
-                <Link to="/search">explore the differential abundance</Link>
-                {' '}
-                in the dataset. Please refer to the
-                {' '}
-                <a href={import.meta.env.VITE_DATA_RELEASE_README} target="_blank" rel="noopener noreferrer">
-                  Consortium Release document
-                </a>
-                {' '}
-                for more information on this dataset.
-              </span>
+            <div className="row">
+              <div className="col-md-6 lead d-flex align-items-start">
+                <div className="data-release-icon mr-2">
+                  <span class="material-icons">
+                    person
+                  </span>
+                </div>
+                <div className="data-release-icon mr-1">
+                  <span className="data-release-text">
+                    The pre-COVID human sedentary adults dataset has been made available
+                    to consortium users. You may
+                    {' '}
+                    <Link to="/data-download">download them</Link>
+                    {' '}
+                    or
+                    {' '}
+                    <Link to="/search">explore the differential abundance</Link>
+                    {' '}
+                    in the dataset. Please refer to the
+                    {' '}
+                    <a href={import.meta.env.VITE_DATA_RELEASE_README} target="_blank" rel="noopener noreferrer">
+                      Consortium Release document
+                    </a>
+                    {' '}
+                    for more information on this dataset.
+                  </span>
+                </div>
+              </div>
+              <div className="col-md-6 lead d-flex align-items-start">
+                <div className="data-release-icon mr-2">
+                  <span class="material-icons">
+                    pest_control_rodent
+                  </span>
+                </div>
+                <div className="data-release-icon mr-1">
+                  <span className="data-release-text">
+                    The consortium release of young adult rats acute exercise data is also
+                    accessible now. Users may
+                    {' '}
+                    <Link to="/data-download">download them</Link>
+                    {' '}
+                    or
+                    {' '}
+                    <Link to="/search">explore the differential abundance</Link>
+                    {' '}
+                    in the dataset. Please refer to the
+                    {' '}
+                    <a href="https://docs.google.com/document/d/1PlWzZ6SPMX7SeW8TxeDKEfnACrG2YOiZclDsw2UBs84/edit?tab=t.0#heading=h.nizg0n7kpig1" target="_blank" rel="noopener noreferrer">
+                      Consortium Release document
+                    </a>
+                    {' '}
+                    for more information on this dataset.
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       )}
+
       {userType && userType === 'external' && (
         <div className="alert-data-release">
           <h1 className="office-hour-title display-4 mb-4">
