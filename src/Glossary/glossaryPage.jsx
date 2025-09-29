04b617cfc26e7ec2217dee4326e66e5a31b68d0f
Jimmy Zhen <jzhen@stanford.edu>
2025-09-28 23:33:56 -0700

    feat: add Glossary page for PASS1B-06 variable names
    Introduces a new Glossary page displaying variable names and definitions for the endurance trained young adult rats study. The page uses data from a JSON file and includes basic styling and metadata.

---
+++ src/Glossary/glossaryPage.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import PageTitle from '../lib/ui/pageTitle';
+
+import variableNamesPass1b06 from '../data/glossary_variable_names_pass1b06.json';
+
+import '@styles/glossary.scss';
+
+function Glossary() {
+  return (
+    <div className="glossaryPage px-3 px-md-4 mb-3 container">
+      <Helmet>
+        <html lang="en" />
+        <title>Glossary - MoTrPAC Data Hub</title>
+      </Helmet>
+      <PageTitle title="Glossary" />
+      <div className="glossary-content-container row mb-4">
+        <div className="col-12">
+          <h3 className="study-title-species-icon mr-1 d-flex align-items-center mt-4">
+            <span className="material-icons study-title-species-icon mr-1">pest_control_rodent</span>
+            <span>Variable names in the endurance trained young adult rats study</span>
+          </h3>
+          <div className="glossary-table-container table-responsive my-2">
+            <table className="glossary-table table table-striped table-bordered">
+              <thead className="thead-dark">
+                <tr>
+                  <th scope="col">Variable Name</th>
+                  <th scope="col">Definition</th>
+                </tr>
+              </thead>
+              <tbody>
+                {variableNamesPass1b06.map((item, index) => (
+                  <tr key={`glossary-item-${index}-${item.variable_name}`}>
+                    <th scope="row">{item.variable_name}</th>
+                    <td>{item.definition}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Glossary;
