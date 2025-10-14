5aec58c517d4e1585d48e67360675dfe13fec2b5
Jimmy Zhen <jzhen@stanford.edu>
2025-10-13 22:42:25 -0700

    feat: add reference genome filter to BrowseDataFilter
    Introduces a new 'reference_genome' filter to the BrowseDataFilter component, dynamically displaying options based on selected data types. The filter is hidden if no options are available, and PropTypes are updated to include the new filter.

--- src/BrowseDataPage/browseDataFilter.jsx
+++ src/BrowseDataPage/browseDataFilter.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
-import browseDataFilters, { tissues, omes } from '../lib/browseDataFilters';
+import browseDataFilters, { tissues, omes, referenceGenomes } from '../lib/browseDataFilters';
 import assayList from '../lib/assayList';
 
 import '@styles/browseData.scss';
 import '@styles/tooltip.scss';
 
-function BrowseDataFilter({ activeFilters = { assay: [], omics: [], tissue_name: [], category: [] }, onChangeFilter, onResetFilters }) {
+function BrowseDataFilter({ activeFilters = { assay: [], omics: [], tissue_name: [], category: [], reference_genome: [] }, onChangeFilter, onResetFilters }) {
   const dataDownload = useSelector((state) => state.browseData);
 
   const fileFilters = [...browseDataFilters];
@@ -17,6 +17,17 @@ function BrowseDataFilter({ activeFilters = { assay: [], omics: [], tissue_name:
   }
 
   fileFilters.forEach((item) => {
+    if (item.keyName === 'reference_genome') {
+      if (dataDownload.pass1b06DataSelected) {
+        item.filters = referenceGenomes.pass1b_06;
+      }
+      if (dataDownload.pass1a06DataSelected) {
+        item.filters = referenceGenomes.pass1a_06;
+      }
+      if (dataDownload.humanPrecovidSedAduDataSelected) {
+        item.filters = referenceGenomes.human_sed_adu;
+      }
+    }
     if (item.keyName === 'tissue_name') {
       if (dataDownload.pass1b06DataSelected) {
         item.filters = tissues.pass1b_06;
@@ -51,47 +62,55 @@ function BrowseDataFilter({ activeFilters = { assay: [], omics: [], tissue_name:
       }
     }
   });
-  const filters = fileFilters.map((item) => (
-    <div key={item.name} className="card filter-module mb-4">
-      <div className="card-header font-weight-bold d-flex align-items-center">
-        <div>{item.name}</div>
-        {item.keyName === 'category' && item.name === 'Category' && (
-          <div className="data-filter-info-icon-wrapper d-flex align-items-center">
-            <i className="material-icons data-filter-info-icon ml-1">info</i>
-            <span className="tooltip-on-right" id="data-filter-info-tooltip">
-              <span>
-                <strong>Analysis</strong> - Differential analysis and normalized
-                data tables.
-                <br />
-                <strong>Results</strong> - Quantitative results,
-                experimental/sample metadata, and QA/QC reports.
+  const filters = fileFilters
+    .filter((item) => {
+      // Hide reference genome filter if no options available
+      if (item.keyName === 'reference_genome' && (!item.filters || item.filters.length === 0)) {
+        return false;
+      }
+      return true;
+    })
+    .map((item) => (
+      <div key={item.name} className="card filter-module mb-4">
+        <div className="card-header font-weight-bold d-flex align-items-center">
+          <div>{item.name}</div>
+          {item.keyName === 'category' && item.name === 'Category' && (
+            <div className="data-filter-info-icon-wrapper d-flex align-items-center">
+              <i className="material-icons data-filter-info-icon ml-1">info</i>
+              <span className="tooltip-on-right" id="data-filter-info-tooltip">
+                <span>
+                  <strong>Analysis</strong> - Differential analysis and normalized
+                  data tables.
+                  <br />
+                  <strong>Results</strong> - Quantitative results,
+                  experimental/sample metadata, and QA/QC reports.
+                </span>
+                <i />
               </span>
-              <i />
-            </span>
-          </div>
-        )}
+            </div>
+          )}
+        </div>
+        <div className="card-body">
+          {item.filters.map((filter) => {
+            const isActiveFilter =
+              activeFilters[item.keyName] &&
+              activeFilters[item.keyName].indexOf(filter) > -1;
+            return (
+              <button
+                key={filter}
+                type="button"
+                className={`btn filterBtn ${
+                  isActiveFilter ? 'activeFilter' : ''
+                }`}
+                onClick={() => onChangeFilter(item.keyName, filter)}
+              >
+                {filter}
+              </button>
+            );
+          })}
+        </div>
       </div>
-      <div className="card-body">
-        {item.filters.map((filter) => {
-          const isActiveFilter =
-            activeFilters[item.keyName] &&
-            activeFilters[item.keyName].indexOf(filter) > -1;
-          return (
-            <button
-              key={filter}
-              type="button"
-              className={`btn filterBtn ${
-                isActiveFilter ? 'activeFilter' : ''
-              }`}
-              onClick={() => onChangeFilter(item.keyName, filter)}
-            >
-              {filter}
-            </button>
-          );
-        })}
-      </div>
-    </div>
-  ));
+    ));
   return (
     <div className="col-md-3 browse-data-filter-group">
       <div className="browse-data-filter-group-header d-flex justify-content-between align-items-center mb-3">
@@ -115,6 +134,7 @@ BrowseDataFilter.propTypes = {
     assay: PropTypes.arrayOf(PropTypes.string),
     omics: PropTypes.arrayOf(PropTypes.string),
     category: PropTypes.arrayOf(PropTypes.string),
+    reference_genome: PropTypes.arrayOf(PropTypes.string),
   }),
   onChangeFilter: PropTypes.func.isRequired,
   onResetFilters: PropTypes.func.isRequired,
