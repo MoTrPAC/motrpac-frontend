a141eeaf733a7767cea188490f71b4f4bf8d6b00
Jimmy Zhen <jzhen@stanford.edu>
2025-12-04 23:44:19 -0800

    Re-enable download button in results tables
    Uncomments the download results button in both timewiseTable.jsx and trainingResultsTable.jsx, making the feature available again after previously being disabled due to a bug.

--- src/Search/trainingResultsTable.jsx
+++ src/Search/trainingResultsTable.jsx
@@ -112,8 +112,6 @@ function TrainingDataTable({
           setPageSize={setPageSize}
           pageSizeOptions={range(50, preGlobalFilteredRows.length)}
         />
-        {/* TODO: PASS1B-06 - Re-enable once download bug is fixed. See ticket for details. */}
-        {/*
         <div className="file-download-button">
           <button
             type="button"
@@ -129,7 +127,6 @@ function TrainingDataTable({
             <span>Download results</span>
           </button>
         </div>
-        */}
       </div>
       <div className="card mb-3">
         <div className="card-body table-ui-wrapper">
